39403b3cb33cd0f1ff295276c29fa1f3debae725
Robin Winslow <robin@robinwinslow.co.uk>
2017-08-27 11:11:37 +0100

    Optional custom message for 404s

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 
 
 @app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+def page_not_found(error):
+    return render_template('404.html', description=error.description), 404
