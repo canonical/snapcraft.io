4aa10b78d35b8207cc7f06b06d0d2d023219196b
Robin Winslow <robin@robinwinslow.co.uk>
2017-08-29 19:31:15 +0100

    Add error message when API request fails

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import abort, Flask, render_template
-from requests_cache import CachedSession
-from dateutil import parser
+import flask
+import requests
+import requests_cache
+import datetime
 import json
 import humanize
+from dateutil import parser
 
 
-app = Flask(__name__)
-cached_request = CachedSession(
-    expire_after=60,
-    old_data_on_error=True
-)
+app = flask.Flask(__name__)
+uncached_session = requests.Session()
+cached_session = requests_cache.CachedSession(expire_after=60)
+request_timeout = 2
 snap_details_url = (
     "https://api.snapcraft.io/api/v1/snaps/details/{snap_name}"
     "?channel=stable"
@@ -18,7 +19,9 @@ snap_details_url = (
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html', description=error.description), 404
+    return flask.render_template(
+        '404.html', description=error.description
+    ), 404
 
 
 @app.route('/<snap_name>/')
@@ -27,10 +30,9 @@ def snap_details(snap_name):
         'X-Ubuntu-Series': '16',
     }
 
-    response = cached_request.get(
+    response = _get_from_cache(
         snap_details_url.format(snap_name=snap_name),
-        headers=query_headers,
-        timeout=2
+        headers=query_headers
     )
 
     if response.status_code >= 400:
@@ -41,13 +43,14 @@ def snap_details(snap_name):
         if response.status_code == 404:
             message = 'Snap not found: {snap_name}'.format(**locals())
 
-        abort(response.status_code, message)
+        flask.abort(response.status_code, message)
 
     snap_data = json.loads(response.text)
 
     context = {
         # Always available
         'name': snap_data['title'],
+        'api_error': response.old_data_from_error,
         'version': snap_data['version'],
         'revision': snap_data['revision'],
         'filesize': humanize.naturalsize(snap_data['binary_filesize']),
@@ -68,7 +71,52 @@ def snap_details(snap_name):
         'icon_url': snap_data.get('icon_url'),
     }
 
-    return render_template(
+    return flask.render_template(
         'snap-details.html',
         **context
     )
+
+
+def _get_from_cache(url, headers):
+    """
+    Retrieve the response from the requests cache.
+    If the cache has expired then it will attempt to update the cache.
+    If it gets an error, it will use the cached response, if it exists.
+    """
+
+    request_error = False
+
+    request = cached_session.prepare_request(
+        requests.Request(
+            method='GET',
+            url=url,
+            headers=headers
+        )
+    )
+
+    cache_key = cached_session.cache.create_key(request)
+    response, timestamp = cached_session.cache.get_response_and_time(
+        cache_key
+    )
+
+    if response:
+        age = datetime.datetime.utcnow() - timestamp
+
+        if age > cached_session._cache_expire_after:
+            try:
+                new_response = uncached_session.send(
+                    request,
+                    timeout=request_timeout
+                )
+                if response.status_code >= 500:
+                    new_response.raise_for_status()
+            except:
+                request_error = True
+            else:
+                response = new_response
+    else:
+        response = cached_session.send(request)
+
+    response.old_data_from_error = request_error
+
+    return response
