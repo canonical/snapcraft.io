fd8e1ee5299b8506dec8b2c699874b38b294dc08
Robin Winslow <robin@robinwinslow.co.uk>
2017-09-01 10:17:32 +0100

    Add tests for snap routing

--- tests.py
+++ tests.py
@@ -10,9 +10,28 @@ class WebAppTestCase(unittest.TestCase):
         self.app.testing = True
 
     def test_no_homepage(self):
-        homepage_request = self.app.get('/')
-        assert homepage_request.status_code == 404
-        assert "Page not found" in str(homepage_request.data)
+        response = self.app.get('/')
+        assert response.status_code == 404
+        assert "Page not found" in str(response.data)
+
+    def test_redirect_to_add_slash(self):
+        response = self.app.get('/canonical-livepatch')
+        location = response.headers.get('Location')
+
+        assert response.status_code in (301,302)
+        assert location == "http://localhost/canonical-livepatch/"
+
+    def test_canonical_livepatch_snap(self):
+        response = self.app.get('/canonical-livepatch/')
+
+        assert response.status_code == 200
+
+    def test_non_existent_snap(self):
+        response = self.app.get('/non-existent-snap/')
+
+        assert response.status_code == 404
+        assert "Snap not found" in str(response.data)
+
 
 if __name__ == '__main__':
     unittest.main()
