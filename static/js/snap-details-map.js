57892663a6c780cca185009be3dcf9494bd4d0e3
Bartek Szopka <bartek.szopka+github@gmail.com>
2017-09-28 13:08:14 +0200

    Moved rendering map into separate function

--- static/js/snap-details-map.js
+++ static/js/snap-details-map.js
@@ -1,112 +1,119 @@
+/* global d3, topojson, SNAP_DETAILS_MAP_DATA */
+
 !(function(window, document, d3) {
-  var data = [];
+  function renderMap(el, data) {
 
-  // TODO: get data via params rather then global variable
-  var snapData = SNAP_DETAILS_MAP_DATA || {};
 
-  // TODO: move colors to CSS
-  var colors = [
-    '#cfe2f3',
-    '#9fc5e8',
-    '#6fa8dc',
-    '#3d85c6',
-    '#0b5394',
-    '#0b5394' // TODO: workaround for when value equals 1
-  ];
+    // TODO: get data via params rather then global variable
+    var snapData = data; // TODO SNAP_DETAILS_MAP_DATA || {};
 
-  var width = 990;
-  var height = width / 2;
+    // TODO: move colors to CSS
+    var colors = [
+      '#cfe2f3',
+      '#9fc5e8',
+      '#6fa8dc',
+      '#3d85c6',
+      '#0b5394',
+      '#0b5394' // TODO: workaround for when value equals 1
+    ];
 
-  var projection = d3.geoEquirectangular()
-    .scale((width + 1) / 2 / Math.PI)
-    .translate([(width / 2), (height / 2)])
-    .clipExtent([[0,0], [width, height-80]])
-    .precision(.1);
+    var width = 990;
+    var height = width / 2;
 
-  var path = d3.geoPath()
-    .projection(projection);
+    var projection = d3.geoEquirectangular()
+      .scale((width + 1) / 2 / Math.PI)
+      .translate([(width / 2), (height / 2)])
+      .clipExtent([[0,0], [width, height-80]])
+      .precision(.1);
 
-  var graticule = d3.geoGraticule();
+    var path = d3.geoPath()
+      .projection(projection);
 
-  var svg = d3.select("#canvas-svg").append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    var graticule = d3.geoGraticule();
 
-  svg.append("path")
-    .datum(graticule)
-    .attr("class", "graticule")
-    .attr("d", path);
-
-  // TODO: host data files ourselves?
-  // TODO: avoid getting tsv data file (mapping ids, names, etc)
-  d3.queue()
-    .defer(d3.json, "https://d3js.org/world-110m.v1.json")
-    .defer(d3.tsv, "https://d3js.org/world-110m.v1.tsv")
-    .await(ready);
-
-  function ready(error, world, worldData) {
-
-    var countries = topojson.feature(world, world.objects.countries).features;
+    var svg = d3.select(el).append("svg")
+      .attr("width", width)
+      .attr("height", height);
 
     svg.append("path")
       .datum(graticule)
-      .attr("class", "choropleth")
+      .attr("class", "graticule")
       .attr("d", path);
 
-    var g = svg.append("g");
-    var country = g.selectAll(".country").data(countries);
+    // TODO: host data files ourselves?
+    // TODO: avoid getting tsv data file (mapping ids, names, etc)
+    d3.queue()
+      .defer(d3.json, "https://d3js.org/world-110m.v1.json")
+      .defer(d3.tsv, "https://d3js.org/world-110m.v1.tsv")
+      .await(ready);
 
-    country.enter().insert("path")
-      .attr("class", "country")
-      .attr("d", path)
-      .attr("id", function(d, i) {
-        return d.id;
-      })
-      .attr("title", function(d) {
-        return d.properties.name;
-      })
-      .style("fill", function(d, index, test) {
-        var id = d.id;
-        var cData = worldData.filter(data => data.iso_n3 === id)[0];
-        var cSnapData = snapData[cData.iso_a2];
+    function ready(error, world, worldData) {
 
-        if (cSnapData) {
-          var colorId = ~~(cSnapData * 5);
-          var color = colors[colorId];
-          return color;
-        }
+      var countries = topojson.feature(world, world.objects.countries).features;
 
-        return "#f4f4f4";
-      })
-      .on("mouseenter", function(d) {
-        var pos = path.centroid(d);
+      svg.append("path")
+        .datum(graticule)
+        .attr("class", "choropleth")
+        .attr("d", path);
 
-        var id = d.id;
-        var cData = worldData.filter(data => data.iso_n3 === id)[0];
-        var cSnapData = snapData[cData.iso_a2];
+      var g = svg.append("g");
+      var country = g.selectAll(".country").data(countries);
 
-        if (cSnapData) {
-          // TODO: create 'tip' in JS, avoid getting it from HTML by id
-          document.getElementById('tip').style.top = (pos[1]) + 'px';
-          document.getElementById('tip').style.left = (pos[0]) + 'px';
-          document.getElementById("tip-message").innerHTML = cData.name;
-        }
-      })
-      .on("mouseout", function() {
-        document.getElementById('tip').style.top = -1000 + 'px';
-        document.getElementById('tip').style.left = -1000 + 'px';
-      });
+      country.enter().insert("path")
+        .attr("class", "country")
+        .attr("d", path)
+        .attr("id", function(d) {
+          return d.id;
+        })
+        .attr("title", function(d) {
+          return d.properties.name;
+        })
+        .style("fill", function(d) {
+          var id = d.id;
+          var cData = worldData.filter(data => data.iso_n3 === id)[0];
+          var cSnapData = snapData[cData.iso_a2];
 
-    g.append("path")
-      .datum(topojson.mesh(world, world.objects.countries, function(a, b) {
-        return a !== b;
-      }))
-      .attr("class", "boundary")
-      .attr("d", path);
+          if (cSnapData) {
+            var colorId = ~~(cSnapData * 5);
+            var color = colors[colorId];
+            return color;
+          }
 
-    svg.attr("height", height - 80);
-  };
+          return "#f4f4f4";
+        })
+        .on("mouseenter", function(d) {
+          var pos = path.centroid(d);
 
-  d3.select(self.frameElement).style("height", (height * 2.3 / 3) + "px");
+          var id = d.id;
+          var cData = worldData.filter(data => data.iso_n3 === id)[0];
+          var cSnapData = snapData[cData.iso_a2];
+
+          if (cSnapData) {
+            // TODO: create 'tip' in JS, avoid getting it from HTML by id
+            document.getElementById('tip').style.top = (pos[1]) + 'px';
+            document.getElementById('tip').style.left = (pos[0]) + 'px';
+            document.getElementById("tip-message").innerHTML = cData.name;
+          }
+        })
+        .on("mouseout", function() {
+          document.getElementById('tip').style.top = -1000 + 'px';
+          document.getElementById('tip').style.left = -1000 + 'px';
+        });
+
+      g.append("path")
+        .datum(topojson.mesh(world, world.objects.countries, function(a, b) {
+          return a !== b;
+        }))
+        .attr("class", "boundary")
+        .attr("d", path);
+
+      svg.attr("height", height - 80);
+    }
+
+    d3.select(self.frameElement).style("height", (height * 2.3 / 3) + "px");
+
+  }
+
+  window.renderMap = renderMap;
 
 })(window, document, d3);
