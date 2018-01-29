452a732762e1954805da572e84096d40b5b4b086
Robin Winslow <robin@robinwinslow.co.uk>
2018-01-29 18:31:01 +0000

    Fix and expand tests
    Fix tests to accommodate for URLs without slash, and add a load of tests
    for other URL endpoints not currently tested.

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@
 
 import app
 import unittest
+from urllib.parse import urlparse, urlunparse
 
 
 class WebAppTestCase(unittest.TestCase):
@@ -9,28 +10,105 @@ class WebAppTestCase(unittest.TestCase):
         self.app = app.app.test_client()
         self.app.testing = True
 
+    # Static pages
+    # ==
+
     def test_homepage(self):
-        response = self.app.get('/')
-        assert response.status_code == 200
-        assert "Ubuntu and Canonical are registered" in str(response.data)
+        self._check_basic_page('/')
 
-    def test_redirect_to_add_slash(self):
-        response = self.app.get('/canonical-livepatch')
-        location = response.headers.get('Location')
+    def test_storefront(self):
+        response = self._check_basic_page('/store')
+        assert 'type="search"' in str(response.data)
 
-        assert response.status_code in (301, 302)
-        assert location == "http://localhost/canonical-livepatch/"
+    def test_search(self):
+        redirect = self._get_response('/search')
+        assert redirect.status_code == 302
+        assert redirect.headers.get('Location') == "http://localhost/store"
+
+        response = self._check_basic_page('/search?q=livepatch')
+        assert 'type="search"' in str(response.data)
+        assert 'canonical-livepatch' in str(response.data)
+
+    def test_not_found(self):
+        response = self._get_response('/store/nothing-page')
+
+        assert response.status_code == 404
+        assert "Page not found" in str(response.data)
+
+    # Account pages
+    # ===
+
+    def test_account(self):
+        """
+        Naive check that the basic redirects to the authentication system
+        are working for the /account page
+        """
+
+        local_redirect = self._get_response('/account')
+        redirect_url = "http://localhost/login?next=/account"
+        assert local_redirect.status_code == 302
+        assert local_redirect.headers.get('Location') == redirect_url
+
+        ext_redirect = self._get_response('/login?next=/account')
+        ext2_redirect = self._get_response('/login?next=/account')
+        assert ext_redirect.status_code == 302
+        assert ext2_redirect.status_code == 302
+        assert 'login.ubuntu.com' in ext_redirect.headers.get('Location')
+        assert 'login.ubuntu.com' in ext2_redirect.headers.get('Location')
+
+    # Snap details pages
+    # ==
 
     def test_canonical_livepatch_snap(self):
-        response = self.app.get('/canonical-livepatch/')
+        response = self._check_basic_page('/canonical-livepatch')
+        assert "canonical-livepatch" in str(response.data)
 
-        assert response.status_code == 200
+    def test_lxd_snap(self):
+        response = self._check_basic_page('/lxd')
+        assert "LXD" in str(response.data)
 
     def test_non_existent_snap(self):
-        response = self.app.get('/non-existent-snap/')
+        response = self._get_response('/non-existent-snap')
 
         assert response.status_code == 404
         assert "Snap not found" in str(response.data)
+
+    # Helper methods
+    # ==
+
+    def _get_response(self, uri):
+        """
+        Given a basic app path (e.g. '/page'), check that any trailing
+        slashes are removed with a 302 redirect, and return the response
+        for the principal URL
+        """
+
+        # Check trailing slashes trigger redirect
+        parsed_uri = urlparse(uri)
+        slash_uri = urlunparse(parsed_uri._replace(path=parsed_uri.path + '/'))
+        redirect_response = self.app.get(slash_uri)
+        assert redirect_response.status_code == 302
+
+        url = "http://localhost" + uri
+        assert redirect_response.headers.get('Location') == url
+
+        return self.app.get(uri)
+
+    def _check_basic_page(self, uri):
+        """
+        Check that a URI returns an HTML page that will redirect to remove
+        slashes, returns a 200 and contains the standard footer text
+        """
+
+        if uri == '/':
+            response = self.app.get(uri)
+        else:
+            response = self._get_response(uri)
+
+        assert response.status_code == 200
+        assert "Ubuntu and Canonical are registered" in str(response.data)
+
+        return response
 
 
 if __name__ == '__main__':
