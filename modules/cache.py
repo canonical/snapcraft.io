d4f11160ea12b512ef51b968212809a6a1c7fda5
Thomas Bille <thomas.bille@canonical.com>
2018-02-23 15:22:19 +0000

    Remove caching system

--- modules/cache.py
+++ modules/cache.py
@@ -1,27 +1,4 @@
-import datetime
 import requests
-import requests_cache
-from requests.adapters import HTTPAdapter
-from requests.exceptions import RequestException
-from requests.packages.urllib3.util.retry import Retry
-
-# Setup session to retry requests 5 times
-uncached_session = requests.Session()
-retries = Retry(
-    total=5,
-    backoff_factor=0.1,
-    status_forcelist=[500, 502, 503, 504]
-)
-uncached_session.mount(
-    'https://api.snapcraft.io',
-    HTTPAdapter(max_retries=retries)
-)
-
-# The cache expires after 5 seconds
-cached_session = requests_cache.CachedSession(expire_after=5)
-
-# Requests should timeout after 2 seconds in total
-request_timeout = 2
 
 
 def get(
@@ -33,50 +10,22 @@ def get(
         files=None
 ):
     """
+    WARNING: The cache is disabled due to a bug on our caching system.
+
     Retrieve the response from the requests cache.
     If the cache has expired then it will attempt to update the cache.
     If it gets an error, it will use the cached response, if it exists.
     """
 
-    request_error = False
-
     if method is None:
         method = "POST" if json else "GET"
 
-    request = cached_session.prepare_request(
-        requests.Request(
-            method=method,
-            url=url,
-            headers=headers,
-            json=json,
-            files=files,
-            data=data
-        )
+    return requests.request(
+        method=method,
+        url=url,
+        headers=headers,
+        json=json,
+        files=files,
+        data=data,
+        timeout=2
     )
-
-    cache_key = cached_session.cache.create_key(request)
-    response, timestamp = cached_session.cache.get_response_and_time(
-        cache_key
-    )
-
-    if response:
-        age = datetime.datetime.utcnow() - timestamp
-
-        if age > cached_session._cache_expire_after:
-            try:
-                new_response = uncached_session.send(
-                    request,
-                    timeout=request_timeout
-                )
-                if response.status_code >= 500:
-                    new_response.raise_for_status()
-            except RequestException:
-                request_error = True
-            else:
-                response = new_response
-    else:
-        response = cached_session.send(request)
-
-    response.old_data_from_error = request_error
-
-    return response
