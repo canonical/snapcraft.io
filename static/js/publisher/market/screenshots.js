6b2733e96963fbdb84abf7f98c76a81b1fcadad8
Bartek Szopka <bartek.szopka@canonical.com>
2018-03-29 16:41:32 +0200

    Removes SVG from accepted screenshot formats

--- static/js/publisher/market/screenshots.js
+++ static/js/publisher/market/screenshots.js
@@ -172,7 +172,7 @@ function initSnapScreenshotsEdit(screenshotsToolbarElId, screenshotsWrapperElId,
 
       const input = document.createElement('input');
       input.type = "file";
-      input.accept = "image/*";
+      input.accept = "image/x-png,image/gif,image/jpeg";
       input.name="screenshots";
       input.hidden = "hidden";
 
