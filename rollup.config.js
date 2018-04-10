0349ffb79e7eb3a58089fb8d6a2d7f42b011392d
Bartek Szopka <bartek.szopka@canonical.com>
2018-04-10 15:41:34 +0200

    Add base script to the layout to toggle navigation dropdown

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,23 @@ import babel from 'rollup-plugin-babel';
 
 export default [
   {
+    input: 'static/js/base/base.js',
+    plugins: [
+      nodeResolve({
+        jsnext: true
+      }),
+      babel({
+        exclude: 'node_modules/**'
+      })
+    ],
+    output: {
+      file: 'static/js/dist/base.js',
+      format: 'iife',
+      name: 'snapcraft.base',
+      sourcemap: true
+    }
+  },
+  {
     input: 'static/js/publisher/publisher.js',
     plugins: [
       nodeResolve({
--- static/js/publisher/publisher.js
+++ static/js/publisher/publisher.js
@@ -1,4 +1,3 @@
-import '../polyfills';
 import metrics from './metrics/metrics';
 import { selector } from './metrics/filters';
 import * as market from './market/market';
