3ff0e15299b861bd4927883c9e7855572218f57a
Thomas Bille <thomas.bille@canonical.com>
2018-04-23 14:43:52 +0100

    Handle errors POST agreement errors

--- tests/endpoint_testing.py
+++ tests/endpoint_testing.py
@@ -279,3 +279,139 @@ class BaseTestCases:
             self.assertEqual(
                 'http://localhost/account/username',
                 response.location)
+
+    class EndpointPostLoggedIn(BaseAppTesting):
+        def setUp(self, snap_name, api_url, endpoint_url, data):
+            super().setUp(snap_name, api_url, endpoint_url)
+            self.authorization = self._log_in(self.client)
+            self.data = data
+
+        @responses.activate
+        def test_timeout(self):
+            responses.add(
+                responses.POST, self.api_url,
+                body=requests.exceptions.Timeout(), status=504)
+
+            response = self.client.post(self.endpoint_url, data=self.data)
+
+            self.assertEqual(1, len(responses.calls))
+            called = responses.calls[0]
+            self.assertEqual(
+                self.api_url,
+                called.request.url)
+            self.assertEqual(
+                self.authorization,
+                called.request.headers.get('Authorization'))
+
+            assert response.status_code == 504
+
+        @responses.activate
+        def test_connection_error(self):
+            responses.add(
+                responses.POST, self.api_url,
+                body=requests.exceptions.ConnectionError(), status=500)
+
+            response = self.client.post(self.endpoint_url, data=self.data)
+
+            self.assertEqual(1, len(responses.calls))
+            called = responses.calls[0]
+            self.assertEqual(
+                self.api_url,
+                called.request.url)
+            self.assertEqual(
+                self.authorization,
+                called.request.headers.get('Authorization'))
+
+            assert response.status_code == 502
+
+        @responses.activate
+        def test_broken_json(self):
+            # To test this I return no json from the server, this makes the
+            # call to the function response.json() raise a ValueError exception
+            responses.add(
+                responses.POST, self.api_url,
+                status=500)
+
+            response = self.client.post(self.endpoint_url, data=self.data)
+
+            self.assertEqual(1, len(responses.calls))
+            called = responses.calls[0]
+            self.assertEqual(
+                self.api_url,
+                called.request.url)
+            self.assertEqual(
+                self.authorization,
+                called.request.headers.get('Authorization'))
+
+            assert response.status_code == 502
+
+        @responses.activate
+        def test_unknown_error(self):
+            responses.add(
+                responses.POST, self.api_url,
+                json={}, status=500)
+
+            response = self.client.post(self.endpoint_url, data=self.data)
+
+            self.assertEqual(1, len(responses.calls))
+            called = responses.calls[0]
+            self.assertEqual(
+                self.api_url,
+                called.request.url)
+            self.assertEqual(
+                self.authorization,
+                called.request.headers.get('Authorization'))
+
+            assert response.status_code == 502
+
+        @responses.activate
+        def test_error_4xx(self):
+            payload = {
+                'error_list': []
+            }
+            responses.add(
+                responses.POST, self.api_url,
+                json=payload, status=400)
+
+            response = self.client.post(self.endpoint_url, data=self.data)
+
+            self.assertEqual(1, len(responses.calls))
+            called = responses.calls[0]
+            self.assertEqual(
+                self.api_url,
+                called.request.url)
+            self.assertEqual(
+                self.authorization,
+                called.request.headers.get('Authorization'))
+
+            assert response.status_code == 502
+
+        @responses.activate
+        def test_expired_macaroon(self):
+            responses.add(
+                responses.POST, self.api_url,
+                json={}, status=500,
+                headers={'WWW-Authenticate': 'Macaroon needs_refresh=1'})
+            responses.add(
+                responses.POST,
+                'https://login.ubuntu.com/api/v2/tokens/refresh',
+                json={'discharge_macaroon': 'macaroon'}, status=200)
+
+            response = self.client.post(self.endpoint_url, data=self.data)
+
+            self.assertEqual(2, len(responses.calls))
+
+            called = responses.calls[0]
+            self.assertEqual(
+                self.api_url,
+                called.request.url)
+            self.assertEqual(
+                self.authorization,
+                called.request.headers.get('Authorization'))
+            called = responses.calls[1]
+            self.assertEqual(
+                'https://login.ubuntu.com/api/v2/tokens/refresh',
+                called.request.url)
+
+            assert response.status_code == 302
+            assert response.location == self._get_location()
