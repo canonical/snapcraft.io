42b5dcbda2166303f01eec2c862030411df68a1a
Bartek Szopka <bartek.szopka@canonical.com>
2018-05-11 10:05:51 +0200

    Add install tab to channel map overlay

--- static/js/public/snap-details/channelMap.js
+++ static/js/public/snap-details/channelMap.js
@@ -101,15 +101,46 @@ function setArchitecture(arch, packageName, channelMapData) {
   setTrack(arch, track, packageName, channelMap);
 }
 
+function selectTab(tabEl, tabsWrapperEl) {
+  const selected = tabEl.getAttribute('aria-selected');
+  if (!selected) {
+    tabsWrapperEl.querySelector('.p-channel-map__tab.is-open').classList.remove('is-open');
+    tabsWrapperEl.querySelector('.p-tabs__link[aria-selected]').removeAttribute('aria-selected');
+
+    document.getElementById(tabEl.getAttribute('aria-controls')).classList.add('is-open');
+    tabEl.setAttribute('aria-selected', "true");
+  }
+}
+
+function initTabs(el) {
+  el.addEventListener('click', (event) => {
+    const target = event.target.closest('.p-tabs__link');
+
+    if (target) {
+      event.preventDefault();
+      selectTab(target, el);
+    }
+  });
+}
+
 export default function initChannelMap(el, packageName, channelMapData) {
+
   const channelMapEl = document.querySelector(el);
   const channelOverlayEl = document.querySelector('.p-channel-map-overlay');
+
+  const defaultTab = channelMapEl.querySelector('.p-tabs__link[aria-controls=channel-map-tab-versions]');
+  initTabs(channelMapEl);
+
   let closeTimeout;
 
   // init open/hide buttons
   const openChannelMap = () => {
     // clear hiding animation if it's still running
     clearTimeout(closeTimeout);
+
+    // select default tab before opening
+    selectTab(defaultTab, channelMapEl);
+
     // make sure overlay is displayed before CSS transitions are triggered
     channelOverlayEl.style.display = 'block';
     setTimeout(() => channelMapEl.classList.remove('is-closed'), 10);
