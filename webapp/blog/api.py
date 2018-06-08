520f4afe87c2235d4400893cab2e85650c9c1da5
Thomas Bille <thomas.bille@canonical.com>
2018-06-08 15:36:37 +0100

    Add article endpoint

--- webapp/blog/api.py
+++ webapp/blog/api.py
@@ -4,7 +4,19 @@ API_URL = 'https://admin.insights.ubuntu.com/wp-json/wp/v2'
 
 
 def get_articles():
-    url = API_URL + '/posts?tag=snappy'
+    url = ''.join([API_URL, '/posts?tags=2080'])
     response = cache.get(url, {})
 
+    return response.json()
+
+
+def get_posts(slug):
+    url = ''.join([
+        API_URL,
+        '/posts?slug=',
+        slug,
+        '&tags=2080',
+    ])
+
+    response = cache.get(url, {})
     return response.json()
