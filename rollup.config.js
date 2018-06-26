a21b505b74d00a6ccd0918f5b43282f6d46bdadc
Bartek Szopka <bartek.szopka@canonical.com>
2018-06-26 16:32:49 +0200

    React test setup with vanilla-framework-react

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
 
 export default [
   {
@@ -52,4 +54,45 @@ export default [
       name: 'snapcraft.public',
       sourcemap: true
     }
-  }];
+  },
+  {
+    input: 'static/js/publisher/release.js',
+    plugins: [
+      nodeResolve({
+        jsnext: true
+      }),
+      babel({
+        exclude: 'node_modules/**'
+      }),
+      // https://github.com/rollup/rollup-plugin-commonjs/issues/200
+      commonjs({
+        exclude: 'node_modules/process-es6/**',
+        include: [
+          'node_modules/create-react-class/**',
+          'node_modules/fbjs/**',
+          'node_modules/object-assign/**',
+          'node_modules/react/**',
+          'node_modules/react-dom/**',
+          'node_modules/prop-types/**',
+          'node_modules/vanilla-framework-react/**'
+        ],
+        namedExports: {
+          'node_modules/react/index.js': ['Children', 'Component', 'PropTypes', 'createElement'],
+          'node_modules/react-dom/index.js': ['render'],
+          'node_modules/vanilla-framework-react/build/index.js': ['Button'],
+        }
+      }),
+      // needed for React
+      // https://github.com/rollup/rollup/issues/487
+      replace({
+        'process.env.NODE_ENV': JSON.stringify( 'production' )
+      })
+    ],
+    output: {
+      file: 'static/js/dist/release.js',
+      format: 'iife',
+      name: 'snapcraft.release',
+      sourcemap: true
+    }
+  },
+];
