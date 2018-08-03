b708f9059ee1b24a44a0ec231ded23fa1cc99628
Lukewh <luke@lukewh.com>
2018-08-03 16:13:15 +0100

    Add basic logout tests

---
+++ tests/publisher/tests_account_logout.py
@@ -0,0 +1,57 @@
+import unittest
+import responses
+
+from tests.publisher.endpoint_testing import BaseTestCases
+
+# Make sure tests fail on stray responses.
+responses.mock.assert_all_requests_are_fired = True
+
+
+class LogoutRedirects(BaseTestCases.BaseAppTesting):
+    def setUp(self):
+        endpoint_url = '/logout'
+
+        super().setUp(
+            snap_name=None,
+            endpoint_url=endpoint_url,
+            api_url=None,
+        )
+
+    @responses.activate
+    def test_logout(self):
+        response = self.client.get(self.endpoint_url)
+
+        self.assertEqual(
+            302,
+            response.status_code
+        )
+
+        self.assertEqual(
+            'https://build.snapcraft.io/auth/logout',
+            response.location
+        )
+
+    def test_no_redirect_logout(self):
+        bad_param_response = self.client.get(
+            self.endpoint_url + '?no_redirect=false'
+        )
+
+        self.assertEqual(
+            302,
+            bad_param_response.status_code
+        )
+        self.assertEqual(
+            'https://build.snapcraft.io/auth/logout',
+            bad_param_response.location
+        )
+
+        response = self.client.get(
+            self.endpoint_url + '?no_redirect=true'
+        )
+
+        self.assertEqual(302, response.status_code)
+        self.assertEqual('http://localhost/', response.location)
+
+
+if __name__ == '__main__':
+    unittest.main()
