d64b0e557649986e192d6a661768eabaf647f99d
Bartek Szopka <bartek.szopka@canonical.com>
2018-09-06 13:38:46 +0200

    Update channel map after release response

--- static/js/publisher/release/releases.js
+++ static/js/publisher/release/releases.js
@@ -15,6 +15,7 @@ export default class Releases extends Component {
     this.state = {
       error: null,
       isLoading: false,
+      releasedChannels: this.props.releasedChannels
     };
   }
 
@@ -22,7 +23,7 @@ export default class Releases extends Component {
     const { csrfToken } = this.props.options;
 
     console.log('fetch', revision, channels );
-    return fetch(`/account/snaps/${this.props.snapName}/release/snap-release`, {
+    return fetch(`/account/snaps/${this.props.snapName}/release`, {
       method: "POST",
       mode: "cors",
       cache: "no-cache",
@@ -33,17 +34,62 @@ export default class Releases extends Component {
       },
       redirect: "follow",
       referrer: "no-referrer",
-      body: JSON.stringify({ revision, channels }),
+      body: JSON.stringify({ revision, channels, name: this.props.snapName }),
     })
       .then(response => response.json());
   }
 
-  handleReleaseResponse(json) {
+  handleReleaseResponse(json, release) {
     console.log("response", json);
 
     // TODO: update state/channel map after each successful release
     if (json.success) {
       console.log("update channel map", json.channel_map);
+
+      this.setState(state => {
+        const releasedChannels = state.releasedChannels;
+
+        // update releasedChannels based on channel map from the response
+        json.channel_map.forEach(map => {
+          console.log('map', map)
+          // TODO:
+          // - close channels when there is no release?
+          // - ignore revisions other then the one just released?
+          // - get revision data from revisionsMap?
+          if (map.revision) {
+
+            let revision;
+            if (map.revision === release.id) {
+              revision = release.revision;
+            } else {
+              revision = {
+                revision: map.revision,
+                version: map.version,
+                architectures: release.revision.architectures
+              };
+            }
+
+            let channel = map.channel;
+            if (channel.indexOf('/') === -1) {
+              channel = `latest/${channel}`;
+            }
+
+            if (!releasedChannels[channel]) {
+              releasedChannels[channel] = {};
+            }
+
+
+            revision.architectures.forEach(arch => {
+              releasedChannels[channel][arch] = revision;
+            });
+
+          }
+        });
+
+        return {
+          releasedChannels
+        };
+      });
     } else {
       console.log("error")
     }
@@ -57,10 +103,9 @@ export default class Releases extends Component {
       return queue = queue
         .then(() => {
           return this
-            .fetchRelease(release.revision, release.channels)
-            .then(this.handleReleaseResponse.bind(this));
+            .fetchRelease(release.id, release.channels)
+            .then(json => this.handleReleaseResponse(json, release));
         });
-      //.then(json => this.handleReleaseResponse(json));
     });
     return queue;
   }
@@ -69,18 +114,20 @@ export default class Releases extends Component {
     console.log("revisions to release", revisionsToRelease);
 
     const releases = Object.keys(revisionsToRelease).map(id => {
-      return { revision: id, channels: revisionsToRelease[id].channels };
+      return { id, revision: revisionsToRelease[id].revision, channels: revisionsToRelease[id].channels };
     });
 
     console.log('fetchReleases', releases);
     this.setState({ isLoading: true });
     this.fetchReleases(releases)
       .then(() => console.log('all done'))
+      // TODO: reset pending releases
       .then(() => this.setState({ isLoading: false }));
   }
 
   render() {
-    const { releasedChannels, archs, tracks, options } = this.props;
+    const { archs, tracks, options } = this.props;
+    const { releasedChannels } = this.state;
 
     return (
       <RevisionsTable
