5094cbdf291ac981032867868134ad4d205658da
Lukewh <luke@lukewh.com>
2018-09-14 15:58:03 +0100

    Add settings.js and added multiselect disable styling

--- static/js/publisher/publisher.js
+++ static/js/publisher/publisher.js
@@ -2,5 +2,8 @@ import metrics from './metrics/metrics';
 import { selector } from './metrics/filters';
 import * as market from './market/market';
 import { initMultiselect } from "./form/multiselect";
+import { enableInput, changeHandler } from "./settings";
 
-export { metrics, selector, market, initMultiselect };
+const settings = { enableInput, changeHandler };
+
+export { metrics, selector, market, initMultiselect, settings };
