06af1a2a9d5033bc626df99114081ab51a795219
Bartek Szopka <bartek.szopka@canonical.com>
2018-09-20 18:15:22 +0200

    Remove promote buttons from unreleased revisions

--- static/js/publisher/release/revisionsList.js
+++ static/js/publisher/release/revisionsList.js
@@ -2,43 +2,10 @@ import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 
-import PromoteButton from './promoteButton';
-
-const RISKS = ['stable', 'candidate', 'beta', 'edge'];
-
-// TODO:
-// - don't allow releasing multiple revisions into same arch/channel
-
 export default class RevisionsList extends Component {
-  isRevisionReleased(revision, channel) {
-    const releasedChannels = this.props.getNextReleasedChannels();
-
-    const channelReleases = releasedChannels[channel];
-    if (channelReleases) {
-      return Object.keys(channelReleases).some((arch) => {
-        return channelReleases[arch].revision === revision.revision;
-      });
-    }
-    return false;
-  }
-
   renderRows(revisions) {
     return revisions.map((revision) => {
       const uploadDate = moment(revision.created_at);
-      let canBeReleased = true;
-      let hasPendingRelease = false;
-
-      const targetRisks = RISKS.filter((risk) => {
-        return !this.isRevisionReleased(revision, `${this.props.currentTrack}/${risk}`);
-      });
-
-      if (targetRisks.length === 0) {
-        canBeReleased = false;
-      }
-
-      if (!canBeReleased && this.props.pendingReleases[revision.revision]) {
-        hasPendingRelease = true;
-      }
 
       return (
         <tr key={revision.revision}>
@@ -54,42 +21,10 @@ export default class RevisionsList extends Component {
               </span>
             </span>
           </td>
-
-          <td className="u-align--right">
-            { canBeReleased &&
-              <PromoteButton
-                track={this.props.currentTrack}
-                targetRisks={targetRisks}
-                promoteToChannel={this.onPromoteToChannel.bind(this, revision)}
-              />
-            }
-            { hasPendingRelease &&
-              <button className="p-icon-button p-tooltip p-tooltip--btm-center" onClick={this.undoClick.bind(this, revision)}>
-                &#x2715;
-                <span className="p-tooltip__message">Cancel promoting this revision</span>
-              </button>
-            }
-          </td>
         </tr>
       );
     });
   }
-
-  onPromoteToChannel(revision, targetChannel) {
-    this.props.promoteRevision(revision, targetChannel);
-  }
-
-  // TODO:
-  // - undo doesn't work if release is in channel different then edge
-  // - do it nicer?
-  undoClick(revision) {
-    this.props.undoRelease(revision, `${this.props.currentTrack}/edge`);
-    this.props.undoRelease(revision, `${this.props.currentTrack}/beta`);
-    this.props.undoRelease(revision, `${this.props.currentTrack}/candidate`);
-    this.props.undoRelease(revision, `${this.props.currentTrack}/stable`);
-  }
-
-
 
   render() {
     return (
@@ -103,7 +38,6 @@ export default class RevisionsList extends Component {
               <th width="12%" scope="col">Architecture</th>
               <th width="30%" scope="col">Channels</th>
               <th width="15%" scope="col" className="u-align--right">Submission date</th>
-              <th width="10%" scope="col" className="u-align--right">Actions</th>
             </tr>
           </thead>
 
@@ -117,10 +51,5 @@ export default class RevisionsList extends Component {
 }
 
 RevisionsList.propTypes = {
-  currentTrack: PropTypes.string.isRequired,
-  pendingReleases: PropTypes.object.isRequired,
   revisions: PropTypes.object.isRequired,
-  getNextReleasedChannels: PropTypes.func.isRequired,
-  promoteRevision: PropTypes.func.isRequired,
-  undoRelease: PropTypes.func.isRequired
 };
