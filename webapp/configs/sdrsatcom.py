06d2b7159fab6eba4aaa45da0946e7e550319c09
Thomas Bille <thomas.bille@canonical.com>
2018-10-04 08:58:58 +0100

    Add sdratcom brandstore

---
+++ webapp/configs/sdrsatcom.py
@@ -0,0 +1,25 @@
+WEBAPP_CONFIG = {
+    "LAYOUT": "_layout-brandstore.html",
+    "STORE_NAME": "sdrsatcom",
+    "STORE_BRAND": {
+        "LOGO": "https://assets.ubuntu.com/v1/7a1fe153-ESA.png",
+        "COLOUR": "#fff",
+    },
+    "STORE_LINKS": [
+        {
+            "title": "Documentation",
+            "href": "https://wiki.myriadrf.org/SDR_Satcom_App_Store",
+        },
+        {
+            "title": "SDR Satcom",
+            "href": "https://wiki.myriadrf.org/SDR_Satcom",
+        },
+    ],
+    "CUSTOM_STYLES": ".p-navigation__link a,.p-navigation__link a:visited,\
+     .p-navigation__link a:focus {color: #000;}\
+     .p-navigation__link a:hover {color: #002568; background: transparent;}",
+    "STORE_INSTALL_INSTRUCTIONS": True,
+    "FOOTER_TEXT": "Copyright 2018  Canonical Ltd.<br/>\
+      Ubuntu and Canonical are registered trademarks of Canonical Ltd.",
+    "STORE_QUERY": "SDR_Satcom",
+}
