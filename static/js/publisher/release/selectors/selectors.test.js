2571fc7fd9edb73aa89baa4ef5582d6e60135e2a
Bartek Szopka <bartek.szopka@canonical.com>
2018-12-07 12:47:11 +0100

    More tests for getPendingChannelMap selector

--- static/js/publisher/release/selectors/selectors.test.js
+++ static/js/publisher/release/selectors/selectors.test.js
@@ -259,4 +259,58 @@ describe("getPendingChannelMap", () => {
       );
     });
   });
+
+  describe("when there are pending releases to other channels", () => {
+    const stateWithPendingReleases = {
+      channelMap: {
+        "test/edge": {
+          test64: { revision: 1 }
+        }
+      },
+      pendingReleases: {
+        2: {
+          revision: { revision: 2, architectures: ["test64"] },
+          channels: ["latest/stable"]
+        }
+      }
+    };
+
+    it("should return channel map with pending revisions added", () => {
+      expect(getPendingChannelMap(stateWithPendingReleases)).toEqual({
+        ...stateWithPendingReleases.channelMap,
+        "latest/stable": {
+          test64: {
+            ...stateWithPendingReleases.pendingReleases[2].revision
+          }
+        }
+      });
+    });
+  });
+
+  describe("when there are pending releases overriding existing releases", () => {
+    const stateWithPendingReleases = {
+      channelMap: {
+        "test/edge": {
+          test64: { revision: 1 }
+        }
+      },
+      pendingReleases: {
+        2: {
+          revision: { revision: 2, architectures: ["test64"] },
+          channels: ["test/edge"]
+        }
+      }
+    };
+
+    it("should return channel map with pending revisions", () => {
+      expect(getPendingChannelMap(stateWithPendingReleases)).toEqual({
+        ...stateWithPendingReleases.channelMap,
+        "test/edge": {
+          test64: {
+            ...stateWithPendingReleases.pendingReleases[2].revision
+          }
+        }
+      });
+    });
+  });
 });
