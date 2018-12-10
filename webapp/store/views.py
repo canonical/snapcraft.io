d32acb8db16bf3ffcb2f5852378dcf4a82a0d7ce
Thomas Bille <thomas.bille@canonical.com>
2018-12-10 11:49:20 +0000

    Add feature snap call to store page

--- webapp/store/views.py
+++ webapp/store/views.py
@@ -66,10 +66,18 @@ def store_blueprint(store_query=None, testing=False):
 
         categories = logic.get_categories(categories_results)
 
+        try:
+            featured_snaps_results = api.get_featured_snaps()
+        except ApiError as api_error:
+            featured_snaps_results = []
+
+        featured_snaps = logic.get_searched_snaps(featured_snaps_results)
+
         return (
             flask.render_template(
                 "store/store.html",
                 categories=categories,
+                featured_snaps=featured_snaps,
                 error_info=error_info,
             ),
             status_code,
