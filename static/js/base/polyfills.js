9ebbb8e161a4d3314fca709501e88ce16b2354e5
Lukewh <luke@lukewh.com>
2018-12-18 20:48:54 +1100

    Polyfills e.matches fix

--- static/js/base/polyfills.js
+++ static/js/base/polyfills.js
@@ -182,7 +182,7 @@ if (!Element.prototype.closest) {
     var el = this;
     if (!document.documentElement.contains(el)) return null;
     do {
-      if (el.matches(s)) return el;
+      if (el && el.matches && el.matches(s)) return el;
       el = el.parentElement || el.parentNode;
     } while (el !== null && el.nodeType === 1);
     return null;
