f3e8ab36100b880e138c042c5e3940cc7bad968e
Bartek Szopka <bartek.szopka@canonical.com>
2018-12-21 11:14:59 +0100

    Automatically select revisions when on available revisions select change

--- static/js/publisher/release/actions/availableRevisionsSelect.test.js
+++ static/js/publisher/release/actions/availableRevisionsSelect.test.js
@@ -1,7 +1,15 @@
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+
+const mockStore = configureMockStore([thunk]);
+
 import {
   SET_AVAILABLE_REVISIONS_SELECT,
-  setAvailableRevisionsSelect
+  setAvailableRevisionsSelect,
+  selectAvailableRevisions
 } from "./availableRevisionsSelect";
+import { selectRevision } from "./channelMap";
+import { AVAILABLE_REVISIONS_SELECT_UNRELEASED } from "../constants";
 
 describe("availableRevisionsSelect actions", () => {
   describe("setAvailableRevisionsSelect", () => {
@@ -15,6 +23,44 @@ describe("availableRevisionsSelect actions", () => {
 
     it("should supply a payload with a value to set", () => {
       expect(setAvailableRevisionsSelect(value).payload.value).toEqual(value);
+    });
+  });
+
+  describe("selectAvailableRevisions", () => {
+    const value = "test";
+    const revisions = {
+      1: { revision: 1, architectures: ["test64", "amd42"] },
+      2: { revision: 2, architectures: ["test64"] },
+      3: { revision: 3, architectures: ["abc42"], channels: ["test/edge"] }
+    };
+    let store;
+
+    beforeEach(() => {
+      store = mockStore({
+        // mock store state is not modified by actions
+        // so this is the value we expect after the actions are dispatched
+        availableRevisionsSelect: AVAILABLE_REVISIONS_SELECT_UNRELEASED,
+        revisions
+      });
+
+      store.dispatch(selectAvailableRevisions(value));
+    });
+
+    it("should dispatch SET_AVAILABLE_REVISIONS_SELECT action", () => {
+      expect(store.getActions()).toContainEqual(
+        setAvailableRevisionsSelect(value)
+      );
+    });
+
+    it("should dispatch SELECT_REVISION action for latest revision from every selected architecture", () => {
+      expect(store.getActions()).toContainEqual(selectRevision(revisions[1]));
+      expect(store.getActions()).toContainEqual(selectRevision(revisions[2]));
+    });
+
+    it("should not dispatch SELECT_REVISION action for not selected revisions", () => {
+      expect(store.getActions()).not.toContainEqual(
+        selectRevision(revisions[3])
+      );
     });
   });
 });
