960a47f1152f94363943d1cc83a9af0e7f29045b
Lukewh <luke@lukewh.com>
2019-01-17 15:11:56 +0000

    Improve based on UX feedback

--- static/js/publisher/market/markdown.js
+++ static/js/publisher/market/markdown.js
@@ -5,5 +5,10 @@ export default function() {
   markdownLink.addEventListener("click", e => {
     e.preventDefault();
     markdownInfo.classList.toggle("u-hide");
+    if (markdownInfo.classList.contains("u-hide")) {
+      markdownLink.innerHTML = markdownLink.innerHTML.replace("Hide", "Show");
+    } else {
+      markdownLink.innerHTML = markdownLink.innerHTML.replace("Show", "Hide");
+    }
   });
 }
