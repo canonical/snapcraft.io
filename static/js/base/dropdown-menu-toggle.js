ec2f65772924511d6d36fd95b0cae7c8bf8ff01b
Bartek Szopka <bartek.szopka@canonical.com>
2019-02-01 16:23:32 +0100

    Close nav dropdown on outside click

--- static/js/base/dropdown-menu-toggle.js
+++ static/js/base/dropdown-menu-toggle.js
@@ -11,26 +11,41 @@
    * </div>
    *
    */
-
   function toggle(toggleCtrl) {
     var toggles = document.querySelectorAll(toggleCtrl);
+
     for (var i = 0, ii = toggles.length; i < ii; i += 1) {
       toggles[i].addEventListener("click", function(evt) {
         evt.preventDefault();
-        var targetMenu = document.getElementById(
-          this.getAttribute("aria-controls")
-        );
+        evt.stopPropagation();
 
-        if (targetMenu) {
-          if (targetMenu.getAttribute("aria-hidden") === "true") {
-            this.setAttribute("aria-expanded", true);
-            targetMenu.setAttribute("aria-hidden", false);
-          } else {
-            this.setAttribute("aria-expanded", false);
-            targetMenu.setAttribute("aria-hidden", true);
-          }
-        }
+        toggleDropdown(this, this.getAttribute("aria-expanded") === "false");
       });
+    }
+
+    document.body.addEventListener("click", function() {
+      for (var i = 0, ii = toggles.length; i < ii; i += 1) {
+        toggles[i].setAttribute("aria-expanded", false);
+
+        toggleDropdown(toggles[i], false);
+      }
+    });
+  }
+
+  // helper function to toggle dropdown
+  function toggleDropdown(toggle, show) {
+    var targetMenu = document.getElementById(
+      toggle.getAttribute("aria-controls")
+    );
+
+    if (targetMenu) {
+      if (show) {
+        toggle.setAttribute("aria-expanded", true);
+        targetMenu.setAttribute("aria-hidden", false);
+      } else {
+        toggle.setAttribute("aria-expanded", false);
+        targetMenu.setAttribute("aria-hidden", true);
+      }
     }
   }
 
