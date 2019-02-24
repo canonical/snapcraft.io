5bdb879c35ce2c299f6e3b208bc7c75b0c5451ca
Thomas Bille <thomas.bille@canonical.com>
2019-02-24 20:52:08 +0000

    Ugrade to talisker 0.14.2

--- webapp/api/requests.py
+++ webapp/api/requests.py
@@ -1,30 +1,13 @@
 import os
-from urllib.parse import urlparse
 
 import requests
 
-import prometheus_client
 import requests_cache
 from pybreaker import CircuitBreaker, CircuitBreakerError
 from webapp.api.exceptions import (
     ApiCircuitBreaker,
     ApiConnectionError,
     ApiTimeoutError,
-)
-
-timeout_counter = prometheus_client.Counter(
-    "feed_timeouts", "A counter of timed out requests", ["domain"]
-)
-connection_failed_counter = prometheus_client.Counter(
-    "feed_connection_failures",
-    "A counter of requests which failed to connect",
-    ["domain"],
-)
-latency_histogram = prometheus_client.Histogram(
-    "feed_latency_seconds",
-    "Feed requests retrieved",
-    ["domain", "code"],
-    buckets=[0.25, 0.5, 0.75, 1, 2, 3],
 )
 
 
@@ -61,21 +44,15 @@ class BaseSession:
         self.api_breaker = CircuitBreaker(fail_max=5, reset_timeout=60)
 
     def request(self, method, url, **kwargs):
-        domain = urlparse(url).netloc
-
         try:
             request = self.api_breaker.call(
                 super().request, method=method, url=url, **kwargs
             )
         except requests.exceptions.Timeout:
-            timeout_counter.labels(domain=domain).inc()
-
             raise ApiTimeoutError(
                 "The request to {} took too long".format(url)
             )
         except requests.exceptions.ConnectionError:
-            connection_failed_counter.labels(domain=domain).inc()
-
             raise ApiConnectionError(
                 "Failed to establish connection to {}.".format(url)
             )
@@ -83,10 +60,6 @@ class BaseSession:
             raise ApiCircuitBreaker(
                 "Requests are closed because of too many failures".format(url)
             )
-
-        latency_histogram.labels(
-            domain=domain, code=request.status_code
-        ).observe(request.elapsed.total_seconds())
 
         return request
 
