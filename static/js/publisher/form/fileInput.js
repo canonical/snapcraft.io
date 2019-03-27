91ccbe99ff30e00fb9ab8fdfde12c2dacbc16e71
Lukewh <luke@lukewh.com>
2019-03-27 15:59:40 +0000

    Drag to upload files

--- static/js/publisher/form/fileInput.js
+++ static/js/publisher/form/fileInput.js
@@ -11,13 +11,25 @@ class FileInput extends React.Component {
     this.fileClickHandler = this.fileClickHandler.bind(this);
     this.fileChangedHandler = this.fileChangedHandler.bind(this);
     this.keyboardEventHandler = this.keyboardEventHandler.bind(this);
+    this.dragOverHandler = this.dragOverHandler.bind(this);
+    this.dropHandler = this.dropHandler.bind(this);
+    this.dragExitHandler = this.dragExitHandler.bind(this);
+    this.dragLeaveHandler = this.dragLeaveHandler.bind(this);
+
+    this.cancelDragging = this.cancelDragging.bind(this);
+
+    this.state = {
+      isDragging: false,
+      canDrop: false,
+      overLimit: false
+    };
   }
 
   componentDidMount() {
     const { restrictions, inputName } = this.props;
 
-    // Handle input creation outside of the react lifecycle so as to avoid
-    // rerendering and side-effects. We need the input to maintain a consistent
+    // Handle input creation outside of the react lifecycle to avoid
+    // re-rendering and side-effects. We need the input to maintain a consistent
     // DOM state throughout the component lifecycle.
     this.input = document.createElement("input");
     this.input.type = "file";
@@ -29,6 +41,29 @@ class FileInput extends React.Component {
     this.input.addEventListener("change", this.fileChangedHandler);
 
     this.holder.appendChild(this.input);
+
+    document.addEventListener("dragover", this.dragOverHandler);
+    document.addEventListener("drop", this.dropHandler);
+
+    if ("ondragexit" in document) {
+      // Firefox fires dragexit and we can use the event.relatedTarget to see what the
+      // new element taking focus is. When leaving the window, this is set to null
+      document.addEventListener("dragexit", this.dragExitHandler);
+    } else {
+      // Chrome does not implement dragexit, but it does fire an event as soon as the
+      // mouse leaves the window, setting the mouse position to 0, 0
+      document.addEventListener("dragleave", this.dragLeaveHandler);
+    }
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("dragover", this.dragOverHandler);
+    document.removeEventListener("drop", this.dropHandler);
+    if ("ondragexit" in document) {
+      document.removeEventListener("dragexit", this.dragExitHandler);
+    } else {
+      document.removeEventListener("dragleave", this.dragLeaveHandler);
+    }
   }
 
   fileClickHandler() {
@@ -55,12 +90,96 @@ class FileInput extends React.Component {
     }
   }
 
+  dragOverHandler(e) {
+    e.preventDefault();
+    const dataTransfer = e.dataTransfer || e.target.dataTransfer;
+
+    if (dataTransfer.types && dataTransfer.types.indexOf("Files") !== -1) {
+      if (dataTransfer.items.length > 1) {
+        this.setState({
+          isDragging: true,
+          overLimit: true
+        });
+      } else {
+        this.setState({
+          isDragging: true,
+          canDrop: false,
+          overLimit: false
+        });
+
+        if (e.target === this.holder) {
+          this.setState({
+            canDrop: true
+          });
+        }
+      }
+    }
+  }
+
+  cancelDragging() {
+    this.setState({
+      isDragging: false,
+      canDrop: false,
+      overLimit: false
+    });
+  }
+
+  dragExitHandler(e) {
+    const relatedTarget = e.relatedTarget || e.target.relatedTarget;
+    if (relatedTarget === null) {
+      this.cancelDragging();
+    }
+  }
+
+  dragLeaveHandler(e) {
+    if (e.clientX + e.clientY === 0) {
+      this.cancelDragging();
+    }
+  }
+
+  dropHandler(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const dataTransfer = e.dataTransfer || e.target.dataTransfer;
+
+    if (dataTransfer.items.length === 1) {
+      this.setState({
+        isDragging: false,
+        canDrop: false
+      });
+
+      if (e.target === this.holder) {
+        // I can't find a way to test this
+        this.input.files = dataTransfer.files;
+        this.fileChangedHandler();
+      }
+    } else {
+      this.cancelDragging();
+    }
+  }
+
   render() {
     const { className, children } = this.props;
+    const { isDragging, canDrop, overLimit } = this.state;
+    const localClassName = [className, "p-file-input"];
+
+    if (isDragging) {
+      localClassName.push("is-dragging");
+    }
+
+    if (canDrop) {
+      localClassName.push("can-drop");
+    }
+
+    if (overLimit) {
+      localClassName.push("over-limit");
+    }
+
     return (
       <div
         ref={el => (this.holder = el)}
-        className={className}
+        className={localClassName.join(" ")}
         onClick={this.fileClickHandler}
         onKeyDown={this.keyboardEventHandler}
         tabIndex={0}
