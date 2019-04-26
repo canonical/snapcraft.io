65a78fd43bf6fd95b9be9c5f7db3aa45fa73bd33
Bartek Szopka <bartek.szopka@canonical.com>
2019-04-26 15:21:35 +0200

    Move test polyfill to setup file

--- static/js/public/accordion.test.js
+++ static/js/public/accordion.test.js
@@ -1,18 +1,5 @@
 import initAccordion, { initAccordionButtons } from "./accordion";
 
-// jsdom (in version that comes with jest) doesn't support .closest
-// so we need to polyfill that
-// shouldn't be needed when jsdom used by jest is updated to v11.12.0
-window.Element.prototype.closest = function(selector) {
-  var el = this;
-  while (el) {
-    if (el.matches(selector)) {
-      return el;
-    }
-    el = el.parentElement;
-  }
-};
-
 describe("accordion", () => {
   let button1;
   let button2;
