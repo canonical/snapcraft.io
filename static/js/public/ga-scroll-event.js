5d052e57f33de415d2f50bbe0b24e9a73aca1fe7
Bartek Szopka <bartek.szopka@canonical.com>
2019-06-06 12:19:25 +0200

    Add GA event triggered when install instructions are on screen

---
+++ static/js/public/ga-scroll-event.js
@@ -0,0 +1,43 @@
+import { triggerEvent } from "../base/ga";
+import debounce from "../libs/debounce";
+
+const isInViewport = el => {
+  var bounding = el.getBoundingClientRect();
+  return (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    bounding.right <=
+      (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+export default function triggerEventWhenVisible(selector) {
+  const el = document.querySelector(selector);
+
+  if (isInViewport(el)) {
+    triggerEvent(
+      "element-visible",
+      origin,
+      selector,
+      `Element visible on screen: ${selector}`
+    );
+  } else {
+    let triggered = false;
+    window.addEventListener(
+      "scroll",
+      debounce(() => {
+        if (!triggered && isInViewport(el)) {
+          triggerEvent(
+            "element-visible",
+            origin,
+            selector,
+            `Element visible on screen: ${selector}`
+          );
+          triggered = true;
+        }
+      }, 500)
+    );
+  }
+}
