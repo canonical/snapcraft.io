cee00ddb4d84bf3602082604b7c29fc1e8932756
Bartek Szopka <bartek.szopka@canonical.com>
2019-06-27 17:17:31 +0200

    Change RevisionListRow from class to function component

--- static/js/publisher/release/components/revisionsListRow.js
+++ static/js/publisher/release/components/revisionsListRow.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
@@ -10,80 +10,74 @@ import { getSelectedRevisions } from "../selectors";
 
 import DevmodeIcon from "./devmodeIcon";
 
-class RevisionsListRow extends Component {
-  revisionSelectChange(revision) {
-    this.props.toggleRevision(revision);
+const RevisionsListRow = props => {
+  const { revision, isSelectable, showAllColumns, isPending } = props;
+
+  const revisionDate = revision.release
+    ? new Date(revision.release.when)
+    : new Date(revision.created_at);
+
+  const isSelected = props.selectedRevisions.includes(revision.revision);
+
+  const id = `revision-check-${revision.revision}`;
+  const className = `${isSelectable ? "is-clickable" : ""} ${
+    isPending || isSelected ? "is-pending" : ""
+  }`;
+
+  function revisionSelectChange() {
+    props.toggleRevision(revision);
   }
 
-  renderRow(revision, isSelectable, showAllColumns, isPending) {
-    const revisionDate = revision.release
-      ? new Date(revision.release.when)
-      : new Date(revision.created_at);
-    const isSelected = this.props.selectedRevisions.includes(revision.revision);
-
-    const id = `revision-check-${revision.revision}`;
-    const className = `${isSelectable ? "is-clickable" : ""} ${
-      isPending || isSelected ? "is-pending" : ""
-    }`;
-
-    return (
-      <tr
-        key={id}
-        className={className}
-        onClick={
-          isSelectable ? this.revisionSelectChange.bind(this, revision) : null
-        }
-      >
-        <td>
-          {isSelectable ? (
-            <Fragment>
-              <input
-                type="checkbox"
-                checked={isSelected}
-                id={id}
-                onChange={this.revisionSelectChange.bind(this, revision)}
-              />
-              <label className="u-no-margin--bottom" htmlFor={id}>
-                {revision.revision}
-              </label>
-            </Fragment>
-          ) : (
-            <span>{revision.revision}</span>
-          )}
-        </td>
-        <td>
-          <DevmodeIcon revision={revision} showTooltip={true} />
-        </td>
-        <td>{revision.version}</td>
-        {showAllColumns && <td>{revision.channels.join(", ")}</td>}
-        <td className="u-align--right">
-          {isPending ? (
-            <em>pending release</em>
-          ) : (
+  return (
+    <tr
+      key={id}
+      className={className}
+      onClick={isSelectable ? revisionSelectChange : null}
+    >
+      <td>
+        {isSelectable ? (
+          <Fragment>
+            <input
+              type="checkbox"
+              checked={isSelected}
+              id={id}
+              onChange={revisionSelectChange}
+            />
+            <label className="u-no-margin--bottom" htmlFor={id}>
+              {revision.revision}
+            </label>
+          </Fragment>
+        ) : (
+          <span>{revision.revision}</span>
+        )}
+      </td>
+      <td>
+        <DevmodeIcon revision={revision} showTooltip={true} />
+      </td>
+      <td>{revision.version}</td>
+      {showAllColumns && <td>{revision.channels.join(", ")}</td>}
+      <td className="u-align--right">
+        {isPending ? (
+          <em>pending release</em>
+        ) : (
+          <span
+            className="p-tooltip p-tooltip--btm-center"
+            aria-describedby={`revision-uploaded-${revision.revision}`}
+          >
+            {distanceInWords(new Date(), revisionDate, { addSuffix: true })}
             <span
-              className="p-tooltip p-tooltip--btm-center"
-              aria-describedby={`revision-uploaded-${revision.revision}`}
+              className="p-tooltip__message u-align--center"
+              role="tooltip"
+              id={`revision-uploaded-${revision.revision}`}
             >
-              {distanceInWords(new Date(), revisionDate, { addSuffix: true })}
-              <span
-                className="p-tooltip__message u-align--center"
-                role="tooltip"
-                id={`revision-uploaded-${revision.revision}`}
-              >
-                {format(revisionDate, "YYYY-MM-DD HH:mm")}
-              </span>
+              {format(revisionDate, "YYYY-MM-DD HH:mm")}
             </span>
-          )}
-        </td>
-      </tr>
-    );
-  }
-
-  render() {
-    const { revision, isSelectable, showAllColumns, isPending } = this.props;
-    return this.renderRow(revision, isSelectable, showAllColumns, isPending);
-  }
-}
+          </span>
+        )}
+      </td>
+    </tr>
+  );
+};
 
 RevisionsListRow.propTypes = {
   // props
