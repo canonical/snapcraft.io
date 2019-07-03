4b654b90b7a14bdee47d84a0c953d644c811e997
Bartek Szopka <bartek.szopka@canonical.com>
2019-07-03 13:06:12 +0200

    Include react-dnd ES6 modules in babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -33,8 +33,11 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        // http://idangero.us/swiper/get-started/
-        exclude: /node_modules\/(?!(dom7|ssr-window|swiper)\/).*/,
+        // Exclude node_modules from using babel-loader
+        // except some that use ES6 modules and need to be transpiled:
+        // such as swiper http://idangero.us/swiper/get-started/
+        // and also react-dnd related
+        exclude: /node_modules\/(?!(dom7|ssr-window|swiper|dnd-core|react-dnd|react-dnd-html5-backend)\/).*/,
         use: {
           loader: "babel-loader"
         }
