ff92656db57de5fc5b6e8bd5a2a4e23369d98e3f
Lukewh <luke@lukewh.com>
2019-08-14 09:23:25 +0100

    import and init global-nav from base.js

--- static/js/base/base.js
+++ static/js/base/base.js
@@ -2,3 +2,9 @@ import "./polyfills";
 import "./dropdown-menu-toggle";
 import "./notification-dismiss";
 import "./ga";
+
+import { createNav } from "@canonical/global-nav";
+createNav({
+  maxWidth: "72rem",
+  showLogins: false
+});
