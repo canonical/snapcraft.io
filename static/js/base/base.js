810a2c3a866491c34e7de6f5d92c5e2612a44601
Lukewh <luke@lukewh.com>
2019-08-14 10:07:20 +0100

    Updated to be it's own module

--- static/js/base/base.js
+++ static/js/base/base.js
@@ -2,9 +2,3 @@ import "./polyfills";
 import "./dropdown-menu-toggle";
 import "./notification-dismiss";
 import "./ga";
-
-import { createNav } from "@canonical/global-nav";
-createNav({
-  maxWidth: "72rem",
-  showLogins: false
-});
