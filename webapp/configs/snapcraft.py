d80d806fc0a6f8ae9fcbc969822ad9aa575016d8
Bartek Szopka <bartek.szopka@canonical.com>
2019-08-19 17:37:49 +0200

    Add feature flag for featured banners

--- webapp/configs/snapcraft.py
+++ webapp/configs/snapcraft.py
@@ -2,4 +2,8 @@ import os
 
 WEBAPP_CONFIG = {"LAYOUT": "_layout.html", "STORE_NAME": "Snap store"}
 
+FEATURED_BANNERS_ENABLED = os.getenv(
+    "FEATURED_BANNERS_ENABLED", "false"
+).lower() in ["1", "t", "true"]
+
 BLOG_CATEGORIES_ENABLED = os.getenv("BLOG_CATEGORIES_ENABLED", "false")
