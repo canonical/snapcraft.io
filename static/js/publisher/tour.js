9ec74f256e5e0e6cf7e07d5af9044a483dd618f2
Bartek Szopka <bartek.szopka@canonical.com>
2019-08-21 16:24:21 +0200

    Reactify tour overlay demo

--- static/js/publisher/tour.js
+++ static/js/publisher/tour.js
@@ -1,60 +1,141 @@
-export default function initTour() {
-  var overlay = document.querySelector(".p-tour-overlay");
-  var overlayMask = document.querySelector(".p-tour-overlay__mask");
-  var tooltip = document.querySelector(".p-card--tour");
+import React, { useState, useRef } from "react";
+import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-  var offset = 5;
-  function moveOverlay(rect) {
-    let top = rect.top - offset;
-    if (top < 0) {
-      top = 0;
-    }
+const MASK_OFFSET = 5;
 
-    let left = rect.left - offset;
-    if (left < 0) {
-      left = 0;
-    }
+const getRectFromEl = el => {
+  let clientRect = el.getBoundingClientRect();
+  return {
+    top:
+      clientRect.top +
+      (window.pageYOffset || document.documentElement.scrollTop),
+    left:
+      clientRect.left +
+      (window.pageXOffset || document.documentElement.scrollLeft),
+    width: clientRect.width,
+    height: clientRect.height
+  };
+};
 
-    let bottom = rect.top + rect.height + offset;
-    let right = rect.left + rect.width + offset;
+const getClipPathFromMask = ({ top, bottom, left, right }) => {
+  let mask = [
+    `${left}px ${top}px`,
+    `${left}px ${bottom}px`,
+    `${right}px ${bottom}px`,
+    `${right}px ${top}px`,
+    `${left}px ${top}px`
+  ].join(",");
 
-    let mask = [
-      `${left}px ${top}px`,
-      `${left}px ${bottom}px`,
-      `${right}px ${bottom}px`,
-      `${right}px ${top}px`,
-      `${left}px ${top}px`
-    ].join(",");
+  return `polygon(0 0, 100% 0, 100% 100%, 0 100%, 0 0, ${mask})`;
+};
 
-    let clipPath = `polygon(0 0, 100% 0, 100% 100%, 0 100%, 0 0, ${mask})`;
-    overlayMask.style.webkitClipPath = clipPath;
-    overlayMask.style.clipPath = clipPath;
-    tooltip.style.left = (left > offset ? left : offset) + "px";
-    tooltip.style.top = bottom + "px";
+const getMaskFromRect = rect => {
+  let top = rect.top - MASK_OFFSET;
+  if (top < 0) {
+    top = 0;
   }
 
-  document.addEventListener("click", function(event) {
-    overlay.style.pointerEvents = "none";
+  let left = rect.left - MASK_OFFSET;
+  if (left < 0) {
+    left = 0;
+  }
+
+  let bottom = rect.top + rect.height + MASK_OFFSET;
+  let right = rect.left + rect.width + MASK_OFFSET;
+
+  return {
+    top,
+    bottom,
+    left,
+    right
+  };
+};
+
+function Tour({ targetEl }) {
+  const [rect, setRect] = useState(targetEl ? getRectFromEl(targetEl) : null);
+  const overlayEl = useRef(null);
+
+  const mask = rect ? getMaskFromRect(rect) : null;
+  let maskStyle = {};
+  let tooltipStyle = {};
+
+  if (mask) {
+    const clipPath = rect ? getClipPathFromMask(getMaskFromRect(rect)) : {};
+    maskStyle = {
+      clipPath,
+      WebkitClipPath: clipPath
+    };
+
+    tooltipStyle = {
+      top: mask.bottom,
+      left: mask.left > MASK_OFFSET ? mask.left : MASK_OFFSET
+    };
+  }
+
+  const onClick = event => {
+    overlayEl.current.style.pointerEvents = "none";
     var x = event.clientX,
       y = event.clientY,
       elementMouseIsOver = document.elementFromPoint(x, y);
-    overlay.style.pointerEvents = "";
+    overlayEl.current.style.pointerEvents = "";
 
-    let clientRect = elementMouseIsOver.getBoundingClientRect();
-    let rect = {
-      top:
-        clientRect.top +
-        (window.pageYOffset || document.documentElement.scrollTop),
-      left:
-        clientRect.left +
-        (window.pageXOffset || document.documentElement.scrollLeft),
-      width: clientRect.width,
-      height: clientRect.height
-    };
+    setRect(getRectFromEl(elementMouseIsOver));
+  };
 
-    moveOverlay(rect);
+  return (
+    <div className="p-tour-overlay" ref={overlayEl} onClick={onClick}>
+      <div className="p-tour-overlay__mask" style={maskStyle} />
 
-    event.preventDefault();
-    event.stopPropagation();
-  });
+      <div
+        className="p-card--tour is-tooltip--bottom-left"
+        style={tooltipStyle}
+      >
+        <h4>Welcome to the demo tour!</h4>
+        <p>
+          This is just a test to see how to position such information and how to
+          mask overlay background to highlight target element.
+        </p>
+        <p>Click around and see how overlay highlights the element.</p>
+        <p>Thank you for testing!</p>
+
+        <p className="p-tour-controls">
+          <span>
+            Done?{" "}
+            <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Skip tour</a>.
+          </span>
+
+          <span className="p-tour-controls__buttons">
+            <small className="p-tour-controls__step">1/1</small>
+            <button
+              disabled
+              className="p-button--neutral is-inline has-icon u-no-margin--bottom"
+            >
+              <i className="p-icon--contextual-menu is-prev">Previous step</i>
+            </button>
+            <button
+              disabled
+              className="p-button--positive is-inline has-icon u-no-margin--bottom u-no-margin--right"
+            >
+              <i className="p-icon--contextual-menu is-light is-next">
+                Next step
+              </i>
+            </button>
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+Tour.propTypes = {
+  targetEl: PropTypes.node
+};
+
+export default function initTour() {
+  const tour = document.createElement("div");
+  document.body.appendChild(tour);
+
+  const target = document.querySelector("h2");
+  ReactDOM.render(<Tour targetEl={target} />, tour);
 }
