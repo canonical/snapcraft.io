6fb99e3a20ed891d56bbe4b473bac8ea4c5ae056
Bartek Szopka <bartek.szopka@canonical.com>
2019-08-23 13:12:54 +0200

    Add support for multiple tour steps with tooltips in different positions

--- static/js/publisher/tour/tourBar.js
+++ static/js/publisher/tour/tourBar.js
@@ -5,7 +5,11 @@ export default function TourBar({ onButtonClick }) {
   return (
     <div className="p-tour-bar">
       <div className="u-fixed-width u-clearfix">
-        <button className="p-tour-bar__button" onClick={onButtonClick}>
+        <button
+          className="p-tour-bar__button"
+          data-tour="tour-end"
+          onClick={onButtonClick}
+        >
           <i className="p-icon--question">Tour</i>
         </button>
       </div>
