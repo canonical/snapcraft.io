ad8e3efdf2ba531214ac22c1904e7f727937ef6f
Bartek Szopka <bartek.szopka@canonical.com>
2019-08-27 13:57:00 +0200

    Replace custom animation in animateScrollTo with native "smooth" scroll

--- static/js/public/scroll-to.js
+++ static/js/public/scroll-to.js
@@ -1,87 +1,29 @@
-// From: https://gist.github.com/felipenmoura/650e7e1292c1e7638bcf6c9f9aeb9dd5
+export function animateScrollTo(to, offset = 0) {
+  const element = document.scrollingElement;
 
-/**
- * Will gracefuly scroll the page
- * This function will scroll the page using
- * an `ease-in-ou` effect.
- *
- * You can use it to scroll to a given element, as well.
- * To do so, pass the element instead of a number as the position.
- * Optionally, you can pass a `queryString` for an element selector.
- *
- * The default duration is half a second (500ms)
- *
- * This function returns a Promise that resolves as soon
- * as it has finished scrolling. If a selector is passed and
- * the element is not present in the page, it will reject.
- *
- * EXAMPLES:
- *
- * ```js
- * window.scrollPageTo('#some-section', 2000);
- * window.scrollPageTo(document.getElementById('some-section'), 1000);
- * window.scrollPageTo(500); // will scroll to 500px in 500ms
- * ```
- *
- * @returns {Promise}
- * @param {HTMLElement|Number|Selector} Target
- * @param {Number} Duration [default=500]
- *
- * Inspired by @andjosh's work
- *
- */
-export function animateScrollTo(to, duration = 500, offset = 0) {
-  //t = current time
-  //b = start value
-  //c = change in value
-  //d = duration
-  const easeInOutQuad = function(t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return (c / 2) * t * t + b;
-    t--;
-    return (-c / 2) * (t * (t - 2) - 1) + b;
-  };
-
-  return new Promise((resolve, reject) => {
-    const element = document.scrollingElement;
-
-    if (typeof to === "string") {
-      to = document.querySelector(to) || reject();
+  if (typeof to === "string") {
+    to = document.querySelector(to);
+    if (!to) {
+      throw Error(`Can't find any element for "${to}" in animateScrollTo.`);
     }
-    if (typeof to !== "number") {
-      to = to.getBoundingClientRect().top + element.scrollTop;
-    }
-    to = to + offset;
+  }
+  if (typeof to !== "number") {
+    to = to.getBoundingClientRect().top + element.scrollTop;
+  }
+  to = to - offset;
 
-    let start = element.scrollTop,
-      change = to - start,
-      currentTime = 0,
-      increment = 20;
-
-    const animateScroll = function() {
-      currentTime += increment;
-      let val = easeInOutQuad(currentTime, start, change, duration);
-      element.scrollTop = val;
-      if (currentTime < duration) {
-        setTimeout(animateScroll, increment);
-      } else {
-        resolve();
-      }
-    };
-    animateScroll();
-  });
+  element.scrollTo({ top: to, behavior: "smooth" });
 }
 
-export function initLinkScroll(link, { duration = 500, offset = 0 }) {
+export function initLinkScroll(link, { offset = 0 }) {
   if (link && (link.dataset.scrollTo || link.href)) {
     const href = link.dataset.scrollTo || link.getAttribute("href");
     const target = document.querySelector(href);
     if (target) {
       link.addEventListener("click", event => {
         event.preventDefault();
-        animateScrollTo(target, duration, offset).then(() => {
-          window.history.pushState({}, null, href);
-        });
+        animateScrollTo(target, offset);
+        setTimeout(() => window.history.pushState({}, null, href), 100);
       });
     }
   }
