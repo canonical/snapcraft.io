12aff771549c62185e49075796b8e3730fbb6602
Bartek Szopka <bartek.szopka@canonical.com>
2019-09-02 14:37:49 +0200

    Add tests for TourOverlay component

---
+++ static/js/publisher/tour/__mocks__/constants.js
@@ -0,0 +1,8 @@
+// this specifically needs to be mocked
+// not to rely on getting font size from browser
+export const REM = 16;
+
+export const MASK_OFFSET = 8;
+export const SCROLL_MARGIN = 400;
+export const SCROLL_OFFSET_TOP = 10;
+export const SCROLL_OFFSET_BOTTOM = 10;
