2a29f7b8424a9f338ef70e4f15bafbfd34d95b0f
Bartek Szopka <bartek.szopka@canonical.com>
2019-10-11 09:29:53 +0200

    Clean up and split releases table cell components into several files

---
+++ static/js/publisher/release/components/releasesTableRevisionCell.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { DND_ITEM_REVISIONS } from "./dnd";
+
+import {
+  ReleasesTableCellView,
+  RevisionInfo,
+  EmptyInfo
+} from "./releasesTableCellViews";
+
+const ReleasesTableRevisionCell = props => {
+  const { revision, showVersion } = props;
+
+  const item = {
+    revisions: [revision],
+    architectures: revision ? revision.architectures : [],
+    type: DND_ITEM_REVISIONS
+  };
+
+  return (
+    <ReleasesTableCellView item={item} canDrag={!!revision}>
+      {revision ? (
+        <RevisionInfo revision={revision} showVersion={showVersion} />
+      ) : (
+        <EmptyInfo />
+      )}
+    </ReleasesTableCellView>
+  );
+};
+
+ReleasesTableRevisionCell.propTypes = {
+  revision: PropTypes.object,
+  showVersion: PropTypes.bool
+};
+
+export default ReleasesTableRevisionCell;
