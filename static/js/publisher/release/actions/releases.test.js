f016d04b314b307adda42548001a285f655a7e53
Luke Wesley-Holley <luke.wesley-holley@canonical.com>
2019-11-19 15:30:50 +0000

    Update progressiveBar target % when props change (#2353)
    * Update progressiveBar target % when props change
    
    * Maintain progressive state in pendingRelease
    
    * Improve tests
    
    * Improve release testing
    
    * Persist progressive percentage on pause and resume (without saving)
    
    * When pausing also show percentage

--- static/js/publisher/release/actions/releases.test.js
+++ static/js/publisher/release/actions/releases.test.js
@@ -91,55 +91,104 @@ describe("releases actions", () => {
   });
 
   describe("handleReleaseResponse", () => {
-    it("should dispatch RELEASE_REVISION_SUCCESS", () => {
-      const store = mockStore({});
+    describe("RELEASE_REVISION_SUCCESS", () => {
+      it("should be dispatched if revision is passed", () => {
+        const store = mockStore({});
 
-      const dispatch = store.dispatch;
-      const json = {
-        success: true,
-        channel_map_tree: {
-          latest: {
-            16: {
-              amd64: [
-                {
-                  channel: "edge",
-                  info: "specific",
-                  revision: 3,
-                  version: "test"
-                }
-              ]
+        const dispatch = store.dispatch;
+        const json = {
+          success: true,
+          channel_map_tree: {
+            latest: {
+              16: {
+                amd64: [
+                  {
+                    channel: "edge",
+                    info: "specific",
+                    revision: 3,
+                    version: "test"
+                  }
+                ]
+              }
             }
           }
-        }
-      };
+        };
 
-      const release = [
-        {
-          id: 3,
+        const release = [
+          {
+            id: 3,
+            revision: 3,
+            channels: ["latest/edge"]
+          }
+        ];
+
+        const revision = {
+          architectures: ["amd64"],
+          revision: 3
+        };
+
+        const revisions = {
+          "3": revision
+        };
+
+        handleReleaseResponse(dispatch, json, release, revisions, "latest");
+
+        const actions = store.getActions();
+
+        expect(actions[0]).toEqual({
+          payload: {
+            channel: "latest/edge",
+            revision: revision
+          },
+          type: "RELEASE_REVISION_SUCCESS"
+        });
+      });
+
+      it("should be dispatched if there are no revisions", () => {
+        const store = mockStore({});
+
+        const dispatch = store.dispatch;
+        const json = {
+          success: true,
+          channel_map_tree: {
+            latest: {
+              16: {
+                amd64: [
+                  {
+                    channel: "edge",
+                    info: "specific",
+                    revision: 3,
+                    version: "test"
+                  }
+                ]
+              }
+            }
+          }
+        };
+
+        const revision = {
+          architectures: ["amd64"],
           revision: 3,
+          version: "test"
+        };
+
+        const release = {
+          id: 4,
+          revision: revision,
           channels: ["latest/edge"]
-        }
-      ];
+        };
 
-      const revision = {
-        architectures: ["amd64"],
-        revision: 3
-      };
+        handleReleaseResponse(dispatch, json, release, {}, "latest");
 
-      const revisions = {
-        "3": revision
-      };
+        const actions = store.getActions();
 
-      handleReleaseResponse(dispatch, json, release, revisions, "latest");
-
-      const actions = store.getActions();
-
-      expect(actions[0]).toEqual({
-        payload: {
-          channel: "latest/edge",
-          revision: revision
-        },
-        type: "RELEASE_REVISION_SUCCESS"
+        expect(actions[0]).toEqual({
+          payload: {
+            channel: "latest/edge",
+            revision: revision
+          },
+          type: "RELEASE_REVISION_SUCCESS"
+        });
       });
     });
   });
@@ -147,6 +196,153 @@ describe("releases actions", () => {
   describe("releaseRevisions", () => {
     afterEach(() => {
       global.fetch.mockRestore();
+    });
+
+    it("should remove progressive release if percentage is 100", () => {
+      const revision = {
+        architectures: ["amd64"],
+        revision: 3
+      };
+
+      const release = {
+        architecture: "amd64",
+        branch: null,
+        revision: 3,
+        risk: "edge",
+        track: "latest"
+      };
+      const store = mockStore({
+        options: {
+          snapName: "test",
+          csrfToken: "test",
+          defaultTrack: "latest"
+        },
+        pendingReleases: {
+          "3": {
+            "latest/edge": {
+              revision: revision,
+              channel: "latest/edge",
+              progressive: {
+                key: "test",
+                percentage: 100,
+                paused: false
+              }
+            }
+          }
+        },
+        revisions: {
+          "3": revision
+        }
+      });
+
+      global.fetch = jest
+        .fn()
+        // fetchReleases API Response
+        .mockResolvedValueOnce({
+          json: () => ({
+            success: true,
+            channel_map_tree: {
+              latest: {
+                16: {
+                  amd64: [
+                    {
+                      channel: "edge",
+                      info: "specific",
+                      revision: 3,
+                      version: "test"
+                    }
+                  ]
+                }
+              }
+            }
+          })
+        })
+        // fetchReleasesHistory API Response
+        .mockResolvedValueOnce({
+          json: () => ({
+            releases: [release],
+            revisions: [revision]
+          })
+        });
+
+      return store.dispatch(releaseRevisions()).then(() => {
+        const actions = store.getActions();
+        expect(actions).toEqual([
+          {
+            type: "HIDE_NOTIFICATION"
+          },
+          {
+            payload: {
+              channel: "latest/edge",
+              revision: revision
+            },
+            type: "RELEASE_REVISION_SUCCESS"
+          },
+          {
+            payload: {
+              revisions: {
+                3: revision
+              }
+            },
+            type: "UPDATE_REVISIONS"
+          },
+          {
+            payload: {
+              releases: [release]
+            },
+            type: "UPDATE_RELEASES"
+          },
+          {
+            type: "CANCEL_PENDING_RELEASES"
+          },
+          {
+            type: "CLOSE_HISTORY"
+          }
+        ]);
+      });
+    });
+
+    it("should handle an error", () => {
+      const store = mockStore({
+        options: {
+          snapName: "test",
+          csrfToken: "test",
+          defaultTrack: "latest"
+        },
+        pendingReleases: {}
+      });
+
+      global.fetch = jest
+        .fn()
+        // fetchReleases API Response
+        .mockResolvedValueOnce({
+          json: () => ({
+            success: true
+          })
+        });
+
+      return store.dispatch(releaseRevisions()).then(() => {
+        const actions = store.getActions();
+        expect(actions).toEqual([
+          {
+            type: "HIDE_NOTIFICATION"
+          },
+          {
+            type: "SHOW_NOTIFICATION",
+            payload: {
+              appearance: "negative",
+              content: "Cannot read property 'forEach' of undefined",
+              status: "error"
+            }
+          },
+          {
+            type: "CANCEL_PENDING_RELEASES"
+          },
+          {
+            type: "CLOSE_HISTORY"
+          }
+        ]);
+      });
     });
 
     it("should dispatch all the actions", () => {
@@ -160,7 +356,12 @@ describe("releases actions", () => {
         branch: null,
         revision: 3,
         risk: "edge",
-        track: "latest"
+        track: "latest",
+        progressive: {
+          key: "test",
+          percentage: 50,
+          paused: false
+        }
       };
       const store = mockStore({
         options: {
