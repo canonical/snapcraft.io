763a438fd4bc44284997882ac228e773d74c3182
solazio <ovidiu.suciu@canonical.com>
2020-01-16 12:39:56 +0000

    Add show less button

--- static/js/public/snap-details/exapandable-distr-chart.js
+++ static/js/public/snap-details/exapandable-distr-chart.js
@@ -1,18 +1,27 @@
 export default function initExpandableDistroChart() {
   const moreEl = document.querySelector(".snapcraft-distro-chart__more");
+  const lessEl = document.querySelector(".snapcraft-distro-chart__less");
   const detailsEl = document.querySelector(".snapcraft-distro-chart");
 
-  if (moreEl && detailsEl) {
-    moreEl.classList.remove("u-hide");
-    detailsEl.classList.add("is-collapsed");
-
-    document
-      .querySelector(".js-show-more-description")
+  if (moreEl && lessEl && detailsEl) {
+    moreEl
+      .querySelector("[data-js='js-show-more-description']")
       .addEventListener("click", function(event) {
         event.preventDefault();
 
-        moreEl.classList.add("u-hide");
+        moreEl.classList.toggle("u-show--small");
+        lessEl.classList.toggle("u-show--small");
         detailsEl.classList.remove("is-collapsed");
+      });
+
+    lessEl
+      .querySelector("[data-js='js-show-less-description']")
+      .addEventListener("click", function(event) {
+        event.preventDefault();
+
+        moreEl.classList.toggle("u-show--small");
+        lessEl.classList.toggle("u-show--small");
+        detailsEl.classList.add("is-collapsed");
       });
   }
 }
