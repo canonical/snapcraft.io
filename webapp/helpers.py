e55ad2e9ca3cc13c1851a8684884827c7c274c9d
Luke Wesley-Holley <luke.wesley-holley@canonical.com>
2020-01-21 21:55:31 +0000

    Duplicate licenses (#2448)
    * Ensure API errors are visible on form submission
    
    * Remove deprecated license ID's from license list
    
    * use the whitelisted version of licenses from the store

--- webapp/helpers.py
+++ webapp/helpers.py
@@ -25,10 +25,8 @@ class RegexConverter(BaseConverter):
 
 def get_licenses():
     try:
-        with open("webapp/licenses.json") as f:
-            licenses_file = json.load(f)
-
-        licenses = licenses_file["licenses"]
+        with open("webapp/store-licenses.json") as f:
+            licenses = json.load(f)
 
         def _build_custom_license(license_id, license_name):
             return {"licenseId": license_id, "name": license_name}
@@ -36,6 +34,21 @@ def get_licenses():
         CUSTOM_LICENSES = [
             _build_custom_license("Proprietary", "Proprietary"),
             _build_custom_license("Other Open Source", "Other Open Source"),
+            _build_custom_license(
+                "AGPL-3.0+", "GNU Affero General Public License v3.0 or later"
+            ),
+            _build_custom_license(
+                "GPL-2.0+", "GNU General Public License v2.0 or later"
+            ),
+            _build_custom_license(
+                "GPL-3.0+", "GNU General Public License v3.0 or later"
+            ),
+            _build_custom_license(
+                "LGPL-2.1+", "GNU Lesser General Public License v2.1 or later"
+            ),
+            _build_custom_license(
+                "LGPL-3.0+", "GNU Lesser General Public License v3.0 or later"
+            ),
         ]
 
         licenses = licenses + CUSTOM_LICENSES
