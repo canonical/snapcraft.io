3f4bc419924c7a0e9a3ada997e142b821708dcbe
Francisco Jimenez Cabrera <francisco.jimenez.cabrera@canonical.com>
2020-02-06 09:31:51 +0000

    Trigger new build

--- webapp/publisher/snaps/views.py
+++ webapp/publisher/snaps/views.py
@@ -1491,3 +1491,23 @@ def post_snap_builds(snap_name):
     return flask.redirect(
         flask.url_for(".get_snap_builds", snap_name=snap_name)
     )
+
+
+@publisher_snaps.route("/<snap_name>/builds/trigger-build", methods=["POST"])
+@login_required
+def post_trigger_build(snap_name):
+    try:
+        details = api.get_snap_info(snap_name, flask.session)
+    except ApiResponseErrorList as api_response_error_list:
+        if api_response_error_list.status_code == 404:
+            return flask.abort(404, "No snap named {}".format(snap_name))
+        else:
+            return _handle_error_list(api_response_error_list.errors)
+    except ApiError as api_error:
+        return _handle_error(api_error)
+
+    launchpad.trigger_build(details["snap_name"])
+
+    return flask.redirect(
+        flask.url_for(".get_snap_builds", snap_name=snap_name)
+    )
