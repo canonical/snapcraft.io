4a51a3626dde227a9a2b4a0f6fdb83803196c0b7
Luke Wesley-Holley <luke.wesley-holley@canonical.com>
2020-02-07 15:28:49 +0000

    Build list (#2500)
    * Update build list to new design
    
    * Better styling
    
    * Remove duplicate heading

--- static/js/publisher/builds/index.js
+++ static/js/publisher/builds/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 
 import distanceInWords from "date-fns/distance_in_words_strict";
@@ -15,135 +15,112 @@ const IN_PROGRESS = "in_progress";
 const FAILED_TO_BUILD = "failed_to_build";
 const UNKNOWN = "unknown";
 
-// based on BSI:
-// https://github.com/canonical-web-and-design/build.snapcraft.io/blob/master/src/common/helpers/snap-builds.js
-export const BuildStatusColours = {
-  BLUE: "blue",
-  GREEN: "green",
-  YELLOW: "yellow",
-  RED: "red",
-  GREY: "grey"
-};
-
-export const BuildStatusIcons = {
-  TICK: "tick",
-  TICK_OUTLINED: "outlined_tick",
-  TICK_SOLID: "solid_tick",
-  ELLIPSES: "ellipses",
-  SPINNER: "spinner",
-  CROSS: "cross"
-};
-
 export const UserFacingStatus = {
   // Used only when there is no build returned from LP.
   // When build is returned from LP (scheduled) it's 'Building soon' for BSI.
-  [NEVER_BUILT]: createStatus(
-    "Never built",
-    BuildStatusColours.GREY,
-    false,
-    8,
-    "never_built"
-  ),
+  [NEVER_BUILT]: createStatus("Never built", "Never built", 8, "never_built"),
   [BUILDING_SOON]: createStatus(
     "Building soon",
-    BuildStatusColours.GREY,
-    BuildStatusIcons.ELLIPSES,
+    "Building",
     7,
     "building_soon"
   ),
   [WONT_RELEASE]: createStatus(
     "Built, won’t be released",
-    BuildStatusColours.GREEN,
-    BuildStatusIcons.TICK_OUTLINED,
+    "Built",
     6,
     "wont_release"
   ),
-  [RELEASED]: createStatus(
-    "Built and released",
-    BuildStatusColours.GREEN,
-    BuildStatusIcons.TICK_SOLID,
-    5,
-    "released"
-  ),
+  [RELEASED]: createStatus("Built and released", "Released", 5, "released"),
   [RELEASE_FAILED]: createStatus(
     "Built, failed to release",
-    BuildStatusColours.RED,
-    BuildStatusIcons.TICK,
+    "Failed",
     4,
     "release_failed"
   ),
   [RELEASING_SOON]: createStatus(
     "Built, releasing soon",
-    BuildStatusColours.GREY,
-    BuildStatusIcons.TICK,
+    "Built",
     3,
     "releasing_soon"
   ),
-  [IN_PROGRESS]: createStatus(
-    "In progress",
-    BuildStatusColours.BLUE,
-    BuildStatusIcons.SPINNER,
-    2,
-    "in_progress"
-  ),
+  [IN_PROGRESS]: createStatus("In progress", "In progress", 2, "in_progress"),
   [FAILED_TO_BUILD]: createStatus(
     "Failed to build",
-    BuildStatusColours.RED,
-    BuildStatusIcons.CROSS,
+    "Failed",
     1,
     "failed_to_build"
   ),
-  [UNKNOWN]: createStatus(
-    "Unknown",
-    BuildStatusColours.GREY,
-    false,
-    8,
-    "never_built"
-  )
+  [UNKNOWN]: createStatus("Unknown", "Unknown", 8, "never_built")
 };
 
-function createStatus(statusMessage, colour, icon, priority, badge) {
+function createStatus(statusMessage, shortStatusMessage, priority, badge) {
   return {
     statusMessage,
-    colour,
-    icon,
+    shortStatusMessage,
+    icon: badge.indexOf("failed") > -1 ? "error" : false,
     priority,
     badge
   };
 }
 
+function createDuration(duration) {
+  const durationParts = duration.split(":");
+  const hours = parseInt(durationParts[0]);
+  const minutes = parseInt(durationParts[1]);
+  const seconds = Math.round(parseInt(durationParts[2]));
+
+  if (hours > 0) {
+    return `${hours} hour${hours > 1 ? "s" : ""}`;
+  }
+  if (minutes > 0) {
+    return `${minutes} minute${minutes > 1 ? "s" : ""}`;
+  }
+
+  return `${seconds} second${seconds > 1 || seconds === 0 ? "s" : ""}`;
+}
+
 export function initBuilds(id, snapName, builds) {
   const rows = builds.map(build => {
+    const status = UserFacingStatus[build.status];
+    let icon;
+
+    if (status.icon) {
+      icon = `p-icon--${status.icon}`;
+    }
+
     return {
       columns: [
         {
-          content: <a href={build.link}>{build.id}</a>
+          content: <a href={build.link}>#{build.id}</a>
         },
         {
           content: build.arch_tag
         },
         {
-          content: build.duration,
+          content: createDuration(build.duration),
           className: "u-hide--small"
         },
         {
           content: (
-            <span
-              className={`u-build-status--${
-                UserFacingStatus[build.status].colour
-              }`}
-            >
-              <a href={build.logs}>
-                {UserFacingStatus[build.status].statusMessage}
-              </a>
-            </span>
-          )
+            <Fragment>
+              <span className="u-hide u-show--small">
+                {icon && <i className={icon} />}
+                {status.shortStatusMessage}
+              </span>
+              <span className="u-hide--small">
+                {icon && <i className={icon} />}
+                {status.statusMessage}
+              </span>
+            </Fragment>
+          ),
+          className: "has-icon"
         },
         {
           content: distanceInWords(new Date(), build.datebuilt, {
             addSuffix: true
           }),
-          className: "u-align--right"
+          className: "u-align-text--right"
         }
       ]
     };
@@ -153,7 +130,7 @@ export function initBuilds(id, snapName, builds) {
     <MainTable
       headers={[
         {
-          content: "Build id"
+          content: "ID"
         },
         {
           content: "Architecture"
@@ -163,11 +140,11 @@ export function initBuilds(id, snapName, builds) {
           className: "u-hide--small"
         },
         {
-          content: "Result"
+          content: "Result",
+          className: "has-icon"
         },
         {
-          content: "Build date",
-          className: "u-align--right"
+          content: ""
         }
       ]}
       rows={rows}
