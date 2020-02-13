27fbaf1b5dbeebc991089b9e1307262f1c8f2b91
Luke Wesley-Holley <luke.wesley-holley@canonical.com>
2020-02-13 16:38:36 +0000

    Dynamic build list (#2502)
    * Update build list to new design
    
    * Wrap maintable in polling script
    
    * Dedupe header
    
    * Don't pass builds on page load
    
    * Load 15 at a time
    
    * formatting
    
    * Rearrange code so it works
    
    * Sticky bar even when no builds
    
    * Don't show duration on in progress builds

--- static/js/publisher/builds/index.js
+++ static/js/publisher/builds/index.js
@@ -1,153 +1,208 @@
 import React, { Fragment } from "react";
+import PropTypes from "prop-types";
 import ReactDOM from "react-dom";
 
 import distanceInWords from "date-fns/distance_in_words_strict";
 
 import MainTable from "@canonical/react-components/dist/components/MainTable";
 
-const NEVER_BUILT = "never_built";
-const BUILDING_SOON = "building_soon";
-const WONT_RELEASE = "wont_release";
-const RELEASED = "released";
-const RELEASE_FAILED = "release_failed";
-const RELEASING_SOON = "releasing_soon";
-const IN_PROGRESS = "in_progress";
-const FAILED_TO_BUILD = "failed_to_build";
-const UNKNOWN = "unknown";
+import { UserFacingStatus, createDuration } from "./helpers";
 
-export const UserFacingStatus = {
-  // Used only when there is no build returned from LP.
-  // When build is returned from LP (scheduled) it's 'Building soon' for BSI.
-  [NEVER_BUILT]: createStatus("Never built", "Never built", 8, "never_built"),
-  [BUILDING_SOON]: createStatus(
-    "Building soon",
-    "Building",
-    7,
-    "building_soon"
-  ),
-  [WONT_RELEASE]: createStatus(
-    "Built, won’t be released",
-    "Built",
-    6,
-    "wont_release"
-  ),
-  [RELEASED]: createStatus("Built and released", "Released", 5, "released"),
-  [RELEASE_FAILED]: createStatus(
-    "Built, failed to release",
-    "Failed",
-    4,
-    "release_failed"
-  ),
-  [RELEASING_SOON]: createStatus(
-    "Built, releasing soon",
-    "Built",
-    3,
-    "releasing_soon"
-  ),
-  [IN_PROGRESS]: createStatus("In progress", "In progress", 2, "in_progress"),
-  [FAILED_TO_BUILD]: createStatus(
-    "Failed to build",
-    "Failed",
-    1,
-    "failed_to_build"
-  ),
-  [UNKNOWN]: createStatus("Unknown", "Unknown", 8, "never_built")
-};
+class Builds extends React.Component {
+  constructor(props) {
+    super(props);
 
-function createStatus(statusMessage, shortStatusMessage, priority, badge) {
-  return {
-    statusMessage,
-    shortStatusMessage,
-    icon: badge.indexOf("failed") > -1 ? "error" : false,
-    priority,
-    badge
-  };
-}
+    this.fetchTimer = null;
 
-function createDuration(duration) {
-  const durationParts = duration.split(":");
-  const hours = parseInt(durationParts[0]);
-  const minutes = parseInt(durationParts[1]);
-  const seconds = Math.round(parseInt(durationParts[2]));
+    this.state = {
+      isLoading: false,
+      fetchSize: 15,
+      fetchStart: 0,
+      builds: props.builds ? props.builds : []
+    };
 
-  if (hours > 0) {
-    return `${hours} hour${hours > 1 ? "s" : ""}`;
-  }
-  if (minutes > 0) {
-    return `${minutes} minute${minutes > 1 ? "s" : ""}`;
+    this.showMoreHandler = this.showMoreHandler.bind(this);
   }
 
-  return `${seconds} second${seconds > 1 || seconds === 0 ? "s" : ""}`;
-}
+  fetchBuilds(fromStart) {
+    const { fetchSize, fetchStart, builds } = this.state;
+    const { snapName, updateFreq } = this.props;
 
-export function initBuilds(id, snapName, builds) {
-  const rows = builds.map(build => {
-    const status = UserFacingStatus[build.status];
-    let icon;
+    let url = `/${snapName}/builds.json`;
+    let params = [];
 
-    if (status.icon) {
-      icon = `p-icon--${status.icon}`;
+    if (fetchStart && !fromStart) {
+      params.push(`start=${fetchStart}`);
     }
 
-    return {
-      columns: [
-        {
-          content: <a href={build.link}>#{build.id}</a>
-        },
-        {
-          content: build.arch_tag
-        },
-        {
-          content: createDuration(build.duration),
-          className: "u-hide--small"
-        },
-        {
-          content: (
-            <Fragment>
-              <span className="u-hide u-show--small">
-                {icon && <i className={icon} />}
-                {status.shortStatusMessage}
-              </span>
-              <span className="u-hide--small">
-                {icon && <i className={icon} />}
-                {status.statusMessage}
-              </span>
-            </Fragment>
-          ),
-          className: "has-icon"
-        },
-        {
-          content: distanceInWords(new Date(), build.datebuilt, {
-            addSuffix: true
-          }),
-          className: "u-align-text--right"
-        }
-      ]
-    };
-  });
+    if (fetchSize) {
+      params.push(`size=${fetchSize}`);
+    }
 
+    if (params.length > 0) {
+      url += `?${params.join("&")}`;
+    }
+
+    fetch(url)
+      .then(res => res.json())
+      .then(result => {
+        const newBuilds = builds;
+
+        this.setState({
+          isLoading: false,
+          builds: fromStart
+            ? result.snap_builds
+            : newBuilds.concat(result.snap_builds)
+        });
+      })
+      .catch(() => {
+        this.setState({
+          isLoading: false
+        });
+      });
+
+    if (this.fetchTimer) {
+      clearTimeout(this.fetchTimer);
+    }
+    this.fetchTimer = setTimeout(() => this.fetchBuilds(true), updateFreq);
+  }
+
+  showMoreHandler(e) {
+    const { fetchStart, fetchSize } = this.state;
+    e.preventDefault();
+    this.setState(
+      {
+        fetchStart: fetchStart + 15,
+        fetchSize: fetchSize + 15,
+        isLoading: true
+      },
+      () => {
+        this.fetchBuilds();
+      }
+    );
+  }
+
+  componentDidMount() {
+    const { builds, updateFreq } = this.props;
+    if (!builds) {
+      this.fetchBuilds();
+    }
+
+    this.fetchTimer = setTimeout(() => this.fetchBuilds(true), updateFreq);
+  }
+
+  render() {
+    const { builds, isLoading } = this.state;
+    const { totalBuilds } = this.props;
+
+    const remainingBuilds = totalBuilds - builds.length;
+
+    const showMoreCount = remainingBuilds > 15 ? 15 : remainingBuilds;
+
+    const rows = builds.map(build => {
+      const status = UserFacingStatus[build.status];
+      let icon;
+
+      if (status.icon) {
+        icon = `p-icon--${status.icon}`;
+      }
+
+      return {
+        columns: [
+          {
+            content: <a href={build.link}>#{build.id}</a>
+          },
+          {
+            content: build.arch_tag
+          },
+          {
+            content: createDuration(build.duration),
+            className: "u-hide--small"
+          },
+          {
+            content: (
+              <Fragment>
+                <span className="u-hide u-show--small">
+                  {icon && <i className={icon} />}
+                  {status.shortStatusMessage}
+                </span>
+                <span className="u-hide--small">
+                  {icon && <i className={icon} />}
+                  {status.statusMessage}
+                </span>
+              </Fragment>
+            ),
+            className: "has-icon"
+          },
+          {
+            content: build.datebuilt
+              ? distanceInWords(new Date(), build.datebuilt, {
+                  addSuffix: true
+                })
+              : "",
+            className: "u-align-text--right"
+          }
+        ]
+      };
+    });
+
+    return (
+      <Fragment>
+        <MainTable
+          headers={[
+            {
+              content: "ID"
+            },
+            {
+              content: "Architecture"
+            },
+            {
+              content: "Duration",
+              className: "u-hide--small"
+            },
+            {
+              content: "Result",
+              className: "has-icon"
+            },
+            {
+              content: ""
+            }
+          ]}
+          rows={rows}
+        />
+        {builds.length < totalBuilds && (
+          <div className="p-show-more__link-container">
+            {isLoading && (
+              <span className="p-show-more__link">
+                <i className="p-icon--spinner u-animation--spin" />
+              </span>
+            )}
+            {!isLoading && (
+              <a className="p-show-more__link" onClick={this.showMoreHandler}>
+                Show {showMoreCount} more
+              </a>
+            )}
+          </div>
+        )}
+      </Fragment>
+    );
+  }
+}
+
+Builds.propTypes = {
+  snapName: PropTypes.string,
+  builds: PropTypes.array,
+  totalBuilds: PropTypes.number,
+  updateFreq: PropTypes.number
+};
+
+export function initBuilds(id, snapName, builds, totalBuilds) {
   ReactDOM.render(
-    <MainTable
-      headers={[
-        {
-          content: "ID"
-        },
-        {
-          content: "Architecture"
-        },
-        {
-          content: "Duration",
-          className: "u-hide--small"
-        },
-        {
-          content: "Result",
-          className: "has-icon"
-        },
-        {
-          content: ""
-        }
-      ]}
-      rows={rows}
+    <Builds
+      snapName={snapName}
+      builds={builds}
+      totalBuilds={totalBuilds}
+      updateFreq={30000}
     />,
     document.querySelector(id)
   );
