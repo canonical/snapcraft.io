e02ff60160d1375aab494cfe17b378d336acf866
Francisco Jiménez Cabrera <jkfran@gmail.com>
2020-02-24 13:57:25 +0000

    Revert "Use flask-base (#2551)"
    This reverts commit 6e95598a763ea865c58c7104e9369f1ff7963798.

--- webapp/app.py
+++ webapp/app.py
@@ -4,7 +4,7 @@ A Flask application for snapcraft.io.
 The web frontend for the snap store.
 """
 
-from canonicalwebteam.flask_base.app import FlaskBase
+import flask
 from werkzeug.contrib.fixers import ProxyFix
 from werkzeug.debug import DebuggedApplication
 
@@ -33,13 +33,10 @@ from webapp.store.views import store_blueprint
 
 
 def create_app(testing=False):
-    app = FlaskBase(
-        __name__,
-        "snapcraft.io",
-        template_folder="../templates",
-        static_folder="../static",
-        template_404="404.html",
+    app = flask.Flask(
+        __name__, template_folder="../templates", static_folder="../static"
     )
+
     app.config.from_object("webapp.config")
     app.testing = testing
 
