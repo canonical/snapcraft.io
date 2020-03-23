61237e2143650997d08c74e396dc9e3de02104b7
Francisco Jiménez Cabrera <francisco.jimenez.cabrera@canonical.com>
2020-03-23 10:44:48 +0000

    Return 401 when no authenticated on GitHub (#2629)

--- webapp/publisher/github/views.py
+++ webapp/publisher/github/views.py
@@ -1,6 +1,7 @@
 import flask
 from webapp.api.github import GitHub
 from webapp.decorators import login_required
+from werkzeug.exceptions import Unauthorized
 
 publisher_github = flask.Blueprint(
     "github", __name__, template_folder="/templates", static_folder="/static"
@@ -13,9 +14,15 @@ def get_repos():
     github = GitHub(flask.session.get("github_auth_secret"))
     org = flask.request.args.get("org")
 
-    if org:
-        repos = github.get_org_repositories(org)
-    else:
-        repos = github.get_user_repositories()
+    try:
+        if org:
+            repos = github.get_org_repositories(org)
+        else:
+            repos = github.get_user_repositories()
+    except Unauthorized:
+        return (
+            flask.jsonify({"error": "You need to be authenticated on GitHub"}),
+            401,
+        )
 
     return flask.jsonify(repos)
