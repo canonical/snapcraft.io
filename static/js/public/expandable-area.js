581d3812aa0b09bd12afc5ae2a72138aeeed1b9e
Luke Wesley-Holley <luke.wesley-holley@canonical.com>
2020-04-17 10:39:34 +0100

    Pi distro page (#2674)
    * Add raspberry pi distro page
    
    * add to distro selector on details page
    
    * Added more optional logic to the expandable-area
    
    * Update heading classes
    
    * Update heading classes

--- static/js/public/expandable-area.js
+++ static/js/public/expandable-area.js
@@ -1,4 +1,13 @@
-export default function initExpandableArea() {
+/**
+   Initializes an expandable area
+
+   @param {string} overflowSelector - CSS Selector for the element that can overflow
+   @param {string} heightMatchSelector - CSS Selector for the element that overflowSelector's height should be compared against
+*/
+export default function initExpandableArea(
+  overflowSelector,
+  heightMatchSelector
+) {
   const showMoreContainer = [].slice.call(
     document.querySelectorAll("[data-js='js-show-more']")
   );
@@ -7,6 +16,18 @@ export default function initExpandableArea() {
     showMoreContainer.forEach(el => {
       const fadeEl = el.querySelector(".p-show-more__fade");
       const linkEl = el.querySelector(".p-show-more__link");
+
+      if (overflowSelector && heightMatchSelector) {
+        const overflowEl = el.querySelector(overflowSelector);
+        const heightMatchEl = el.querySelector(heightMatchSelector);
+
+        if (overflowEl.scrollHeight <= heightMatchEl.scrollHeight) {
+          el.removeAttribute("data-js");
+          el.classList.remove("p-show-more");
+          el.removeChild(fadeEl);
+          el.removeChild(linkEl);
+        }
+      }
 
       if (fadeEl && linkEl) {
         linkEl.addEventListener("click", function(event) {
