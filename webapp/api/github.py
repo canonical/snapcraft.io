ae9e72d467e9c8a973e954fe45bae491595c9300
Lukewh <luke@lukewh.com>
2020-04-29 20:52:52 +0100

    Show repos in list

--- webapp/api/github.py
+++ webapp/api/github.py
@@ -161,7 +161,7 @@ class GitHub:
                 ) {
               edges {
                 node {
-                  name
+                  nameWithOwner
                 }
               }
               pageInfo {
