34af7ead34fd1d9130d4bad3e5918427a168fab3
Ovidiu Suciu <40214246+solazio@users.noreply.github.com>
2020-05-04 15:29:36 +0100

    Do not open lightbox in a new window on mobile (#2708)
    * Do not open lightbox in a new window on mobile
    
    * Fix X button on mobile to be visible

--- static/js/public/snap-details/screenshots.js
+++ static/js/public/snap-details/screenshots.js
@@ -1,5 +1,4 @@
 import lightbox from "./../../publisher/market/lightbox";
-import { isMobile } from "../../libs/mobile";
 import { Swiper, Navigation } from "swiper/dist/js/swiper.esm";
 import { SCREENSHOTS_CONFIG } from "../../config/swiper.config";
 import iframeSize from "../../libs/iframeSize";
@@ -13,12 +12,7 @@ function clickCallback(event) {
   const images = filterImages();
 
   if (url) {
-    if (isMobile()) {
-      window.open(url, "_blank");
-      window.focus();
-    } else {
-      lightbox.openLightbox(url, images);
-    }
+    lightbox.openLightbox(url, images);
   }
 }
 
