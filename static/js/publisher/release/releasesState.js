ad4c63b0d4b30783495cba609eedffd2857e8399
Lukewh <luke@lukewh.com>
2020-05-21 10:40:58 +0100

    Use new channel-map endpoint for release UI

--- static/js/publisher/release/releasesState.js
+++ static/js/publisher/release/releasesState.js
@@ -59,6 +59,30 @@ function getReleaseDataFromChannelMap(channelMap, revisionsMap) {
   return releasedChannels;
 }
 
+// The same as getReleaseDataFromChannelMap but using the v2 API channel-map endpoint
+// https://dashboard.snapcraft.io/docs/v2/en/snaps.html#snap-channel-map
+function getReleaseDataFromChannelMapV2(channelMap, revisionsMap) {
+  const releasedChannels = {};
+
+  channelMap["channel-map"].forEach(mapInfo => {
+    if (!releasedChannels[mapInfo.channel]) {
+      releasedChannels[mapInfo.channel] = {};
+    }
+
+    if (
+      !releasedChannels[mapInfo.channel][mapInfo.architecture] &&
+      revisionsMap[mapInfo.revision]
+    ) {
+      releasedChannels[mapInfo.channel][mapInfo.architecture] =
+        revisionsMap[mapInfo.revision];
+      releasedChannels[mapInfo.channel][mapInfo.architecture].expiration =
+        mapInfo["expiration-date"];
+    }
+  });
+
+  return releasedChannels;
+}
+
 // for channel without release get next (less risk) channel with a release
 function getTrackingChannel(releasedChannels, track, risk, arch) {
   let tracking = null;
@@ -104,5 +128,6 @@ export {
   getTrackingChannel,
   getRevisionsMap,
   initReleasesData,
-  getReleaseDataFromChannelMap
+  getReleaseDataFromChannelMap,
+  getReleaseDataFromChannelMapV2
 };
