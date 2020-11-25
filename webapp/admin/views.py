40a44e607aa7a550be0876674061803d7fc53d9e
Steve Rydz <steve.rydz@canonical.com>
2020-11-25 14:15:00 +0000

    Add stores list to sidebar (#3219)
    * Add skeleton of stores sidebar
    
    * Add template for store page
    
    * Move admin navigation to partial
    
    * Make admin navigation links work
    
    * Move store admin page behind login
    
    * Empty commit

--- webapp/admin/views.py
+++ webapp/admin/views.py
@@ -12,4 +12,12 @@ admin = flask.Blueprint(
 @admin.route("/admin")
 @login_required
 def get_stores():
-    return flask.render_template("admin/admin.html")
+
+    return flask.render_template("admin/admin.html", store_id="store-a1")
+
+
+@admin.route("/admin/<store_id>")
+@login_required
+def get_store(store_id):
+
+    return flask.render_template("admin/admin.html", store_id=store_id)
