9285c7616a4e121df596b9bbe4a40f5a293691dd
Francisco Jiménez Cabrera <francisco.jimenez.cabrera@canonical.com>
2020-11-26 16:31:10 +0000

    Get store Snaps (#3228)

--- webapp/admin/views.py
+++ webapp/admin/views.py
@@ -1,6 +1,6 @@
 # Packages
 import flask
-from canonicalwebteam.store_api.stores.snapstore import SnapPublisher
+from canonicalwebteam.store_api.stores.snapstore import SnapStoreAdmin
 from canonicalwebteam.store_api.exceptions import (
     StoreApiError,
     StoreApiResponseErrorList,
@@ -11,69 +11,63 @@ from webapp.helpers import api_publisher_session
 from webapp.api.exceptions import ApiError
 from webapp.decorators import login_required
 from webapp.publisher.views import _handle_error, _handle_error_list
-from webapp.admin import logic
 
-publisher_api = SnapPublisher(api_publisher_session)
+admin_api = SnapStoreAdmin(api_publisher_session)
 
 admin = flask.Blueprint(
     "admin", __name__, template_folder="/templates", static_folder="/static"
 )
 
 
-def fetch_stores():
+@admin.route("/admin")
+@login_required
+def get_stores():
+    """
+    In this view we get all the stores the user is an admin or we show a 403
+    """
     try:
-        account_info = publisher_api.get_account(flask.session)
+        stores = admin_api.get_stores(flask.session)
     except StoreApiResponseErrorList as api_response_error_list:
         return _handle_error_list(api_response_error_list.errors)
     except (StoreApiError, ApiError) as api_error:
         return _handle_error(api_error)
 
-    stores = logic.get_admin_stores(account_info)
-
     if not stores:
         flask.abort(403)
 
-    return stores
-
-
-def fetch_store(store_id):
-    stores = fetch_stores()
-    store = next((s for s in stores if s["id"] == store_id), None)
-
-    return store
-
-
-@admin.route("/admin")
-@login_required
-def get_stores():
-    stores = fetch_stores()
-
-    return flask.render_template(
-        "admin/admin.html",
-        stores=stores,
-        store=stores[0],
-        store_id=stores[0]["id"],
+    # We redirect to the first store snap list
+    return flask.redirect(
+        flask.url_for(".get_store_snaps", store_id=stores[0]["id"])
     )
 
 
 @admin.route("/admin/<store_id>/snaps")
 @login_required
-def get_store(store_id):
-
-    stores = fetch_stores()
-    store = fetch_store(store_id)
+def get_store_snaps(store_id):
+    try:
+        stores = admin_api.get_stores(flask.session)
+        store = admin_api.get_store(flask.session, store_id, stores)
+        snaps = admin_api.get_store_snaps(flask.session, store["id"])
+    except StoreApiResponseErrorList as api_response_error_list:
+        return _handle_error_list(api_response_error_list.errors)
+    except (StoreApiError, ApiError) as api_error:
+        return _handle_error(api_error)
 
     return flask.render_template(
-        "admin/snaps.html", stores=stores, store=store, store_id=store_id
+        "admin/snaps.html", stores=stores, store=store, snaps=snaps
     )
 
 
 @admin.route("/admin/<store_id>/members")
 @login_required
 def get_members(store_id):
-
-    stores = fetch_stores()
-    store = fetch_store(store_id)
+    try:
+        stores = admin_api.get_stores(flask.session)
+        store = admin_api.get_store(flask.session, store_id, stores)
+    except StoreApiResponseErrorList as api_response_error_list:
+        return _handle_error_list(api_response_error_list.errors)
+    except (StoreApiError, ApiError) as api_error:
+        return _handle_error(api_error)
 
     return flask.render_template(
         "admin/members.html", stores=stores, store=store, store_id=store_id
