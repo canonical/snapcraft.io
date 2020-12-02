76a4b506b1d275b8879ad6207bcc3b7ace8dcbb5
Steve Rydz <steve.rydz@canonical.com>
2020-12-02 15:40:50 +0000

    Add invite member

--- webpack.config.entry.js
+++ webpack.config.entry.js
@@ -6,6 +6,8 @@ module.exports = {
   public: "./static/js/public/public.js",
   about: "./static/js/public/about/index.js",
   tabpanel: "./static/js/public/hero-tabpanel.js",
+  modal: "./static/js/public/modal.js",
+  "manage-members": "./static/js/public/manage-members.js",
   // TODO:
   // publisher bundle is big (webpack warning) - try to chunk it down
   // https://github.com/canonical-web-and-design/snapcraft.io/issues/1246
