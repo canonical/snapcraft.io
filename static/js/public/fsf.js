3c52887d0048563b2c69bec2bd6679f056445a30
Steve Rydz <steve.rydz@canonical.com>
2020-12-07 14:20:10 +0000

    Remove unused JS from fsf

---
+++ static/js/public/fsf.js
@@ -0,0 +1,11 @@
+import initAccordion from "./accordion";
+import { initFSFLanguageSelect } from "./fsf-language-select";
+import firstSnapFlow from "./first-snap-flow";
+import initExpandableArea from "./expandable-area";
+
+export {
+  initAccordion,
+  initExpandableArea,
+  initFSFLanguageSelect,
+  firstSnapFlow,
+};
