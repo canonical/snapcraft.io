b0dcf2d0fec1aed9563e16f543f242e88505a756
Steve Rydz <steve.rydz@canonical.com>
2020-12-07 14:46:54 +0000

    Remove unused JS from distro install

--- webpack.config.entry.js
+++ webpack.config.entry.js
@@ -18,4 +18,5 @@ module.exports = {
   "store-details": "./static/js/public/store-details.js",
   fsf: "./static/js/public/fsf.js",
   search: "./static/js/public/search.js",
+  "distro-install": "./static/js/public/distro-install.js",
 };
