8153f33e9c8c70ea7cf31bac8b4a9a81d2965e61
Francisco Jimenez Cabrera <francisco.jimenez.cabrera@canonical.com>
2021-07-29 11:39:27 +0100

    Upgrade docs and tutorials

--- webapp/docs/views.py
+++ webapp/docs/views.py
@@ -2,33 +2,32 @@ import talisker
 
 from canonicalwebteam.discourse import (
     DiscourseAPI,
-    Docs,
     DocParser,
+    Docs,
 )
 from canonicalwebteam.search import build_search_view
 
 
 def init_docs(app, url_prefix):
+    session = talisker.requests.get_session()
     discourse_docs = Docs(
         parser=DocParser(
             api=DiscourseAPI(
-                base_url="https://forum.snapcraft.io/",
-                session=talisker.requests.get_session(),
+                base_url="https://forum.snapcraft.io/", session=session
             ),
-            index_topic_id=11127,
+            index_topic_id=25750,
             url_prefix=url_prefix,
         ),
         document_template="docs/document.html",
         url_prefix=url_prefix,
     )
-
     discourse_docs.init_app(app)
 
     app.add_url_rule(
         "/docs/search",
         "docs-search",
         build_search_view(
-            session=talisker.requests.get_session(),
+            session=session,
             site="snapcraft.io/docs",
             template_path="docs/search.html",
         ),
