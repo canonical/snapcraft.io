bcd42129b9c52f8b472c31233e857f8d54f0dc7b
Francisco Jiménez Cabrera <francisco.jimenez.cabrera@canonical.com>
2021-07-30 11:25:45 +0100

    Revert "Upgrade docs and tutorials (#3613)" (#3616)
    This reverts commit 572411afe8d1e3c4701dd1b12376053843aeb0c4.

--- webapp/docs/views.py
+++ webapp/docs/views.py
@@ -2,18 +2,18 @@ import talisker
 
 from canonicalwebteam.discourse import (
     DiscourseAPI,
-    DocParser,
     Docs,
+    DocParser,
 )
 from canonicalwebteam.search import build_search_view
 
 
 def init_docs(app, url_prefix):
-    session = talisker.requests.get_session()
     discourse_docs = Docs(
         parser=DocParser(
             api=DiscourseAPI(
-                base_url="https://forum.snapcraft.io/", session=session
+                base_url="https://forum.snapcraft.io/",
+                session=talisker.requests.get_session(),
             ),
             index_topic_id=11127,
             url_prefix=url_prefix,
@@ -21,13 +21,14 @@ def init_docs(app, url_prefix):
         document_template="docs/document.html",
         url_prefix=url_prefix,
     )
+
     discourse_docs.init_app(app)
 
     app.add_url_rule(
         "/docs/search",
         "docs-search",
         build_search_view(
-            session=session,
+            session=talisker.requests.get_session(),
             site="snapcraft.io/docs",
             template_path="docs/search.html",
         ),
