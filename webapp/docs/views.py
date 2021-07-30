94648faf6c1e70abb349a993e2da9194d1e1e1b3
Francisco Jiménez Cabrera <jkfran@gmail.com>
2021-07-30 11:35:27 +0100

    Revert "Revert "Upgrade docs and tutorials (#3613)" (#3616)"
    This reverts commit bcd42129b9c52f8b472c31233e857f8d54f0dc7b.

--- webapp/docs/views.py
+++ webapp/docs/views.py
@@ -1,34 +1,44 @@
+from os import getenv
+
 import talisker
 
 from canonicalwebteam.discourse import (
     DiscourseAPI,
-    Docs,
     DocParser,
+    Docs,
 )
 from canonicalwebteam.search import build_search_view
 
+DISCOURSE_API_KEY = getenv("DISCOURSE_API_KEY")
+DISCOURSE_API_USERNAME = getenv("DISCOURSE_API_USERNAME")
+
 
 def init_docs(app, url_prefix):
+    session = talisker.requests.get_session()
     discourse_docs = Docs(
         parser=DocParser(
             api=DiscourseAPI(
                 base_url="https://forum.snapcraft.io/",
-                session=talisker.requests.get_session(),
+                session=session,
+                api_key=DISCOURSE_API_KEY,
+                api_username=DISCOURSE_API_USERNAME,
+                get_topics_query_id=2,
             ),
-            index_topic_id=11127,
+            index_topic_id=25750,
             url_prefix=url_prefix,
+            tutorials_index_topic_id=15409,
+            tutorials_url_prefix="/tutorials",
         ),
         document_template="docs/document.html",
         url_prefix=url_prefix,
     )
-
     discourse_docs.init_app(app)
 
     app.add_url_rule(
         "/docs/search",
         "docs-search",
         build_search_view(
-            session=talisker.requests.get_session(),
+            session=session,
             site="snapcraft.io/docs",
             template_path="docs/search.html",
         ),
