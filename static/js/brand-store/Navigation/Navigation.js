901a68d20ed52afc9ede8522a090139a42fdce66
Steve Rydz <steve.rydz@canonical.com>
2021-09-06 11:49:21 +0100

    Fix linting errors

--- static/js/brand-store/Navigation/Navigation.js
+++ static/js/brand-store/Navigation/Navigation.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { brandStoresListSelector } from "../selectors";
