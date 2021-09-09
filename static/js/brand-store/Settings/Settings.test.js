6d68e9a2dbe5fb8ffe984ac8244b980bea9fbffd
Scott Mason Nash <sowasred2012@gmail.com>
2021-09-09 17:28:09 +0100

    add tests for is_public checkbox on Settings

--- static/js/brand-store/Settings/Settings.test.js
+++ static/js/brand-store/Settings/Settings.test.js
@@ -40,4 +40,34 @@ describe("<Settings />", () => {
     );
     expect(document.querySelector("#is_public")).toBeInTheDocument();
   });
+
+  test("the 'is public' checkbox should not be checked when the current store is set to private", () => {
+    render(
+      <Provider store={store}>
+        <Settings />
+      </Provider>
+    );
+    expect(document.querySelector("#is_public").checked).toEqual(false);
+  });
+
+  test("the 'is public' checkbox should be checked when the current store is not set to private", () => {
+    useSelector.mockImplementation((callback) => {
+      return callback({
+        currentStore: {
+          currentStore: {
+            id: "test",
+            private: false,
+            "manual-review-policy": "allow",
+          },
+        },
+      });
+    });
+
+    render(
+      <Provider store={store}>
+        <Settings />
+      </Provider>
+    );
+    expect(document.querySelector("#is_public").checked).toEqual(true);
+  });
 });
