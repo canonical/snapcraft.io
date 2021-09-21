cc66e57ca1edd5061e286c9f6a92fcf3c1e5d6c2
Steve Rydz <steve.rydz@canonical.com>
2021-09-21 16:46:51 +0100

    Add snaps section (#3701)

--- static/js/brand-store/Snaps/SnapsTable.js
+++ static/js/brand-store/Snaps/SnapsTable.js
@@ -1,15 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import { useParams } from "react-router-dom";
 
 import SnapsTableRow from "./SnapsTableRow";
+import { CheckboxInput } from "@canonical/react-components";
 
-function SnapsTable({ storeName, snaps, otherStores }) {
+function SnapsTable({
+  storeName,
+  snaps,
+  otherStores,
+  snapsToRemove,
+  setSnapsToRemove,
+  nonEssentialSnapIds,
+}) {
+  const { id } = useParams();
+  const [checkAll, setCheckAll] = useState(false);
+
+  useEffect(() => {
+    setCheckAll(snapsToRemove.length === nonEssentialSnapIds.length);
+  }, [snapsToRemove, nonEssentialSnapIds]);
+
   return (
     <table className="p-table--mobile-card">
       <thead>
         <tr>
           <th>Published in</th>
-          <th>Name</th>
+          <th className="table-cell-head--checkbox">
+            <CheckboxInput
+              onChange={(e) => {
+                if (e.target.checked) {
+                  const otherStoresSnaps = otherStores.map(
+                    (item) => item.snaps
+                  );
+                  setSnapsToRemove(
+                    otherStoresSnaps.flat().filter((item) => !item.essential)
+                  );
+                  setCheckAll(true);
+                } else {
+                  setSnapsToRemove([]);
+                  setCheckAll(false);
+                }
+              }}
+              checked={checkAll}
+              disabled={!nonEssentialSnapIds.length}
+            />
+            Name
+          </th>
           <th>Latest release</th>
           <th>Release date</th>
           <th>Publisher</th>
@@ -20,6 +56,7 @@ function SnapsTable({ storeName, snaps, otherStores }) {
           <SnapsTableRow
             key={snap.id}
             storeName={storeName}
+            storeId={id}
             snap={snap}
             snapsCount={snaps.length}
             index={index}
@@ -32,9 +69,12 @@ function SnapsTable({ storeName, snaps, otherStores }) {
               <SnapsTableRow
                 key={snap.id}
                 storeName={store.name}
+                storeId={store.id}
                 snap={snap}
                 snapsCount={store.snaps.length}
                 index={index}
+                snapsToRemove={snapsToRemove}
+                setSnapsToRemove={setSnapsToRemove}
               />
             ));
           })}
@@ -47,6 +87,9 @@ SnapsTable.propTypes = {
   storeName: PropTypes.string.isRequired,
   snaps: PropTypes.array.isRequired,
   otherStores: PropTypes.object,
+  snapsToRemove: PropTypes.array,
+  setSnapsToRemove: PropTypes.func,
+  nonEssentialSnapIds: PropTypes.array.isRequired,
 };
 
 export default SnapsTable;
