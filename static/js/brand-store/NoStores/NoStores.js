2ff299505f03aa4c29e5ddc7c1ea929c47e4e0c7
Steve Rydz <steve.rydz@canonical.com>
2021-10-27 14:10:33 +0100

    Add message to no stores page (#3744)

--- static/js/brand-store/NoStores/NoStores.js
+++ static/js/brand-store/NoStores/NoStores.js
@@ -7,6 +7,15 @@ function NoStores() {
         <div className="p-panel__content">
           <div className="u-fixed-width">
             <h1>No stores</h1>
+            <p>
+              Currently this section can only show stores if you are a viewer or
+              an admin. If you are a publisher or reviewer you will find your
+              stores{" "}
+              <a href="https://dashboard.snapcraft.io/stores/">
+                on the dashboard
+              </a>
+              .
+            </p>
           </div>
         </div>
       </div>
