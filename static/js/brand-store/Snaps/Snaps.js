d1a74e2f3d1c9d65a677311239e42b434406ff13
Steve Rydz <steve.rydz@canonical.com>
2021-11-03 14:46:01 +0000

    Fix brand store mobile (#3752)
    * Fix brand store ID types
    
    * Fix brand store hash change bug

--- static/js/brand-store/Snaps/Snaps.js
+++ static/js/brand-store/Snaps/Snaps.js
@@ -166,12 +166,14 @@ function Snaps() {
     setSnapsInStore(snaps.filter((snap) => snap.store === id));
 
     setOtherStoreIds(
-      new Set(
-        snaps
-          .filter((snap) => snap.store !== id)
-          .map((snap) => {
-            return snap.store;
-          })
+      Array.from(
+        new Set(
+          snaps
+            .filter((snap) => snap.store !== id)
+            .map((snap) => {
+              return snap.store;
+            })
+        )
       )
     );
 
@@ -188,7 +190,7 @@ function Snaps() {
 
   useEffect(() => {
     setOtherStores(
-      Array.from(otherStoreIds).map((storeId) => {
+      otherStoreIds.map((storeId) => {
         return {
           id: storeId,
           name: getStoreName(storeId),
@@ -199,9 +201,12 @@ function Snaps() {
   }, [otherStoreIds]);
 
   useEffect(() => {
-    setSnapsInStore([]);
-    setOtherStores([]);
-    setIsReloading(true);
+    // protect against hash changes e.g. mobile navigation
+    if (location.pathname !== `/admin/${id}/snaps`) {
+      setSnapsInStore([]);
+      setOtherStores([]);
+      setIsReloading(true);
+    }
   }, [location]);
 
   useEffect(() => {
