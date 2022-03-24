c1592ca22a911d7ba799a1ebf601582b124bd7ce
Anthony Dillon <me@anthonydillon.com>
2022-03-24 14:12:30 +0000

    Fix the layout of the first user flow publish to store step

--- static/js/public/first-snap-flow.js
+++ static/js/public/first-snap-flow.js
@@ -263,9 +263,7 @@ function initRegisterName(formEl, notificationEl, successEl) {
     );
 
     const currentPanel = formEl.closest(".p-accordion__group");
-    const currentToggle = currentPanel.querySelector(
-      ".p-accordion__tab"
-    );
+    const currentToggle = currentPanel.querySelector(".p-accordion__tab");
     const nextPanel = currentPanel.nextElementSibling;
     const nextToggle = nextPanel.querySelector(".p-accordion__tab");
 
