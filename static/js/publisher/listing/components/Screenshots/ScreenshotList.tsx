37c4d6d8e99fc037831c21ef57d4ed5d15ab1364
Steve Rydz <steve.rydz@canonical.com>
2022-08-25 16:34:39 +0100

    Allow screenshot reordering on listing page (#4083)

---
+++ static/js/publisher/listing/components/Screenshots/ScreenshotList.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  horizontalListSortingStrategy,
+} from "@dnd-kit/sortable";
+
+import Screenshot from "./Screenshot";
+
+type Props = {
+  screenshots: Array<{ id: string }>;
+  screenshotUrls: Array<string>;
+  getValues: Function;
+  removeScreenshotUrl: Function;
+  setValue: Function;
+  register: Function;
+  setImage: Function;
+  moveScreenshotUrl: Function;
+};
+
+function ScreenshotList({
+  screenshots,
+  screenshotUrls,
+  getValues,
+  removeScreenshotUrl,
+  setValue,
+  register,
+  setImage,
+  moveScreenshotUrl,
+}: Props) {
+  const [items, setItems] = useState(screenshots);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, { coordinateGetter: sortableKeyboardCoordinates })
+  );
+
+  const handleDragEnd = (e: any) => {
+    const { active, over } = e;
+
+    if (active.id !== over.id) {
+      const oldIndex = items.findIndex((item) => item.id === active.id);
+      const newIndex = items.findIndex((item) => item.id === over.id);
+
+      moveScreenshotUrl(oldIndex, newIndex);
+
+      setItems((items) => {
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  };
+
+  return (
+    <DndContext
+      sensors={sensors}
+      collisionDetection={closestCenter}
+      onDragEnd={handleDragEnd}
+    >
+      <SortableContext items={items} strategy={horizontalListSortingStrategy}>
+        <div className="p-listing-images">
+          {items.map((field, index) => (
+            <Screenshot
+              key={field.id}
+              field={field}
+              index={index}
+              screenshotUrls={screenshotUrls}
+              getValues={getValues}
+              removeScreenshotUrl={removeScreenshotUrl}
+              setValue={setValue}
+              register={register}
+              setImage={setImage}
+            />
+          ))}
+        </div>
+      </SortableContext>
+    </DndContext>
+  );
+}
+
+export default ScreenshotList;
