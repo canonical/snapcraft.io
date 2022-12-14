a6f5aa1965ab7287d4b3d0402e68d6c45ffa1bb2
Lukewh <luke@lukewh.com>
2022-12-14 11:46:31 +0000

    Verbose logging

--- webapp/decorators.py
+++ webapp/decorators.py
@@ -4,6 +4,7 @@ import functools
 # Third party packages
 import flask
 from webapp import authentication
+from talisker import logging
 
 
 def login_required(func):
@@ -18,6 +19,32 @@ def login_required(func):
         login_path = "login-beta" if last_login_method == "candid" else "login"
 
         if not authentication.is_authenticated(flask.session):
+            missing_session_data = []
+            if "publisher" not in flask.session:
+                missing_session_data.append("publisher")
+
+            if "macaroons" not in flask.session:
+                missing_session_data.append("macaroons")
+
+            if "macaroon_discharge" not in flask.session:
+                missing_session_data.append("macaroon_discharge")
+
+            if "macaroon_root" not in flask.session:
+                missing_session_data.append("macaroon_root")
+
+            if missing_session_data:
+                logging.getLogger("talisker.wsgi").error(
+                    "User not authenticated",
+                    extra={
+                        "missing_session_data": missing_session_data,
+                        "email": (
+                            flask.session["publisher"]["email"]
+                            if "publisher" in flask.session
+                            else None
+                        ),
+                    },
+                )
+
             authentication.empty_session(flask.session)
             return flask.redirect(f"/{login_path}?next={flask.request.path}")
 
