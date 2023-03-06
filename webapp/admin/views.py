5dcbec923471450a8546a36aba0362ad9345fd84
Lukewh <luke@lukewh.com>
2023-03-06 15:39:48 +0000

    Add "Fully included stores" section

--- webapp/admin/views.py
+++ webapp/admin/views.py
@@ -89,10 +89,7 @@ def get_store_snaps(store_id):
         for item in store["store-whitelist"]:
             store_item = admin_api.get_store(flask.session, item)
             if store_item:
-                print(store_item)
-                included_stores.append(
-                    {"id": store_item["id"], "name": store_item["name"]}
-                )
+                included_stores.append({"id": store_item["id"], "name": store_item["name"]})
 
         if included_stores:
             snaps.append({"included-stores": included_stores})
