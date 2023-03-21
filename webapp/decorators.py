6a4466b4cf0ba22a895ee20dc494335ece21ccbd
Lukewh <luke@lukewh.com>
2023-03-21 15:05:16 +0000

    Remove unauthenticated logging

--- webapp/decorators.py
+++ webapp/decorators.py
@@ -19,32 +19,6 @@ def login_required(func):
         login_path = "login-beta" if last_login_method == "candid" else "login"
 
         if not authentication.is_authenticated(flask.session):
-            missing_session_data = []
-            if "publisher" not in flask.session:
-                missing_session_data.append("publisher")
-
-            if "macaroons" not in flask.session:
-                missing_session_data.append("macaroons")
-
-            if "macaroon_discharge" not in flask.session:
-                missing_session_data.append("macaroon_discharge")
-
-            if "macaroon_root" not in flask.session:
-                missing_session_data.append("macaroon_root")
-
-            if missing_session_data:
-                logging.getLogger("talisker.wsgi").error(
-                    "User not authenticated",
-                    extra={
-                        "missing_session_data": missing_session_data,
-                        "email": (
-                            flask.session["publisher"]["email"]
-                            if "publisher" in flask.session
-                            else None
-                        ),
-                    },
-                )
-
             authentication.empty_session(flask.session)
             return flask.redirect(f"/{login_path}?next={flask.request.path}")
 
