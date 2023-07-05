03e2e704b277d1838476ba0d731b61ec3833d171
Steve Rydz <steve.rydz@canonical.com>
2023-07-05 16:03:09 +0100

    Add create new model view (#4321)

--- static/js/brand-store/components/Models/Models.test.tsx
+++ static/js/brand-store/components/Models/Models.test.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import "@testing-library/jest-dom";
 
@@ -15,31 +17,83 @@ jest.mock("react-router-dom", () => {
   };
 });
 
-describe("Models", () => {
-  it("displays a filter input", () => {
-    render(
+function renderComponent() {
+  return render(
+    <RecoilRoot>
       <BrowserRouter>
         <Models />
       </BrowserRouter>
+    </RecoilRoot>
+  );
+}
+
+describe("Models", () => {
+  it("displays a link to create a new model", () => {
+    renderComponent();
+    expect(
+      screen.getByRole("link", { name: "Create new model" })
+    ).toBeInTheDocument();
+  });
+
+  it("shows a create model form when  'Create new model' button clicked", () => {
+    const user = userEvent.setup();
+    renderComponent();
+    user.click(screen.getByRole("link", { name: "Create new model" }));
+    expect(screen.getByRole("textbox", { name: "Name" })).toBeInTheDocument();
+    expect(
+      screen.getByRole("textbox", { name: "API key" })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: "Generate key" })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("button", { name: "Add model" })
+    ).toBeInTheDocument();
+  });
+
+  it("disables 'Add model' button if no new model name", async () => {
+    const user = userEvent.setup();
+    renderComponent();
+    await user.click(screen.getByRole("link", { name: "Create new model" }));
+    expect(screen.getByRole("button", { name: "Add model" })).toBeDisabled();
+  });
+
+  it("enables 'Add model' button if there is a new model name", async () => {
+    const user = userEvent.setup();
+    renderComponent();
+    await user.click(screen.getByRole("link", { name: "Create new model" }));
+    await user.type(
+      screen.getByRole("textbox", { name: "Name" }),
+      "test-model-name"
     );
+    expect(
+      screen.getByRole("button", { name: "Add model" })
+    ).not.toBeDisabled();
+  });
+
+  it("generates an API key when clicking 'Generate key'", async () => {
+    const user = userEvent.setup();
+    renderComponent();
+    await user.click(screen.getByRole("link", { name: "Create new model" }));
+    await user.click(screen.getByRole("button", { name: "Generate key" }));
+    const input: HTMLInputElement = screen.getByRole("textbox", {
+      name: "API key",
+    });
+    expect(input.value.length).toEqual(50);
+  });
+
+  it("displays a filter input", () => {
+    renderComponent();
     expect(screen.getByLabelText("Search models")).toBeInTheDocument();
   });
 
   it("populates filter with the filter query parameter", () => {
-    render(
-      <BrowserRouter>
-        <Models />
-      </BrowserRouter>
-    );
+    renderComponent();
     expect(screen.getByLabelText("Search models")).toHaveValue(mockFilterQuery);
   });
 
   it("displays a table of models", () => {
-    render(
-      <BrowserRouter>
-        <Models />
-      </BrowserRouter>
-    );
+    renderComponent();
     expect(screen.getByTestId("models-table")).toBeInTheDocument();
   });
 });
--- static/js/brand-store/components/Models/Models.tsx
+++ static/js/brand-store/components/Models/Models.tsx
@@ -1,119 +1,279 @@
-import React from "react";
-import { Link, useParams, useSearchParams } from "react-router-dom";
+import React, { useState } from "react";
+import { useRecoilState } from "recoil";
+import {
+  Link,
+  useParams,
+  useSearchParams,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
 import { format } from "date-fns";
-import { MainTable, SearchBox, Row, Col } from "@canonical/react-components";
+import randomstring from "randomstring";
+import {
+  MainTable,
+  Row,
+  Col,
+  Button,
+  Input,
+  Icon,
+} from "@canonical/react-components";
+
+import { brandStoresState } from "../../atoms";
 
 import SectionNav from "../SectionNav";
 
-import { getFilteredModels, maskString } from "../../utils";
+import {
+  checkModelNameExists,
+  getFilteredModels,
+  isClosedPanel,
+  maskString,
+} from "../../utils";
 
-import type { Model } from "../../types/shared";
+import type { Model, Store } from "../../types/shared";
 
 // This is temporary until the API is connected
 import modelsData from "./models-data";
 
 function Models() {
   const { id } = useParams();
+  const location = useLocation();
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
   const models = getFilteredModels(modelsData.data, searchParams.get("filter"));
 
+  const [newModelName, setNewModelName] = useState("");
+  const [apiKey, setApiKey] = useState("");
+
+  const stores = useRecoilState(brandStoresState);
+  const currentStore = stores[0].find((store: Store) => store.id === id);
+
   return (
-    <main className="l-main">
-      <div className="p-panel">
-        <div className="p-panel__content">
-          <div className="u-fixed-width">
-            <SectionNav sectionName="models" />
-          </div>
-          <Row>
-            <Col size={6}>
-              {/* Placeholder for "Create new model" button */}
-            </Col>
-            <Col size={6}>
-              <SearchBox
-                placeholder="Search models"
-                autoComplete="off"
-                value={searchParams.get("filter") || ""}
-                onChange={(value) => {
-                  if (value) {
-                    setSearchParams({
-                      filter: value,
-                    });
-                  } else {
-                    setSearchParams();
-                  }
-                }}
-              />
-            </Col>
-          </Row>
-          <div className="u-fixed-width">
-            {models.length > 0 && (
-              <MainTable
-                data-testid="models-table"
-                sortable
-                paginate={10}
-                emptyStateMsg="Fetching models data..."
-                headers={[
-                  {
-                    content: `Name (${models.length})`,
-                    sortKey: "name",
-                  },
-                  {
-                    content: "API key",
-                    className: "u-align--right",
-                  },
-                  {
-                    content: "Last updated",
-                    className: "u-align--right",
-                    sortKey: "modified-at",
-                  },
-                  {
-                    content: "Created date",
-                    className: "u-align--right",
-                    sortKey: "created-at",
-                  },
-                ]}
-                rows={models.map((model: Model) => {
-                  return {
-                    columns: [
-                      {
-                        content: (
-                          <Link to={`/admin/${id}/models/${model.name}`}>
-                            {model.name}
-                          </Link>
-                        ),
-                      },
-                      {
-                        content: maskString(model["api-key"]),
-                        className: "u-align--right",
-                      },
-                      {
-                        content: format(
-                          new Date(model["modified-at"]),
-                          "dd/MM/yyyy"
-                        ),
-                        className: "u-align--right",
-                      },
-                      {
-                        content: format(
-                          new Date(model["created-at"]),
-                          "dd/MM/yyyy"
-                        ),
-                        className: "u-align--right",
-                      },
-                    ],
-                    sortData: {
-                      name: model.name,
-                      "modified-at": model["modified-at"],
-                      "created-at": model["created-at"],
+    <>
+      <main className="l-main">
+        <div className="p-panel">
+          <div className="p-panel__content">
+            <div className="u-fixed-width">
+              <SectionNav sectionName="models" />
+            </div>
+            <Row>
+              <Col size={6}>
+                <Link className="p-button" to={`/admin/${id}/models/create`}>
+                  Create new model
+                </Link>
+              </Col>
+              <Col size={6}>
+                <div className="p-search-box">
+                  <Input
+                    type="search"
+                    id="search"
+                    className="p-search-box__input"
+                    label="Search models"
+                    labelClassName="u-off-screen"
+                    placeholder="Search models"
+                    autoComplete="off"
+                    value={searchParams.get("filter") || ""}
+                    onChange={(e) => {
+                      if (e.target.value) {
+                        setSearchParams({ filter: e.target.value });
+                      } else {
+                        setSearchParams();
+                      }
+                    }}
+                  />
+                  <Button
+                    type="reset"
+                    className="p-search-box__reset"
+                    onClick={() => {
+                      setSearchParams();
+                    }}
+                  >
+                    <Icon name="close">Close</Icon>
+                  </Button>
+                  <Button type="submit" className="p-search-box__button">
+                    <Icon name="search">Search</Icon>
+                  </Button>
+                </div>
+              </Col>
+            </Row>
+            <div className="u-fixed-width">
+              {models.length > 0 && (
+                <MainTable
+                  data-testid="models-table"
+                  sortable
+                  paginate={10}
+                  emptyStateMsg="Fetching models data..."
+                  headers={[
+                    {
+                      content: `Name (${models.length})`,
+                      sortKey: "name",
                     },
-                  };
-                })}
-              />
-            )}
+                    {
+                      content: "API key",
+                      className: "u-align--right",
+                    },
+                    {
+                      content: "Last updated",
+                      className: "u-align--right",
+                      sortKey: "modified-at",
+                    },
+                    {
+                      content: "Created date",
+                      className: "u-align--right",
+                      sortKey: "created-at",
+                    },
+                  ]}
+                  rows={models.map((model: Model) => {
+                    return {
+                      columns: [
+                        {
+                          content: (
+                            <Link to={`/admin/${id}/models/${model.name}`}>
+                              {model.name}
+                            </Link>
+                          ),
+                        },
+                        {
+                          content: maskString(model["api-key"]),
+                          className: "u-align--right",
+                        },
+                        {
+                          content: format(
+                            new Date(model["modified-at"]),
+                            "dd/MM/yyyy"
+                          ),
+                          className: "u-align--right",
+                        },
+                        {
+                          content: format(
+                            new Date(model["created-at"]),
+                            "dd/MM/yyyy"
+                          ),
+                          className: "u-align--right",
+                        },
+                      ],
+                      sortData: {
+                        name: model.name,
+                        "modified-at": model["modified-at"],
+                        "created-at": model["created-at"],
+                      },
+                    };
+                  })}
+                />
+              )}
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+      <div
+        className={`l-aside__overlay ${
+          isClosedPanel(location.pathname, "create") ? "u-hide" : ""
+        }`}
+        onClick={() => {
+          navigate(`/admin/${id}/models`);
+          setNewModelName("");
+          setApiKey("");
+        }}
+      ></div>
+      <aside
+        className={`l-aside ${
+          isClosedPanel(location.pathname, "create") ? "is-collapsed" : ""
+        }`}
+        id="aside-panel"
+      >
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log("Submit");
+          }}
+          style={{ height: "100%" }}
+        >
+          <div className="p-panel is-flex-column">
+            <div className="p-panel__header">
+              <h4 className="p-panel__title">Create new model</h4>
+            </div>
+            <div className="p-panel__content">
+              <div className="u-fixed-width">
+                {currentStore && (
+                  <p>
+                    Brand
+                    <br />
+                    {currentStore.name}
+                  </p>
+                )}
+                <Input
+                  type="text"
+                  id="model-name-field"
+                  placeholder="e.g. display-name-123"
+                  label="Name"
+                  help="Name should contain lowercase alphanumeric characters and hyphens only"
+                  value={newModelName}
+                  onChange={(e) => {
+                    const value = e.target.value;
+                    setNewModelName(value);
+                  }}
+                  error={
+                    checkModelNameExists(newModelName, models)
+                      ? `Model ${newModelName} already exists`
+                      : null
+                  }
+                  required
+                />
+                <Input
+                  type="text"
+                  id="api-key-field"
+                  label="API key"
+                  value={apiKey}
+                  placeholder="yx6dnxsWQ3XUB5gza8idCuMvwmxtk1xBpa9by8TuMit5dgGnv"
+                  className="read-only-dark"
+                  style={{ color: "#000" }}
+                  readOnly
+                />
+                <Button
+                  type="button"
+                  onClick={() => {
+                    setApiKey(
+                      randomstring.generate({
+                        length: 50,
+                      })
+                    );
+                  }}
+                >
+                  Generate key
+                </Button>
+              </div>
+            </div>
+            <div className="u-fixed-width">
+              <p>* Mandatory field</p>
+            </div>
+            <hr />
+            <div className="p-panel__footer u-align--right">
+              <div className="u-fixed-width">
+                <Button
+                  className="u-no-margin--bottom"
+                  onClick={() => {
+                    navigate(`/admin/${id}/models`);
+                    setNewModelName("");
+                    setApiKey("");
+                  }}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  type="submit"
+                  appearance="positive"
+                  className="u-no-margin--bottom"
+                  disabled={
+                    !newModelName || checkModelNameExists(newModelName, models)
+                  }
+                >
+                  Add model
+                </Button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </aside>
+    </>
   );
 }
 
