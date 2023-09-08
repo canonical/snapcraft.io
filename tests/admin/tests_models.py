6274b162dac3e57aeea04cdafd5351cf0b699dc2
ALIMOT AKINBODE <halof.halima@gmail.com>
2023-09-08 11:37:06 +0300

    update tests for model services

--- tests/admin/tests_models.py
+++ tests/admin/tests_models.py
@@ -14,18 +14,19 @@ candid = CandidClient(api_publisher_session)
 
 
 class TestModelServiceEndpoints(unittest.TestCase):
-
     def _candid_log_in(self, client):
-        """Emulates test client login in the store.
-
-        Fill current session with `openid`, `macaroon_root` and
-        `macaroon_discharge`.
-
-        Return the expected `Authorization` header for further verification
-        in API requests.
         """
-        
-        test_macaroon = "AgEQYXBpLnNuYXBjcmFmdC5pbwImAwoQUWyZ4S4wHL6zykGguk4n-RIBMBoOCgVsb2dpbhIFbG9naW4AAid0aW1lLWJlZm9yZSAyMDI0LTA3LTEwVDEzOjMyOjA4LjUxMzE0OFoAAiZ0aW1lLXNpbmNlIDIwMjMtMDctMTFUMTM6MzI6MDguNTEzMTQ4WgACL3Nlc3Npb24taWQgZWI2YWQyOGItMjAxOS00ZjA3LThmOTMtNTRlMTQzODExMWUwAAI5ZGVjbGFyZWQgdXNlcmlkIHVzc286aHR0cHM6Ly9sb2dpbi51YnVudHUuY29tLytpZC9uNnp0Y3lMAAIZZXh0cmEgeyJwZXJtaXNzaW9ucyI6IFtdfQAABiC_ER3fu5Lfo1XdcXG340Ah18sY41_prGOr-ndF-AkKUQ"
+        mocks candid login
+        """
+
+        test_macaroon = """
+            AgEQYXBpLnNuYXBjcmFmdC5pbwImAwoQUWyZ4S4wHL6zykGguk4n-
+            RIBMBoOCgVsb2dpbhIFbG9naW4AAid0aW1lLWJlZm9yZSAyMDI0LTA3LTEwVDEzOjMyOjA4L
+            jUxMzE0OFoAAiZ0aW1lLXNpbmNlIDIwMjMtMDctMTFUMTM6MzI6MDguNTEzMTQ4WgACL3Nlc3
+            Npb24taWQgZWI2YWQyOGItMjAxOS00ZjA3LThmOTMtNTRlMTQzODExMWUwAAI5ZGVjbGFyZWQ
+            gdXNlcmlkIHVzc286aHR0cHM6Ly9sb2dpbi51YnVudHUuY29tLytpZC9uNnp0Y3lMAAIZZXh0
+            cmEgeyJwZXJtaXNzaW9ucyI6IFtdfQAABiC_ER3fu5Lfo1XdcXG340Ah18sY41_prGOr-ndF-AkKUQ
+        """
         with client.session_transaction() as s:
             s["publisher"] = {
                 "account_id": "test_account_id",
@@ -39,7 +40,7 @@ class TestModelServiceEndpoints(unittest.TestCase):
             s["developer_token"] = test_macaroon
 
         return f"Macaroon {test_macaroon}"
-    
+
     def setUp(self):
         self.app = create_app(testing=True)
         self.client = self.app.test_client()
@@ -47,12 +48,13 @@ class TestModelServiceEndpoints(unittest.TestCase):
         self.api_key = "qwertyuioplkjhgfdsazxcvbnmkiopuytrewqasdfghjklmnbv"
 
 
-
 class TestGetModels(TestModelServiceEndpoints):
-
     @patch("webapp.admin.views.admin_api.get_store_models")
     def test_get_models(self, mock_get_store_models):
-        mock_get_store_models.return_value = {"models": [{"name": "test_model"}]}
+        mock_get_store_models.return_value = {
+            "models": [{"name": "test_model"}]
+        }
+
         response = self.client.get("/admin/store/1/models")
         data = response.json
 
@@ -62,6 +64,7 @@ class TestGetModels(TestModelServiceEndpoints):
     @patch("webapp.admin.views.admin_api.get_store_models")
     def test_store_has_no_models(self, mock_get_store_models):
         mock_get_store_models.return_value = {"models": []}
+
         response = self.client.get("/admin/store/2/models")
         data = response.json["data"]
 
@@ -72,7 +75,12 @@ class TestGetModels(TestModelServiceEndpoints):
 
     @patch("webapp.admin.views.admin_api.get_store_models")
     def test_invalid_store_id(self, mock_get_store_models):
-        mock_get_store_models.side_effect = StoreApiResponseErrorList("Store not found", 404, [{"message": "Store not found"}])
+        mock_get_store_models.side_effect = StoreApiResponseErrorList(
+            "Store not found",
+            404,
+            [{"message": "Store not found"}],
+        )
+
         response = self.client.get("/admin/store/3/models")
         data = response.json
 
@@ -81,26 +89,31 @@ class TestGetModels(TestModelServiceEndpoints):
         self.assertIn("Store not found", data["message"])
 
     @patch("webapp.admin.views.admin_api.get_store_models")
-    def test_unauthorized_user(self,mock_get_store_models ):
-        mock_get_store_models.side_effect = StoreApiResponseErrorList("unauthorized", 401, [{"message": "unauthorized"}])
+    def test_unauthorized_user(self, mock_get_store_models):
+        mock_get_store_models.side_effect = StoreApiResponseErrorList(
+            "unauthorized", 401, [{"message": "unauthorized"}]
+        )
+
         response = self.client.get("/admin/store/1/models")
-        self.assertEqual(response.status_code, 500)
         data = response.json
+
+        self.assertEqual(response.status_code, 500)
         self.assertFalse(data["success"])
         self.assertIn("Store not found", data["message"])
 
-class TestCreateModel(TestModelServiceEndpoints):
 
+class TestCreateModel(TestModelServiceEndpoints):
     @patch("webapp.admin.views.admin_api.create_store_model")
     def test_create_model(self, mock_create_store_model):
         mock_create_store_model.return_value = None
+
         payload = {"name": "Test Model", "api_key": self.api_key}
         response = self.client.post("/admin/store/1/models", data=payload)
         data = response.json
 
         self.assertEqual(response.status_code, 201)
         self.assertTrue(data["success"])
-    
+
     def test_create_model_with_invalid_api_key(self):
         payload = {"name": "Test Model", "api_key": "invalid_api_key"}
         response = self.client.post("/admin/store/1/models", data=payload)
@@ -117,7 +130,9 @@ class TestCreateModel(TestModelServiceEndpoints):
 
         self.assertEqual(response.status_code, 500)
         self.assertFalse(data["success"])
-        self.assertEqual(data["message"], "Name is too long. Limit 128 characters")
+        self.assertEqual(
+            data["message"], "Name is too long. Limit 128 characters"
+        )
 
     def test_missing_name(self):
         payload = {"api_key": self.api_key}
@@ -130,12 +145,14 @@ class TestCreateModel(TestModelServiceEndpoints):
 
 
 class TestUpdateModel(TestModelServiceEndpoints):
-
     @patch("webapp.admin.views.admin_api.update_store_model")
     def test_update_model(self, mock_update_store_model):
         mock_update_store_model.return_value = None
+
         payload = {"api_key": self.api_key}
-        response = self.client.patch("/admin/store/1/models/Model1", data=payload)
+        response = self.client.patch(
+            "/admin/store/1/models/Model1", data=payload
+        )
         data = response.json
 
         self.assertEqual(response.status_code, 200)
@@ -143,7 +160,9 @@ class TestUpdateModel(TestModelServiceEndpoints):
 
     def test_update_model_with_invalid_api_key(self):
         payload = {"api_key": "invalid_api_key"}
-        response = self.client.patch("/admin/store/1/models/Model1", data=payload)
+        response = self.client.patch(
+            "/admin/store/1/models/Model1", data=payload
+        )
         data = response.json
 
         self.assertEqual(response.status_code, 500)
@@ -152,9 +171,14 @@ class TestUpdateModel(TestModelServiceEndpoints):
 
     @patch("webapp.admin.views.admin_api.update_store_model")
     def test_model_not_found(self, mock_update_store_model):
-        mock_update_store_model.side_effect = StoreApiResourceNotFound("Model not found", 404, [{"message": "Model not found"}])
+        mock_update_store_model.side_effect = StoreApiResourceNotFound(
+            "Model not found", 404, [{"message": "Model not found"}]
+        )
+
         payload = {"api_key": self.api_key}
-        response = self.client.patch("/admin/store/1/models/Model1", data=payload)
+        response = self.client.patch(
+            "/admin/store/1/models/Model1", data=payload
+        )
         data = response.json
 
         self.assertEqual(response.status_code, 500)
--- tests/admin/tests_policies.py
+++ tests/admin/tests_policies.py
@@ -1,101 +1,145 @@
-from unittest.mock import patch
+from unittest.mock import Mock, patch
+from canonicalwebteam.store_api.exceptions import StoreApiResponseErrorList
+
 from tests.admin.tests_models import TestModelServiceEndpoints
-from canonicalwebteam.store_api.exceptions import (
-    StoreApiResponseErrorList,
-    StoreApiResourceNotFound,
-)
 
 
 class TestGetPolicies(TestModelServiceEndpoints):
-
-    @patch('webapp.admin.views.admin_api.get_store_model_policies')
+    @patch("webapp.admin.views.admin_api.get_store_model_policies")
     def test_get_policies(self, mock_get_store_model_policies):
         mock_get_store_model_policies.return_value = ["policy1", "policy2"]
-        response = self.client.get('/admin/store/1/models/Model1/policies')
+
+        response = self.client.get("/admin/store/1/models/Model1/policies")
         data = response.json
 
         self.assertEqual(response.status_code, 200)
-        self.assertTrue(data['success'])
-        self.assertEqual(data['data'], ["policy1", "policy2"])
-
-    @patch('webapp.admin.views.admin_api.get_store_model_policies')
-    def test_failed_get_policies(self, mock_get_store_model_policies):
-        mock_get_store_model_policies.side_effect = StoreApiResponseErrorList('An error occurred', 500, [{'message': 'An error occurred'}])
-
-        response = self.client.get('/admin/store/1/models/Model1/policies')
-        self.assertEqual(response.status_code, 500)
-        data = response.get_json()
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'], 'An error occurred')
+        self.assertTrue(data["success"])
+        self.assertEqual(data["data"], ["policy1", "policy2"])
 
     @patch("webapp.admin.views.admin_api.get_store_model_policies")
-    def test_model_not_found(self, mock_get_store_model_policies):
-        mock_get_store_model_policies.side_effect = StoreApiResourceNotFound(
-            "Model not found",
-            404,
-            [{"message": "Model not found"}]
+    def test_failed_get_policies(self, mock_get_store_model_policies):
+        mock_get_store_model_policies.side_effect = StoreApiResponseErrorList(
+            "An error occurred", 500, [{"message": "An error occurred"}]
         )
-        payload = {"api_key": self.api_key}
-        response = self.client.patch("/admin/store/1/models/Model1", data=payload)
+
+        response = self.client.get("/admin/store/1/models/Model1/policies")
         data = response.json
 
         self.assertEqual(response.status_code, 500)
         self.assertFalse(data["success"])
-        self.assertEqual(data["message"], "Model not found")
+        self.assertEqual(data["message"], "An error occurred")
+
 
 class TestCreatePolicies(TestModelServiceEndpoints):
-
-    @patch('webapp.admin.views.admin_api.get_store_signing_keys')
-    @patch('webapp.admin.views.admin_api.create_store_model_policy')
-    def test_create_policy(self, mock_create_store_model_policy, mock_get_store_signing_keys):
-        mock_get_store_signing_keys.return_value = [{"sha3-384": "valid_signing_key"}]
+    @patch("webapp.admin.views.admin_api.get_store_signing_keys")
+    @patch("webapp.admin.views.admin_api.create_store_model_policy")
+    def test_create_policy(
+        self, mock_create_store_model_policy, mock_get_store_signing_keys
+    ):
+        mock_get_store_signing_keys.return_value = [
+            {"sha3-384": "valid_signing_key"}
+        ]
         mock_create_store_model_policy.return_value = None
 
-        payload = {'signing_key': 'valid_signing_key'}
-        response = self.client.post('/admin/store/1/models/Model1/policies', data=payload)
-        self.assertEqual(response.status_code, 200)
-        data = response.get_json()
-        self.assertTrue(data['success'])
+        payload = {"signing_key": "valid_signing_key"}
+        response = self.client.post(
+            "/admin/store/1/models/Model1/policies", data=payload
+        )
+        data = response.json
 
-    
-    @patch('webapp.admin.views.admin_api.get_store_signing_keys')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(data["success"])
+
+    @patch("webapp.admin.views.admin_api.get_store_signing_keys")
     def test_missing_signing_key(self, mock_get_store_signing_keys):
-        mock_get_store_signing_keys.return_value = [{"sha3-384": "valid_signing_key"}]
+        mock_get_store_signing_keys.return_value = [
+            {"sha3-384": "valid_signing_key"}
+        ]
 
         payload = {}
-        response = self.client.post('/admin/store/1/models/Model1/policies', data=payload)
-        data = response.get_json()
+        response = self.client.post(
+            "/admin/store/1/models/Model1/policies", data=payload
+        )
+        data = response.json
 
         self.assertEqual(response.status_code, 500)
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'], 'Signing key required')
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "Signing key required")
 
-    @patch('webapp.admin.views.admin_api.get_store_signing_keys')
-    @patch('webapp.admin.views.admin_api.create_store_model_policy')
-    def test_invalid_signing_key(self, mock_create_store_model_policy, mock_get_store_signing_keys):
+    @patch("webapp.admin.views.admin_api.get_store_signing_keys")
+    def test_invalid_signing_key(self, mock_get_store_signing_keys):
         mock_get_store_signing_keys.return_value = [{"sha3-384": "valid_key"}]
 
-        payload = {'signing_key': 'invalid_key'}
-        response = self.client.post('/admin/store/1/models/Model1/policies', data=payload)
-        data = response.get_json()
+        payload = {"signing_key": "invalid_key"}
+        response = self.client.post(
+            "/admin/store/1/models/Model1/policies", data=payload
+        )
+        data = response.json
 
         self.assertEqual(response.status_code, 500)
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'], 'Invalid signing key')
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "Invalid signing key")
 
-    @patch('webapp.admin.views.admin_api.get_store_signing_keys')
-    @patch('webapp.admin.views.admin_api.create_store_model_policy')
-    def test_exception_in_create_policy(self, mock_create_store_model_policy, mock_get_store_signing_keys):
+    @patch("webapp.admin.views.admin_api.get_store_signing_keys")
+    @patch("webapp.admin.views.admin_api.create_store_model_policy")
+    def test_exception_in_create_policy(
+        self,
+        mock_create_store_model_policy,
+        mock_get_store_signing_keys,
+    ):
         mock_get_store_signing_keys.return_value = [{"sha3-384": "valid_key"}]
+        mock_create_store_model_policy.side_effect = StoreApiResponseErrorList(
+            "Simulated failure", 500, [{"message": "An error occurred"}]
+        )
 
-        mock_create_store_model_policy.side_effect = StoreApiResponseErrorList("Simulated failure", 500, [{"message": "An error occurred"}])
+        payload = {"signing_key": "valid_key"}
+        response = self.client.post(
+            "/admin/store/1/models/Model1/policies", data=payload
+        )
+        data = response.json
 
-        payload = {'signing_key': 'valid_key'}
-        response = self.client.post('/admin/store/1/models/Model1/policies', data=payload)
         self.assertEqual(response.status_code, 500)
-        data = response.get_json()
-        self.assertFalse(data['success'])
-        self.assertEqual(data['message'], 'An error occurred')
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "An error occurred")
+
 
 class TestDeletePolicies(TestModelServiceEndpoints):
-    pass
+    @patch("webapp.admin.views.admin_api.delete_store_model_policy")
+    def test_successful_delete_policy(self, mock_delete_store_model_policy):
+        mock_delete_store_model_policy.return_value = Mock(status_code=204)
+
+        response = self.client.delete(
+            "/admin/store/1/models/Model1/policies/1"
+        )
+        data = response.json
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(data["success"])
+
+    @patch("webapp.admin.views.admin_api.delete_store_model_policy")
+    def test_policy_not_found(self, mock_delete_store_model_policy):
+        mock_delete_store_model_policy.return_value = Mock(status_code=404)
+
+        response = self.client.delete(
+            "/admin/store/1/models/Model1/policies/1"
+        )
+        data = response.json
+
+        self.assertEqual(response.status_code, 500)
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "Policy not found")
+
+    @patch("webapp.admin.views.admin_api.delete_store_model_policy")
+    def test_exception_in_delete_policy(self, mock_delete_store_model_policy):
+        mock_delete_store_model_policy.side_effect = StoreApiResponseErrorList(
+            "An error occured", 500, [{"message": "An error occurred"}]
+        )
+
+        response = self.client.delete(
+            "/admin/store/1/models/Model1/policies/1"
+        )
+        data = response.json
+
+        self.assertEqual(response.status_code, 500)
+        self.assertFalse(data["success"])
+        self.assertEqual(data["message"], "An error occurred")
