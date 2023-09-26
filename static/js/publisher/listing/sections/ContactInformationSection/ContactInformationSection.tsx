6ee3b08d32828f30e491fe6c8b7668ed7a65bb14
Steve Rydz <steve.rydz@canonical.com>
2023-09-26 11:58:17 +0100

    Display metadata links if they exist

--- static/js/publisher/listing/sections/ContactInformationSection/ContactInformationSection.tsx
+++ static/js/publisher/listing/sections/ContactInformationSection/ContactInformationSection.tsx
@@ -17,7 +17,6 @@ function ContactInformationSection({
   control,
 }: Props) {
   const queryParams = new URLSearchParams(window.location.search);
-  const showMetadataLinks = queryParams.get("show_metadata_links");
 
   return (
     <>
@@ -25,71 +24,39 @@ function ContactInformationSection({
         <h2 className="p-heading--4">Contact information</h2>
       </div>
 
-      <ListingFormInput
-        type="url"
-        label="Developer website"
-        name="website"
+      <MultipleInputs
+        fieldName="websites"
+        label="Websites"
         register={register}
-        placeholder="https://snapcraft.io"
-        helpText="Please include a valid http:// or https:// link"
-        getFieldState={getFieldState}
-        pattern={
-          /^https?:\/\/[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/
-        }
-        tourLabel=""
+        control={control}
       />
 
-      {showMetadataLinks && (
-        <>
-          <MultipleInputs
-            fieldName="websites"
-            label="Websites"
-            register={register}
-            control={control}
-          />
-
-          <MultipleInputs
-            fieldName="contacts"
-            label="Contacts"
-            register={register}
-            control={control}
-          />
-
-          <MultipleInputs
-            fieldName="donations"
-            label="Donations"
-            register={register}
-            control={control}
-          />
-
-          <MultipleInputs
-            fieldName="source-code"
-            label="Source code"
-            register={register}
-            control={control}
-          />
-
-          <MultipleInputs
-            fieldName="issues"
-            label="Issues"
-            register={register}
-            control={control}
-          />
-        </>
-      )}
-
-      <ListingFormInput
-        type="text"
-        label={`Contact ${publisherName}`}
-        name="contact"
+      <MultipleInputs
+        fieldName="contacts"
+        label="Contacts"
         register={register}
-        placeholder="mailto:example@example.com"
-        helpText="An http: or https: link, or an e-mail address"
-        getFieldState={getFieldState}
-        pattern={
-          /(^https?:\/\/[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$)|(^mailto:)?([a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+(?:\?[a-zA-Z0-9-=&]+)*$)/
-        }
-        tourLabel=""
+        control={control}
+      />
+
+      <MultipleInputs
+        fieldName="donations"
+        label="Donations"
+        register={register}
+        control={control}
+      />
+
+      <MultipleInputs
+        fieldName="source-code"
+        label="Source code"
+        register={register}
+        control={control}
+      />
+
+      <MultipleInputs
+        fieldName="issues"
+        label="Issues"
+        register={register}
+        control={control}
       />
     </>
   );
