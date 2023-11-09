db5d02ee19578f10973ab61dc5c72ddc031c9aaa
Steve Rydz <steve.rydz@canonical.com>
2023-11-09 16:05:51 +0000

    Show store names

--- static/js/brand-store/components/Snaps/SnapsTable.tsx
+++ static/js/brand-store/components/Snaps/SnapsTable.tsx
@@ -3,49 +3,26 @@ import { useRecoilValue } from "recoil";
 import { format } from "date-fns";
 import { MainTable } from "@canonical/react-components";
 
+import { brandStoresState } from "../../atoms";
 import { filteredSnapsListState } from "../../selectors";
 
-import type { Snap } from "../../types/shared";
+import type { Snap, Store } from "../../types/shared";
 
-function makeRows(group: { store: string; snaps: Array<Snap> }) {
-  return group.snaps.map((snap, index) => {
-    const columns = [];
+function getStoreName(storeId: string, stores: Array<Store>) {
+  const store = stores.find((store) => store.id === storeId);
 
-    if (index === 0) {
-      columns.push({
-        content: group.store,
-        rowSpan: group.snaps.length,
-      });
-    }
+  if (!store) {
+    return storeId;
+  }
 
-    columns.push({
-      content: snap.name || "-",
-    });
-
-    columns.push({
-      content:
-        snap["latest-release"] && snap["latest-release"].version
-          ? snap["latest-release"].version
-          : "-",
-    });
-
-    columns.push({
-      content: format(new Date(snap["latest-release"].timestamp), "dd/MM/yyyy"),
-    });
-
-    columns.push({
-      content: snap.users[0].displayname,
-    });
-
-    return {
-      columns,
-    };
-  });
+  return store.name;
 }
 
 function SnapsTable() {
   const snapsList = useRecoilValue<Array<Snap>>(filteredSnapsListState);
   const stores: string[] = [];
+
+  const brandStores = useRecoilValue<Array<Store>>(brandStoresState);
 
   snapsList.forEach((snap) => {
     if (!stores.includes(snap.store)) {
@@ -61,6 +38,45 @@ function SnapsTable() {
       snaps: snapsList.filter((snap) => snap.store === store),
     });
   });
+
+  const makeRows = (group: { store: string; snaps: Array<Snap> }) => {
+    return group.snaps.map((snap, index) => {
+      const columns = [];
+
+      if (index === 0) {
+        columns.push({
+          content: getStoreName(group.store, brandStores),
+          rowSpan: group.snaps.length,
+        });
+      }
+
+      columns.push({
+        content: snap.name || "-",
+      });
+
+      columns.push({
+        content:
+          snap["latest-release"] && snap["latest-release"].version
+            ? snap["latest-release"].version
+            : "-",
+      });
+
+      columns.push({
+        content: format(
+          new Date(snap["latest-release"].timestamp),
+          "dd/MM/yyyy"
+        ),
+      });
+
+      columns.push({
+        content: snap.users[0].displayname,
+      });
+
+      return {
+        columns,
+      };
+    });
+  };
 
   return (
     <div className="u-flex-grow">
