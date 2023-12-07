42e96d9a73893a18a1dc49080adb242be421d5fa
Steve Rydz <steve.rydz@canonical.com>
2023-12-07 16:41:08 +0000

    Update filters component (#4474)

--- static/js/store/components/Packages/Packages.tsx
+++ static/js/store/components/Packages/Packages.tsx
@@ -118,6 +118,17 @@ function Packages() {
                   }}
                   disabled={isFetching}
                   showFeatured={true}
+                  order={[
+                    "development",
+                    "games",
+                    "social",
+                    "productivity",
+                    "utilities",
+                    "music-and-audio",
+                    "art-and-design",
+                    "photo-and-video",
+                    "server-and-cloud",
+                  ]}
                 />
               </div>
             </div>
