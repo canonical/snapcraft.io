6bd8d601194acd938b863456011285b2040e2b95
Steve Rydz <steve.rydz@canonical.com>
2024-03-01 15:47:58 +0000

    Add application layout skeleton (#4544)

---
+++ static/js/brand-store/components/Layout/Logo.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <a href="/" className="p-panel__logo">
+      <img
+        src="https://assets.ubuntu.com/v1/dae35907-Snapcraft%20tag.svg"
+        alt="Snapcraft logo"
+        className="p-panel__logo-image"
+      />
+      <div className="logo-text p-heading--4 is-fading-when-collapsed">
+        Snapcraft
+      </div>
+    </a>
+  );
+}
+
+export default Logo;
