6cf80f979f1f7b4743b4dcaad97effe904bbb5f2
Abbie Sims <74302970+abbiesims@users.noreply.github.com>
2024-04-16 14:51:39 +0400

    Adds "Add track" functionality (#4596)
    Allows users to add a new track to a snap that already has more than one track. Users will receive a successful or unsuccessful message depending on the outcome.

--- static/js/publisher/release/helpers.js
+++ static/js/publisher/release/helpers.js
@@ -1,5 +1,6 @@
 import { AVAILABLE, REVISION_STATUS } from "./constants";
 import { getChannelString } from "../../libs/channels";
+import { useEffect } from "react";
 
 export function isInDevmode(revision) {
   return revision.confinement === "devmode" || revision.grade === "devel";
@@ -71,4 +72,72 @@ export function canBeReleased(revision) {
   const allowed = [REVISION_STATUS.PUBLISHED, REVISION_STATUS.UNPUBLISHED];
 
   return revision && allowed.includes(revision.status);
+}
+
+export function validatePhasingPercentage(value) {
+  if (value.trim()) {
+    if (isNaN(value)) {
+      return "Phasing percentage must be a number";
+    } else {
+      const percentage = parseFloat(value);
+      if (percentage < 0 || percentage > 100) {
+        return "Phasing percentage must be between 0 and 100";
+      }
+    }
+  }
+  return "";
+}
+
+export function resizeAsidePanel(tracks) {
+  useEffect(() => {
+    function adjustAsidePanelHeight() {
+      const targetComponent = document.querySelector("#main-content");
+      const asidePanel =
+        tracks.length > 1
+          ? document.querySelector("#add-track-aside-panel")
+          : document.querySelector("#request-track-aside-panel");
+      if (targetComponent && asidePanel) {
+        const targetRect = targetComponent.getBoundingClientRect();
+        const targetTop = targetRect.top;
+        const targetBottom = targetRect.bottom;
+        const viewportHeight = window.innerHeight;
+
+        if (targetBottom > viewportHeight) {
+          asidePanel.style.position = "fixed";
+          asidePanel.style.top = `${targetTop}px`;
+          asidePanel.style.bottom = "0";
+        } else {
+          asidePanel.style.position = "sticky";
+          asidePanel.style.top = `${targetTop}px`;
+        }
+      }
+    }
+
+    adjustAsidePanelHeight();
+
+    window.addEventListener("resize", adjustAsidePanelHeight);
+    window.addEventListener("scroll", adjustAsidePanelHeight);
+
+    return () => {
+      window.removeEventListener("resize", adjustAsidePanelHeight);
+      window.removeEventListener("scroll", adjustAsidePanelHeight);
+    };
+  }, [tracks]);
+}
+
+export function numericalSort(a, b) {
+  const regex = /\d+/g;
+  const numSeqA = (a.match(regex) || []).map(Number);
+  const numSeqB = (b.match(regex) || []).map(Number);
+
+  for (let i = 0; i < Math.max(numSeqA.length, numSeqB.length); i++) {
+    const numA = numSeqA[i] || 0;
+    const numB = numSeqB[i] || 0;
+
+    if (numA !== numB) {
+      return numA - numB;
+    }
+  }
+
+  return a.localeCompare(b);
 }
