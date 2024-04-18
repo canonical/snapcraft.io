61824aaed430f10ef76ae8f8bb814678be6e0624
Abbie Sims <74302970+abbiesims@users.noreply.github.com>
2024-04-18 09:06:26 +0400

    Add column sorting to brand store (#4600)
    Changes <table> to React <MainTable> and adds sortable columns to the brand store.

--- static/js/brand-store/components/Snaps/PublishedSnapsTable.tsx
+++ static/js/brand-store/components/Snaps/PublishedSnapsTable.tsx
@@ -1,45 +1,48 @@
 import React from "react";
-import { format } from "date-fns";
+import { format, parseISO } from "date-fns";
 
 import type { Snap } from "../../types/shared";
+import { MainTable } from "@canonical/react-components";
 
 type Props = {
   snapsInStore: Array<Snap>;
 };
 
 function PublishedSnapsTable({ snapsInStore }: Props) {
+
   return (
     <>
       {snapsInStore.length > 0 ? (
-        <table>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Latest release</th>
-              <th>Release date</th>
-              <th>Publisher</th>
-            </tr>
-          </thead>
-          <tbody>
-            {snapsInStore.map((snap: Snap) => (
-              <tr key={snap.id}>
-                <td className="u-truncate">{snap.name}</td>
-                <td className="u-truncate">
-                  {snap["latest-release"] && snap["latest-release"].version
-                    ? snap["latest-release"].version
-                    : "-"}
-                </td>
-                <td className="u-truncate">
-                  {format(
-                    new Date(snap["latest-release"].timestamp),
-                    "dd/MM/yyyy"
-                  )}
-                </td>
-                <td className="u-truncate">{snap.users[0].displayname}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <MainTable
+          sortable
+          headers={[
+            { content: "Name", sortKey: "name" },
+            { content: "Latest release", sortKey: "latestRelease" },
+            { content: "Release date", sortKey: "releaseDate" },
+            { content: "Publisher", sortKey: "publisher" },
+          ]}
+          rows={snapsInStore.map((snap: Snap) => {
+            let releaseDate = null;
+            if (snap["latest-release"] && snap["latest-release"].timestamp) {
+              releaseDate = parseISO(snap["latest-release"].timestamp);
+            }
+            
+            return {
+              columns: [
+                { content: snap.name },
+                { content: snap["latest-release"] && snap["latest-release"].version ? snap["latest-release"].version : "-" },
+                { content: releaseDate ? format(releaseDate, "dd/MM/yyyy") : "-" },                
+                { content: snap.users[0].displayname }
+              ],
+              sortData: {
+                name: snap.name,
+                latestRelease: snap["latest-release"].version,
+                releaseDate: releaseDate || "-",
+                publisher: snap.users[0].displayname,
+              },
+            };
+          })}
+        />
       ) : (
         <p>There are currently no snaps in this store</p>
       )}
