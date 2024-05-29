2e76f3edb3c637be2ebfa102b24983cb122b1ea3
Steve Rydz <steverydz@gmail.com>
2024-05-29 16:11:43 +0100

    fix: Fix compiler warnings in libs JS

--- static/js/libs/iframeSize.ts
+++ static/js/libs/iframeSize.ts
@@ -8,31 +8,32 @@ import debounce from "./debounce";
  *                           It's also used to find the iframe element.
  * @param maxWidth  The maximum width the iframe should go.
  */
-function sizeIframe(wrapperSelector) {
+function sizeIframe(wrapperSelector: string) {
   const wrapperEl = document.querySelector(wrapperSelector);
   if (!wrapperEl) {
     return;
   }
 
-  const iframe = wrapperEl.querySelector("iframe");
+  const iframe = wrapperEl.querySelector("iframe") as HTMLIFrameElement;
 
   // asciinema is a snowflake, so treat it as such
   if (!iframe || (iframe.name && iframe.name.indexOf("asciicast") !== -1)) {
     return;
   }
 
-  const IFRAME_RATIO = iframe.width / iframe.height;
+  const IFRAME_RATIO = parseInt(iframe.width) / parseInt(iframe.height);
 
   const width = wrapperEl.clientWidth;
+  const height = width / IFRAME_RATIO;
 
-  iframe.width = width;
-  iframe.height = width / IFRAME_RATIO;
+  iframe.width = width.toString();
+  iframe.height = height.toString();
 }
 
-export default (wrapperSelector) => {
+export default (wrapperSelector: string) => {
   window.addEventListener(
     "resize",
-    debounce(sizeIframe.bind(this, wrapperSelector), 100),
+    debounce(sizeIframe.bind(this, wrapperSelector), 100)
   );
 
   sizeIframe(wrapperSelector);
