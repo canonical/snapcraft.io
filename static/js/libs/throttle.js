00e600b4166337b58111a035f7db7889e05d9d5d
Steve Rydz <steve.rydz@canonical.com>
2024-06-07 14:40:18 +0100

    chore: Increase code coverage for throttle JS

--- static/js/libs/throttle.js
+++ static/js/libs/throttle.js
@@ -1,9 +1,3 @@
-/**
- * Throttle
- * @param {Function} func Function to run.
- * @param {Number} wait Time to wait between tries.
- */
-
 export default function throttle(func, wait) {
   let time = Date.now();
   return function () {
