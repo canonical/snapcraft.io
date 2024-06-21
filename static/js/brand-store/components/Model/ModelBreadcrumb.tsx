f951b58eeaf59074ea9040bfdd01faa7e0870ff3
Seulkee Kang <seulkeekang@gmail.com>
2024-06-21 18:09:38 +0100

    fix: fix coding standard violations in branstore model breadcrumb

--- static/js/brand-store/components/Model/ModelBreadcrumb.tsx
+++ static/js/brand-store/components/Model/ModelBreadcrumb.tsx
@@ -1,11 +1,15 @@
 import { Link, useParams } from "react-router-dom";
 
-function ModelBreadcrumb() {
-  const { id, model_id } = useParams();
+interface RouteParams {
+  id?: string;
+  model_id?: string;
+}
+function ModelBreadcrumb(): JSX.Element {
+  const { id, model_id } = useParams() as RouteParams;
 
   return (
     <h1 className="p-heading--4">
-      <Link to={`/admin/${id}/models`}>&lsaquo;&nbsp;Models</Link> / {model_id}
+      <Link to={`/admin/${id ?? ''}/models`}>&lsaquo;&nbsp;Models</Link> / {model_id ?? ''}
     </h1>
   );
 }
