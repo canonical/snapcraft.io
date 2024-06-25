51c9b6f364f53cd943490beff53dac1a2322aefc
Seulkee Kang <seulkeekang@gmail.com>
2024-06-25 16:43:02 +0100

    fix: add missing return types and avoid using any

--- static/js/public/snap-details/videos.ts
+++ static/js/public/snap-details/videos.ts
@@ -1,4 +1,8 @@
-function vimeo() {
+interface HolderElement {
+  querySelector: (selector: string) => HTMLIFrameElement | null;
+}
+
+function vimeo(): void {
   const vimeoPlayerScript = document.createElement("script");
   vimeoPlayerScript.src = "https://player.vimeo.com/api/player.js";
   const firstScript = document.getElementsByTagName("script")[0] as HTMLElement;
@@ -7,18 +11,20 @@ function vimeo() {
     firstScript.parentNode.insertBefore(vimeoPlayerScript, firstScript);
   }
 
-  const frame = document.getElementById("vimeoplayer");
+  const frame = document.getElementById("vimeoplayer") as HTMLIFrameElement | null;
 
   const vimeoReady = () => {
-    const player = new window.Vimeo.Player(frame);
-    player.on("play", function () {
-      player.setVolume(0);
-    });
-    player.play();
-  };
+    if (frame && window.Vimeo && window.Vimeo.Player) {
+      const player = new window.Vimeo.Player(frame);
+      player.on("play", function () {
+        player.setVolume(0);
+      });
+      player.play();
+      }
+    };
 
   const checkVimeo = () => {
-    if (window.Vimeo) {
+    if (window.Vimeo && window.Vimeo.Player) {
       vimeoReady();
     } else {
       setTimeout(checkVimeo, 200);
@@ -29,19 +35,18 @@ function vimeo() {
 }
 
 function asciinema(
-  this: any,
-  holderEl: { querySelector: (arg0: string) => any }
-) {
+  holderEl: { querySelector: (arg0: string) => HTMLIFrameElement | null }
+): void {
   const asciinemaPlayer = holderEl.querySelector("iframe");
 
   if (!asciinemaPlayer) {
-    setTimeout(asciinema.bind(this, holderEl), 200);
+    setTimeout(() => asciinema(holderEl), 200);
     return;
   }
 }
 
-function videos(holderSelector: any) {
-  const holderEl = document.querySelector(holderSelector);
+function videos(holderSelector: string): void {
+  const holderEl = document.querySelector(holderSelector) as HTMLElement | null;
 
   if (!holderEl) {
     return;
@@ -49,7 +54,7 @@ function videos(holderSelector: any) {
 
   const videoType = holderEl.dataset.videoType;
 
-  const iframe = holderEl.querySelector("iframe");
+  const iframe = holderEl.querySelector("iframe") as HTMLIFrameElement | null;
   if (iframe && iframe.src && iframe.src.indexOf("http://") !== -1) {
     iframe.src = iframe.src.replace("http://", "https://");
   }
@@ -57,7 +62,7 @@ function videos(holderSelector: any) {
   if (videoType === "vimeo") {
     vimeo();
   } else if (videoType === "asciinema") {
-    asciinema(holderEl);
+    asciinema(holderEl as HolderElement);
   }
 }
 
