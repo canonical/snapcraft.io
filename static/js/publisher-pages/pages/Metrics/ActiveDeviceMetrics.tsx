2838989127eb50b23b637665371b943fef749a50
ilayda-cp <ilayda.cavusoglupars@canonical.com>
2024-09-20 12:59:35 +0300

    Wd 14634 metrics page (#4851)
    * feat: migrate metrics page to react

---
+++ static/js/publisher-pages/pages/Metrics/ActiveDeviceMetrics.tsx
@@ -0,0 +1,118 @@
+import { useParams, useSearchParams } from "react-router-dom";
+import { Row, Col, Spinner, CodeSnippet } from "@canonical/react-components";
+
+import { useEffect, useState } from "react";
+import { renderActiveDevicesMetrics } from "../../../publisher/metrics/metrics";
+import { select } from "d3-selection";
+import ActiveDeviceAnnotation from "./ActiveDeviceAnnotation";
+import { ActiveDeviceMetricFilter } from "./ActiveDeviceMetricFilter";
+import useActiveDeviceMetrics from "../../hooks/useActiveDeviceMetrics";
+
+function ActiveDeviceMetrics({
+  isEmpty,
+  onDataLoad,
+}: {
+  isEmpty: boolean;
+  onDataLoad: (dataLength: number | undefined) => void;
+}): JSX.Element {
+  const { snapId } = useParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [latestActiveDevices, setLatestActiveDevices] = useState<string | null>(
+    null
+  );
+
+  const period = searchParams.get("period") ?? "30d";
+  const type = searchParams.get("active-devices") ?? "version";
+  const selector = "#activeDevices";
+
+  const { status, data, isFetching } = useActiveDeviceMetrics({
+    snapId,
+    period,
+    type,
+  });
+
+  useEffect(() => {
+    if (data) {
+      const activeDevices = data.latest_active_devices;
+      activeDevices &&
+        setLatestActiveDevices(
+          String(activeDevices).replace(/(.)(?=(\d{3})+$)/g, "$1,")
+        );
+
+      data.active_devices &&
+        renderActiveDevicesMetrics({
+          selector,
+          metrics: data.active_devices,
+          type,
+        });
+      onDataLoad(data.active_devices?.buckets?.length);
+    }
+  }, [data]);
+
+  const onChange = (key: string, value: string) => {
+    // clear the chart
+    const svg = select(`${selector} svg`);
+    svg.selectAll("*").remove();
+
+    setSearchParams((searchParams) => {
+      searchParams.set(key, value);
+      return searchParams;
+    });
+  };
+
+  return (
+    <section className={`p-strip is-shallow ${isEmpty ? "is-empty" : ""}`}>
+      <Row>
+        <Col size={12} key="activeServices">
+          <h4 className="u-float-left">Weekly active devices</h4>
+          <div className="p-heading--4 u-float-right u-no-margin--top">
+            <strong>{latestActiveDevices}</strong>
+          </div>
+        </Col>
+        <Col size={12} key="spearator">
+          <hr />
+        </Col>
+        {isFetching ? (
+          <Spinner />
+        ) : (
+          <>
+            <ActiveDeviceMetricFilter
+              isEmpty={isEmpty}
+              onChange={onChange}
+              period={period}
+              type={type}
+            />
+            {isEmpty && <div>No data found.</div>}
+            {status === "error" && (
+              <CodeSnippet
+                blocks={[
+                  {
+                    code: <div>An error occurred. Please try again.</div>,
+                    wrapLines: true,
+                  },
+                ]}
+              />
+            )}
+          </>
+        )}
+
+        <Col size={12} key="info">
+          <div>
+            <div
+              id="activeDevices"
+              className="snapcraft-metrics__graph snapcraft-metrics__active-devices"
+            >
+              <div id="area-holder">
+                <svg width="100%" height="320"></svg>
+              </div>
+            </div>
+
+            <ActiveDeviceAnnotation snapId={snapId} />
+          </div>
+        </Col>
+      </Row>
+    </section>
+  );
+}
+
+export default ActiveDeviceMetrics;
