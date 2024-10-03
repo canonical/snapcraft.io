c067bac299dbcae15105b736e3668750bb585cb3
ilayda-cp <ilayda.cavusoglupars@canonical.com>
2024-10-03 13:30:39 +0300

    Wd 15262 improve performance of metric page (#4863)
    * feat: added downsampling and pagination

--- static/js/publisher-pages/hooks/useActiveDeviceMetrics.ts
+++ static/js/publisher-pages/hooks/useActiveDeviceMetrics.ts
@@ -9,32 +9,122 @@ function useActiveDeviceMetrics({
   period: string;
   type: string;
 }) {
-  return useQuery({
+  const { status, data, isFetching } = useQuery({
     queryKey: ["activeDeviceMetrics", snapId, period, type],
     queryFn: async () => {
-      const response = await fetch(
-        `/${snapId}/metrics/active-devices?period=${period}&active-devices=${type}`
-      );
-
-      if (!response.ok) {
-        if (response.status === 404) {
-          return {
-            latest_active_devices: 0,
-            active_devices: {
-              series: [],
-              buckets: [],
-            },
-          };
-        } else {
-          throw new Error("Unable to fetch active device metrics");
-        }
-      }
-
-      return await response.json();
+      return await fetchData();
     },
     retry: 0,
     refetchOnWindowFocus: false,
   });
+
+  const parsePeriod = (period: string) => {
+    const [_, periodLength, periodTime] = period.trim().split(/(\d+)/);
+    return { periodLength: +periodLength, periodTime };
+  };
+
+  const fetchData = async () => {
+    const { periodTime, periodLength } = parsePeriod(period);
+    const pagePeriodLengthInMonths = 3;
+
+    let totalPage = 1;
+    if (
+      periodTime === "d" ||
+      (periodTime === "m" && periodLength <= pagePeriodLengthInMonths)
+    ) {
+      totalPage = 1;
+    } else {
+      totalPage =
+        periodTime === "y"
+          ? Math.floor((periodLength * 12) / pagePeriodLengthInMonths)
+          : Math.floor(periodLength / pagePeriodLengthInMonths);
+    }
+
+    const responses = [];
+    for (let i = 1; i <= totalPage; i++) {
+      responses.push(
+        fetch(
+          `/${snapId}/metrics/active-devices?active-devices=${type}&period=${period}&page=${i}&page-length=${pagePeriodLengthInMonths}`
+        )
+      );
+    }
+
+    const results = await Promise.all(responses);
+
+    return await formatData(results);
+  };
+
+  const handleResponse = async (response: Response) => {
+    if (!response.ok) {
+      if (response.status === 404) {
+        return {
+          active_devices: {
+            buckets: [],
+            series: [],
+          },
+        };
+      } else {
+        throw new Error("Unable to fetch latest active device information");
+      }
+    }
+
+    const data = await response.json();
+    return data;
+  };
+
+  const formatData = async (results: Response[]) => {
+    const buckets = [];
+    const series = new Map();
+
+    let seriesThatAreAddedBefore = 0;
+
+    for (const result of results.reverse()) {
+      const data = await handleResponse(result);
+
+      const activeDeviceBuckets = data.active_devices.buckets;
+
+      buckets.push(...activeDeviceBuckets);
+      // fill the array with 0's if the batch doesnt have that previous series
+      for (const seriesKey of series.keys()) {
+        const seriesExistInBatch = data.active_devices.series.find(
+          (activeDeviceSeries: { name: string }) =>
+            activeDeviceSeries.name === seriesKey
+        );
+        if (!seriesExistInBatch) {
+          series.set(seriesKey, [
+            ...series.get(seriesKey),
+            ...new Array(activeDeviceBuckets.length).fill(0),
+          ]);
+        }
+      }
+
+      // fill the array with 0's if new series introduced in the batch
+      for (const activeDeviceSeries of data.active_devices.series) {
+        const key = activeDeviceSeries.name;
+        const prevData = series.has(key)
+          ? series.get(key)
+          : new Array(seriesThatAreAddedBefore).fill(0);
+
+        series.set(key, [...prevData, ...activeDeviceSeries.values]);
+      }
+
+      seriesThatAreAddedBefore += activeDeviceBuckets.length;
+    }
+
+    const resultArray = Array.from(series.entries()).map(([key, value]) => ({
+      name: key,
+      values: value,
+    }));
+
+    return {
+      activeDevices: {
+        buckets,
+        series: resultArray,
+      },
+    };
+  };
+
+  return { status, data, isFetching };
 }
 
 export default useActiveDeviceMetrics;
