63dddde3f44a6c27e6c6c8593f2222ed25e48ee8
Steve Rydz <steve.rydz@canonical.com>
2024-10-18 16:34:18 +0100

    Simplify step 2

--- static/js/publisher-pages/pages/Builds/Builds.tsx
+++ static/js/publisher-pages/pages/Builds/Builds.tsx
@@ -1,10 +1,52 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "react-query";
+import { useRecoilState } from "recoil";
+import { Strip } from "@canonical/react-components";
 
 import SectionNav from "../../components/SectionNav";
 import LoggedOut from "./LoggedOut";
+import RepoNotConnected from "./RepoNotConnected";
+import RepoConnected from "./RepoConnected";
+
+import { githubDataState } from "../../state/atoms";
+
+import type { GithubData } from "../../types";
 
 function Builds(): JSX.Element {
   const { snapId } = useParams();
+  const [githubData, setGithubData] = useRecoilState(githubDataState);
+  const { isLoading } = useQuery({
+    queryKey: ["githubData"],
+    queryFn: async () => {
+      const response = await fetch(`/api/${snapId}/repo`);
+
+      if (!response.ok) {
+        setGithubData(null);
+      }
+
+      const responseData = await response.json();
+
+      setGithubData(responseData.data);
+      return responseData.data;
+    },
+    retry: 0,
+  });
+
+  const isLoggedIn = (data: GithubData | null) => {
+    if (!data) {
+      return false;
+    }
+
+    return data.github_user !== null;
+  };
+
+  const repoConnected = (data: GithubData | null) => {
+    if (!data) {
+      return false;
+    }
+
+    return data.github_repository !== null;
+  };
 
   return (
     <>
@@ -15,7 +57,23 @@ function Builds(): JSX.Element {
 
       <SectionNav snapName={snapId} activeTab="builds" />
 
-      <LoggedOut />
+      {isLoading && (
+        <Strip shallow>
+          <p>
+            <i className="p-icon--spinner u-animation--spin"></i>&nbsp;Loading{" "}
+            {snapId} builds data
+          </p>
+        </Strip>
+      )}
+
+      {!isLoading && !isLoggedIn(githubData) && <LoggedOut />}
+
+      {!isLoading && isLoggedIn(githubData) && !repoConnected(githubData) && (
+        <RepoNotConnected />
+      )}
+      {githubData && isLoggedIn(githubData) && repoConnected(githubData) && (
+        <RepoConnected />
+      )}
     </>
   );
 }
