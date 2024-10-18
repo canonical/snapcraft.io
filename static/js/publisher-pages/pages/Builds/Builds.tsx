ef3d3994065eefaaa08b828e8ee18c2e6cee7e24
Steve Rydz <steve.rydz@canonical.com>
2024-10-18 15:50:26 +0100

    Simplify step 1

--- static/js/publisher-pages/pages/Builds/Builds.tsx
+++ static/js/publisher-pages/pages/Builds/Builds.tsx
@@ -1,53 +1,10 @@
 import { useParams } from "react-router-dom";
-import { useQuery } from "react-query";
-import { useRecoilState, useSetRecoilState } from "recoil";
-import { Strip } from "@canonical/react-components";
 
 import SectionNav from "../../components/SectionNav";
 import LoggedOut from "./LoggedOut";
-import RepoNotConnected from "./RepoNotConnected";
-import RepoConnected from "./RepoConnected";
-
-import {
-  buildLoggedInState,
-  buildRepoConnectedState,
-  githubDataState,
-} from "../../state/atoms";
 
 function Builds(): JSX.Element {
   const { snapId } = useParams();
-  const [loggedIn, setLoggedOut] = useRecoilState(buildLoggedInState);
-  const [repoConnected, setRepoConnected] = useRecoilState(
-    buildRepoConnectedState,
-  );
-  const setGithubData = useSetRecoilState(githubDataState);
-
-  const { isLoading, isFetched } = useQuery({
-    queryKey: ["githubData"],
-    queryFn: async () => {
-      const response = await fetch(`/api/${snapId}/repo`);
-
-      if (!response.ok) {
-        return {
-          github_orgs: [],
-          github_repository: null,
-          github_user: null,
-        };
-      }
-
-      const data = await response.json();
-
-      if (data.github_user !== null) {
-        setLoggedOut(true);
-      }
-
-      if (data.github_repository !== null) {
-        setRepoConnected(true);
-      }
-
-      setGithubData(data.data);
-    },
-  });
 
   return (
     <>
@@ -58,18 +15,7 @@ function Builds(): JSX.Element {
 
       <SectionNav snapName={snapId} activeTab="builds" />
 
-      {isLoading && (
-        <Strip shallow>
-          <p>
-            <i className="p-icon--spinner u-animation--spin"></i>&nbsp;Loading{" "}
-            {snapId} builds data
-          </p>
-        </Strip>
-      )}
-
-      {isFetched && !loggedIn && <LoggedOut />}
-      {isFetched && loggedIn && !repoConnected && <RepoNotConnected />}
-      {isFetched && loggedIn && repoConnected && <RepoConnected />}
+      <LoggedOut />
     </>
   );
 }
