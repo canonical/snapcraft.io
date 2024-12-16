28a1cdee79a131364de6d33816ce12cd27b66fc0
Steve Rydz <steve.rydz@canonical.com>
2024-12-16 15:20:44 +0000

    fix: Add loading state to single build page (#4947)

--- static/js/publisher-pages/pages/Build/Build.tsx
+++ static/js/publisher-pages/pages/Build/Build.tsx
@@ -7,7 +7,7 @@ import SectionNav from "../../components/SectionNav";
 
 function Build(): JSX.Element {
   const { buildId, snapId } = useParams();
-  const { data, isFetched } = useQuery({
+  const { data, isFetched, isLoading } = useQuery({
     queryKey: ["build"],
     queryFn: async () => {
       const response = await fetch(`/api/${snapId}/builds/${buildId}`);
@@ -33,56 +33,62 @@ function Build(): JSX.Element {
         <Link to={`/${snapId}/builds`}>Builds</Link> / Build #{buildId}
       </h1>
       <SectionNav activeTab="builds" snapName={snapId} />
+      {isLoading && (
+        <Strip shallow>
+          <p>
+            <i className="p-icon--spinner u-animation--spin"></i>&nbsp;Loading{" "}
+            {snapId} build data
+          </p>
+        </Strip>
+      )}
       <Strip shallow>
         {isFetched && data && (
-          <MainTable
-            headers={[
-              { content: "id" },
-              { content: "Architecture" },
-              { content: "Build duration" },
-              { content: "Result" },
-              { content: "Build finished", className: "u-align-text--right" },
-            ]}
-            rows={[
-              {
-                columns: [
-                  { content: buildId },
-                  { content: data.snap_build.arch_tag },
-                  { content: data.snap_build.duration },
-                  { content: data.snap_build.status },
-                  {
-                    content: formatDistanceToNow(data.snap_build.datebuilt, {
-                      addSuffix: true,
-                    }),
-                    className: "u-align--right",
-                  },
-                ],
-              },
-            ]}
-          />
-        )}
-        <Row>
-          <Col size={6}>
-            <h2 className="p-heading--4">Build log</h2>
-          </Col>
-          <Col size={6} className="u-align-text--right">
-            <a className="p-button--base" href="#footer">
-              Scroll to bottom
-            </a>
-            {isFetched && data && (
-              <a
-                target="_blank"
-                href={data.snap_build.logs}
-                className="p-button"
-                rel="noreferrer"
-              >
-                View raw
-              </a>
-            )}
-          </Col>
-        </Row>
-        {isFetched && data && (
           <>
+            <MainTable
+              headers={[
+                { content: "id" },
+                { content: "Architecture" },
+                { content: "Build duration" },
+                { content: "Result" },
+                { content: "Build finished", className: "u-align-text--right" },
+              ]}
+              rows={[
+                {
+                  columns: [
+                    { content: buildId },
+                    { content: data.snap_build.arch_tag },
+                    { content: data.snap_build.duration },
+                    { content: data.snap_build.status },
+                    {
+                      content: formatDistanceToNow(data.snap_build.datebuilt, {
+                        addSuffix: true,
+                      }),
+                      className: "u-align--right",
+                    },
+                  ],
+                },
+              ]}
+            />
+            <Row>
+              <Col size={6}>
+                <h2 className="p-heading--4">Build log</h2>
+              </Col>
+              <Col size={6} className="u-align-text--right">
+                <a className="p-button--base" href="#footer">
+                  Scroll to bottom
+                </a>
+                {isFetched && data && (
+                  <a
+                    target="_blank"
+                    href={data.snap_build.logs}
+                    className="p-button"
+                    rel="noreferrer"
+                  >
+                    View raw
+                  </a>
+                )}
+              </Col>
+            </Row>
             <pre>{data.raw_logs}</pre>
           </>
         )}
