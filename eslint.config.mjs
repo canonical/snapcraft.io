941fac51d907695f6ffdcdf0fc67061c6531a7c0
Steve Rydz <steve.rydz@canonical.com>
2025-01-17 12:13:18 +0000

    Rename publisher-pages to publisher (#4983)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,90 +14,93 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 export default [
-    {
-        ignores: [
-            "static/js/publisher-pages/pages/Releases",
-            "static/js/publisher-pages/pages/Metrics/metrics",
-            "static/js/public/snap-details/publicise.ts",
-            "static/js/modules",
-            "static/js/dist",
-            "node_modules/*",
-        ],
+  {
+    ignores: [
+      "static/js/publisher/pages/Releases",
+      "static/js/publisher/pages/Metrics/metrics",
+      "static/js/public/snap-details/publicise.ts",
+      "static/js/modules",
+      "static/js/dist",
+      "node_modules/*",
+    ],
+  },
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:jsx-a11y/recommended",
+  ),
+  {
+    plugins: {
+      jest,
+      react,
+      prettier,
+      "jsx-a11y": jsxA11Y,
     },
-    ...compat.extends(
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:prettier/recommended",
-        "plugin:jsx-a11y/recommended"
-    ),
-    {
-        plugins: {
-            jest,
-            react,
-            prettier,
-            "jsx-a11y": jsxA11Y,
+    languageOptions: {
+      globals: Object.fromEntries(
+        Object.entries({
+          ...globals.browser,
+          ...globals.node,
+          ...jest.environments.globals.globals,
+        }).map(([key, value]) => [key.trim(), value]),
+      ),
+      parser: babelParser,
+      ecmaVersion: 2020,
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
         },
-        languageOptions: {
-            globals: Object.fromEntries(
-                Object.entries({
-                    ...globals.browser,
-                    ...globals.node,
-                    ...jest.environments.globals.globals,
-                }).map(([key, value]) => [key.trim(), value])
-            ),
-            parser: babelParser,
-            ecmaVersion: 2020,
-            sourceType: "module",
-            parserOptions: {
-                ecmaFeatures: {
-                    jsx: true,
-                },
-                requireConfigFile: false,
-            },
-        },
-        settings: {
-            react: {
-                version: "detect",
-            },
-        },
-        rules: {
-            "linebreak-style": ["error", "unix"],
-            semi: ["error", "always"],
-            "object-curly-spacing": ["error", "always"],
-            "prettier/prettier": "error",
-            "react/react-in-jsx-scope": "off",
-            "react/no-unescaped-entities": "off",
-            "react/display-name": "off",
-        },
+        requireConfigFile: false,
+      },
     },
-    {
-        files: ["**/*.ts", "**/*.tsx"],
-        plugins: {
-            "@typescript-eslint": typescriptEslint,
-        },
-        languageOptions: {
-            parser: tsParser,
-             ecmaVersion: 2020, 
-        },
-        rules: {
-            ...compat.extends("plugin:@typescript-eslint/recommended").reduce(
-                (rules, config) => ({ ...rules, ...config.rules }),
-                {}
-            ),
-            "@typescript-eslint/no-unused-expressions": ["error", { allowTernary: true }],
-            "@typescript-eslint/no-unused-vars": [
-                "error",
-                {
-                     "argsIgnorePattern": "^_", // Ignore variables starting with an underscore
-                    "caughtErrorsIgnorePattern": "^_" // Ignore `catch` block errors starting with an underscore
-                }
-            ]
-        },
+    settings: {
+      react: {
+        version: "detect",
+      },
     },
+    rules: {
+      "linebreak-style": ["error", "unix"],
+      semi: ["error", "always"],
+      "object-curly-spacing": ["error", "always"],
+      "prettier/prettier": "error",
+      "react/react-in-jsx-scope": "off",
+      "react/no-unescaped-entities": "off",
+      "react/display-name": "off",
+    },
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+    },
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2020,
+    },
+    rules: {
+      ...compat
+        .extends("plugin:@typescript-eslint/recommended")
+        .reduce((rules, config) => ({ ...rules, ...config.rules }), {}),
+      "@typescript-eslint/no-unused-expressions": [
+        "error",
+        { allowTernary: true },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_", // Ignore variables starting with an underscore
+          caughtErrorsIgnorePattern: "^_", // Ignore `catch` block errors starting with an underscore
+        },
+      ],
+    },
+  },
 ];
+
