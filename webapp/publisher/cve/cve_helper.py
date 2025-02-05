fc973b0280e4afda64e7083685aca330b9c56bdf
Ilayda Cavusoglu Pars <ilayda.cavusoglupars@canonical.com>
2025-02-05 11:20:46 +0300

    Add filtering, sorting and pagination to cve

--- webapp/publisher/cve/cve_helper.py
+++ webapp/publisher/cve/cve_helper.py
@@ -18,6 +18,7 @@ publisher_api = SnapPublisher(api_publisher_session)
 REST_API_URL = "https://api.github.com"
 GITHUB_SNAPCRAFT_BOT_USER_TOKEN = getenv("GITHUB_SNAPCRAFT_BOT_USER_TOKEN")
 
+
 class CveHelper:
     """
     Provides CVE data through GitHub by using snapcraft-web@canonical.com.
@@ -32,7 +33,7 @@ class CveHelper:
             return response.json()
         else:
             raise NotFound
-        
+
     def _format_cve_response(self, cve_list, cve_details, usn_details):
         cves = []
         for cve_id in cve_list:
@@ -45,30 +46,38 @@ class CveHelper:
                 for usn_id in cve_usn_list:
                     usn_detail = usn_details[usn_id]
                     if usn_detail:
-                        cve_usns.append({
-                            "id": usn_id,
-                            "description": usn_detail["description"],
-                            "published_at": usn_detail["published_at"],
-                            "related_launchpad_bugs": usn_detail["related_launchpad_bugs"],
-                        })
+                        cve_usns.append(
+                            {
+                                "id": usn_id,
+                                "description": usn_detail["description"],
+                                "published_at": usn_detail["published_at"],
+                                "related_launchpad_bugs": usn_detail[
+                                    "related_launchpad_bugs"
+                                ],
+                            }
+                        )
 
-            cves.append({
-                "id": cve_id,
-                "cvss_score": cve_detail["cvss_score"],
-                "cvss_severity": cve_detail["cvss_severity"],
-                "description": cve_detail["description"],
-                "ubuntu_priority": cve_detail["ubuntu_priority"],
-                "affected_binaries": cve["affected_binaries"],
-                "channels_with_fix": cve["channels_with_fix"],
-                "usns": cve_usns
-            })
+            cves.append(
+                {
+                    "id": cve_id,
+                    "cvss_score": cve_detail["cvss_score"],
+                    "cvss_severity": cve_detail["cvss_severity"],
+                    "description": cve_detail["description"],
+                    "ubuntu_priority": cve_detail["ubuntu_priority"],
+                    "affected_binaries": cve["affected_binaries"],
+                    "channels_with_fix": cve["channels_with_fix"],
+                    "usns": cve_usns,
+                }
+            )
 
         return cves
 
     def _fetch_file_content(self, snap_name, revision, file_metadata):
         if "download_url" in file_metadata:
             download_url = file_metadata["download_url"]
-            headers = {"Authorization": f"token {GITHUB_SNAPCRAFT_BOT_USER_TOKEN}"}
+            headers = {
+                "Authorization": f"token {GITHUB_SNAPCRAFT_BOT_USER_TOKEN}"
+            }
             response = requests.get(download_url, headers=headers)
 
             if response.status_code == 200:
@@ -76,14 +85,18 @@ class CveHelper:
 
                 cve_list = content["snaps"][snap_name]["revisions"][revision]
 
-                fixed_cves = cve_list['fixed-cves']
-                unfixed_cves = cve_list['unfixed-cves']
+                fixed_cves = cve_list["fixed-cves"]
+                unfixed_cves = cve_list["unfixed-cves"]
 
-                cve_details = content['security_issues']['cves']
-                usn_details = content['security_issues']['usns']
+                cve_details = content["security_issues"]["cves"]
+                usn_details = content["security_issues"]["usns"]
 
-                unfixed = self._format_cve_response(unfixed_cves, cve_details, usn_details)
-                fixed = self._format_cve_response(fixed_cves, cve_details, usn_details)
+                unfixed = self._format_cve_response(
+                    unfixed_cves, cve_details, usn_details
+                )
+                fixed = self._format_cve_response(
+                    fixed_cves, cve_details, usn_details
+                )
 
                 cves = fixed + unfixed
 
@@ -94,39 +107,47 @@ class CveHelper:
             raise NotFound
 
     def get_cve_with_revision(self, snap_name, revision):
-        file_metadata = self._get_cve_file_metadata("snap-cves/{}.json".format(snap_name))
+        file_metadata = self._get_cve_file_metadata(
+            "snap-cves/{}.json".format(snap_name)
+        )
 
         if file_metadata:
             return self._fetch_file_content(snap_name, revision, file_metadata)
         return None
-    
+
     def can_user_access_cve_data(self, snap_name):
         snap_details = publisher_api.get_snap_info(snap_name, flask.session)
-        snap_store = snap_details['store']
-        snap_publisher = snap_details['publisher']
+        snap_store = snap_details["store"]
+        snap_publisher = snap_details["publisher"]
 
         account_info = publisher_api.get_account(flask.session)
 
         admin_user_stores = logic.get_stores(
             account_info["stores"], roles=["admin"]
         )
-        is_user_admin = [item for item in admin_user_stores if item["name"] == snap_store] 
+        is_user_admin = [
+            item for item in admin_user_stores if item["name"] == snap_store
+        ]
 
-        GLOBAL_STORE= "Global"
+        GLOBAL_STORE = "Global"
         is_snap_in_global_store = snap_store == GLOBAL_STORE
 
         # check if the snap is publised by canonical
-        CANONICAL_PUBLISHER_ID = 'canonical'
-        is_snap_publisher_canonical = snap_publisher["id"] == CANONICAL_PUBLISHER_ID
+        CANONICAL_PUBLISHER_ID = "canonical"
+        is_snap_publisher_canonical = (
+            snap_publisher["id"] == CANONICAL_PUBLISHER_ID
+        )
 
         # check if the user is the publisher
-        is_user_snap_publisher = snap_publisher["username"] == account_info["username"]
+        is_user_snap_publisher = (
+            snap_publisher["username"] == account_info["username"]
+        )
 
         # check if user canonical
         is_user_canonical = flask.session["publisher"].get(
             "is_canonical", False
         )
-        is_user_collaborator = snap_name in account_info["snaps"]["16"] 
+        is_user_collaborator = snap_name in account_info["snaps"]["16"]
 
         can_view_cves = False
         if is_user_snap_publisher or is_user_admin:
@@ -139,3 +160,133 @@ class CveHelper:
                 can_view_cves = True
 
         return can_view_cves
+
+    def _match_filters(
+        self,
+        cve,
+        usn_ids,
+        binary_statuses,
+        binary_versions,
+        binary_fixed_versions,
+        binary_names,
+        cvss_severities,
+        ubuntu_priorities,
+    ):
+        if usn_ids:
+            if not cve.get("usns") or not any(
+                usn["id"] in usn_ids for usn in cve["usns"]
+            ):
+                return False
+
+        if cvss_severities and cve["cvss_severity"] not in cvss_severities:
+            return False
+
+        if (
+            ubuntu_priorities
+            and cve["ubuntu_priority"] not in ubuntu_priorities
+        ):
+            return False
+
+        if any(
+            [
+                binary_statuses,
+                binary_fixed_versions,
+                binary_versions,
+                binary_names,
+            ]
+        ):
+            if not cve.get("affected_binaries"):
+                return False
+
+            # Check if at least one affected binary matches the filters
+            for binary in cve["affected_binaries"]:
+                matches_binary = (
+                    (
+                        not binary_statuses
+                        or binary["status"] in binary_statuses
+                    )
+                    and (
+                        not binary_versions
+                        or binary["version"] in binary_versions
+                    )
+                    and (
+                        not binary_fixed_versions
+                        or binary["fixed_version"] in binary_fixed_versions
+                    )
+                    and (not binary_names or binary["name"] in binary_names)
+                )
+                if matches_binary:
+                    return True
+            return False
+        return True
+
+    def filter_cve_data(
+        self,
+        cves,
+        usn_ids,
+        binary_statuses,
+        binary_versions,
+        binary_fixed_versions,
+        binary_names,
+        cvss_severities,
+        ubuntu_priorities,
+    ):
+        return [
+            cve
+            for cve in cves
+            if self._match_filters(
+                cve,
+                usn_ids=usn_ids,
+                binary_fixed_versions=binary_fixed_versions,
+                binary_names=binary_names,
+                binary_statuses=binary_statuses,
+                binary_versions=binary_versions,
+                cvss_severities=cvss_severities,
+                ubuntu_priorities=ubuntu_priorities,
+            )
+        ]
+
+    def sort_cve_data(self, cves, sort_by, order):
+        priority_order = {
+            "negligible": 0,
+            "low": 1,
+            "medium": 2,
+            "high": 3,
+            "critical": 4,
+        }
+
+        is_reverse_order = order.lower() == "desc"
+
+        if sort_by == "cvss_severity":
+            cves.sort(
+                key=lambda cve: priority_order.get(cve.get("cvss_severity"), -1),
+                reverse=is_reverse_order,
+            )
+
+        elif sort_by == "ubuntu_priority":
+            cves.sort(
+                key=lambda cve: priority_order.get(cve.get("ubuntu_priority"), -1),
+                reverse=is_reverse_order,
+            )
+        elif sort_by == "cvss_score":
+            cves.sort(
+                key=lambda cve: cve.get("cvss_score", 0), reverse=is_reverse_order
+            )
+        else:
+            cves.sort(
+                key=lambda cve: cve.get(sort_by, ""), reverse=is_reverse_order
+            )
+        return cves
+
+    def paginate_cve_list(self, cves, page, page_size):
+        total_items = len(cves)
+        start = (page - 1) * page_size
+        end = start + page_size
+
+        return {
+            "page": page,
+            "page_size": page_size,
+            "total_items": total_items,
+            "total_pages": (total_items + page_size - 1) // page_size,
+            "data": cves[start:end],
+        }
--- webapp/publisher/cve/cve_views.py
+++ webapp/publisher/cve/cve_views.py
@@ -9,18 +9,74 @@ from webapp.publisher.cve.cve_helper import CveHelper
 
 publisher_api = SnapPublisher(api_publisher_session)
 
+
 @login_required
 def get_cves(snap_name, revision):
- 
-    # add query param filters, pagination, sort
+
+    # Filter params
+    usn_ids = flask.request.args.getlist("usn_id")
+    binary_statuses = flask.request.args.getlist("binary_status")
+    binary_versions = flask.request.args.getlist("binary_version")
+    binary_fixed_versions = flask.request.args.getlist("binary_fixed_version")
+    binary_names = flask.request.args.getlist("binary_name")
+    cvss_severities = flask.request.args.getlist("cvss_severity")
+    ubuntu_priorities = flask.request.args.getlist("ubuntu_priority")
+
+    # Sort params
+    sort_by = flask.request.args.get("sort_by", default="id")
+    order = flask.request.args.get("order", default="desc")
+
+    allowed_fields_for_sort = [
+        "id",
+        "cvss_severity",
+        "cvss_score",
+        "ubuntu_priority",
+    ]
+    if sort_by and sort_by not in allowed_fields_for_sort:
+        return (
+            flask.jsonify(
+                {
+                    "success": False,
+                    "error": "Data can only be sorted by id, cvss_severity, cvss_score, ubuntu_priority",
+                }
+            ),
+            400,
+        )
+
+    allowed_order_params = ["asc", "desc"]
+    if order and order not in allowed_order_params:
+        return (
+            flask.jsonify(
+                {
+                    "success": False,
+                    "error": "'order' param can only be 'asc' or 'desc'",
+                }
+            ),
+            400,
+        )
+
+    # Pagination params
+    page = flask.request.args.get("page", default=1, type=int)
+    page_size = flask.request.args.get("page_size", default=10, type=int)
+
     cve = CveHelper()
 
     can_view_cves = cve.can_user_access_cve_data(snap_name=snap_name)
     if not can_view_cves:
-        return flask.jsonify({"error": "User is not allowed to see snap's CVE data.".format(revision, snap_name)}), 403  
+        return flask.jsonify({"success": False, "error": "User is not allowed to see snap's CVE data.".format(revision, snap_name)}), 403
 
     cves = cve.get_cve_with_revision(snap_name, revision)
-    # print(json.dumps({"cves": cves}, indent=4))
+    cves = cve.filter_cve_data(
+        cves,
+        usn_ids,
+        binary_statuses,
+        binary_versions,
+        binary_fixed_versions,
+        binary_names,
+        cvss_severities,
+        ubuntu_priorities,
+    )
+    cves = cve.sort_cve_data(cves=cves, order=order, sort_by=sort_by)
+    cves = cve.paginate_cve_list(cves=cves, page=page, page_size=page_size)
 
- 
-    return flask.jsonify({"success": True, "data": cves})
+    return flask.jsonify({"success": True, **cves})
