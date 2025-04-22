13885f956143fd6acb13f15e99306976dee3c22c
Steve Rydz <steve.rydz@canonical.com>
2025-04-22 16:22:23 +0100

    chore: Address TiCS coding standards warnings in libs JS (#5107)

--- static/js/libs/formHelpers.ts
+++ static/js/libs/formHelpers.ts
@@ -1,16 +1,16 @@
-function buttonLoading(button: HTMLButtonElement, text: string) {
+function buttonLoading(button: HTMLButtonElement, text: string): void {
   button.disabled = true;
   button.classList.add("--dark");
   button.innerHTML = `<i class='p-icon--spinner u-animation--spin'></i> ${text}`;
 }
 
-function buttonEnabled(button: HTMLButtonElement, text: string) {
+function buttonEnabled(button: HTMLButtonElement, text: string): void {
   button.disabled = false;
   button.classList.remove("--dark");
   button.innerHTML = text;
 }
 
-function buttonDisable(button: HTMLButtonElement, text: string) {
+function buttonDisable(button: HTMLButtonElement, text: string): void {
   button.disabled = true;
   button.innerHTML = text;
 }
