907ce740501076327f21901ecb191ded8fe34f6f
Steve Rydz <steve.rydz@canonical.com>
2025-04-25 13:07:18 +0100

    chore: Remove unnecessary JS file

--- static/js/public/distro-install.ts
+++ static/js/public/distro-install.ts
@@ -2,12 +2,5 @@ import screenshots from "./snap-details/screenshots";
 import videos from "./snap-details/videos";
 import initExpandableArea from "./expandable-area";
 import triggerEventWhenVisible from "./ga-scroll-event";
-import { initLinkScroll } from "./scroll-to";
 
-export {
-  screenshots,
-  initExpandableArea,
-  triggerEventWhenVisible,
-  initLinkScroll,
-  videos,
-};
+export { screenshots, initExpandableArea, triggerEventWhenVisible, videos };
