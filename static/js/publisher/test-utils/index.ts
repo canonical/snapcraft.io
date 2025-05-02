b9ffd8375c463b5fa86e4aa17ab4a402df6d2e7e
Steve Rydz <steve.rydz@canonical.com>
2025-05-02 08:54:17 +0100

    chore: Add tests for publishers releases (#5124)

--- static/js/publisher/test-utils/index.ts
+++ static/js/publisher/test-utils/index.ts
@@ -1,6 +1,11 @@
 import { mockListingData } from "./mockListingData";
 import { mockActiveDeviceMetrics } from "./mockActiveDeviceMetrics";
 import { mockTerritoryMetrics } from "./mockTerritoryMetrics";
+import { mockReleases } from "./mockReleases";
+import { mockRevisions } from "./mockRevisions";
+import { mockReleasedChannels } from "./mockReleasedChannels";
+import { mockReleasesData } from "./mockReleasesData";
+import { mockRevisionsMap } from "./mockRevisionsMap";
 import RecoilObserver from "./RecoilObserver";
 import brandStoreRequests from "./brand-store-requests";
 import {
@@ -20,4 +25,9 @@ export {
   storesResponse,
   mockActiveDeviceMetrics,
   mockTerritoryMetrics,
+  mockReleases,
+  mockRevisions,
+  mockReleasedChannels,
+  mockReleasesData,
+  mockRevisionsMap,
 };
---
+++ static/js/publisher/test-utils/mockReleases.ts
@@ -0,0 +1,32 @@
+export const mockReleases = [
+  {
+    architecture: "arm64",
+    branch: null,
+    channel: "latest/stable",
+    "expiration-date": null,
+    progressive: {
+      "current-percentage": null,
+      paused: null,
+      percentage: null,
+    },
+    revision: 61,
+    risk: "edge",
+    track: "latest",
+    when: "2025-05-01T09:31:37Z",
+  },
+  {
+    architecture: "arm64",
+    branch: "ubuntu-core-desktop",
+    channel: "latest/edge",
+    "expiration-date": null,
+    progressive: {
+      "current-percentage": null,
+      paused: null,
+      percentage: null,
+    },
+    revision: 58,
+    risk: "stable",
+    track: "latest",
+    when: "2025-04-30T18:21:01Z",
+  },
+];
