d1aca1ccb40d4977afefd5d53687f0596b1acb83
Steve Rydz <steve.rydz@canonical.com>
2025-06-12 14:13:47 +0100

    Progressive release UI QA fixes

--- static/js/publisher/pages/Releases/components/releasesConfirmDetails/releaseRow.js
+++ static/js/publisher/pages/Releases/components/releasesConfirmDetails/releaseRow.js
@@ -2,15 +2,13 @@ import PropTypes from "prop-types";
 
 const ReleaseRow = ({ type, revisionInfo, channel }) => (
   <p>
-    <span className="p-release-details-row__type">{type}</span>
-    <span className="p-release-details-row__info">
-      <span className="p-tooltip--btm-center">
-        <b>{revisionInfo.revision}</b> to{" "}
-        <span className="p-tooltip__message">
-          Version: <b>{revisionInfo.version}</b>
-        </span>{" "}
-        <b>{channel}</b> on <b>{revisionInfo.architectures.join(", ")}</b>
-      </span>
+    <span className="p-tooltip--btm-center">
+      {type} <strong>{revisionInfo.revision}</strong> to{" "}
+      <span className="p-tooltip__message">
+        Version: <b>{revisionInfo.version}</b>
+      </span>{" "}
+      <strong>{channel}</strong> on{" "}
+      <strong>{revisionInfo.architectures.join(", ")}</strong>
     </span>
   </p>
 );
