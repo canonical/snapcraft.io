c83434d24f9fb6823fc324c9fd278cf03fc92898
Steve Rydz <steve.rydz@canonical.com>
2025-06-17 15:27:39 +0100

    feat: Add template for explore page (#5170)

--- webapp/store/views.py
+++ webapp/store/views.py
@@ -118,6 +118,10 @@ def store_blueprint(store_query=None):
     def store_view():
         return flask.render_template("store/store.html")
 
+    @store.route("/explore")
+    def explore_view():
+        return flask.render_template("store/store.html")
+
     @store.route("/youtube", methods=["POST"])
     def get_video_thumbnail_data():
         body = flask.request.form
@@ -342,6 +346,8 @@ def store_blueprint(store_query=None):
         store.add_url_rule("/search", "search", brand_search_snap)
     else:
         store.add_url_rule("/store", "homepage", store_view)
+
+    store.add_url_rule("/explore", "explore", explore_view)
 
     @store.route("/<snap_name>/create-track", methods=["POST"])
     @login_required
