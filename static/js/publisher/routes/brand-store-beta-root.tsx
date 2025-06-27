9f4a5eb5f7826af6fff55fc3a1022c37678a075f
Steve Rydz <steve.rydz@canonical.com>
2025-06-27 15:28:22 +0100

    Create hook for Jotai query

--- static/js/publisher/routes/brand-store-beta-root.tsx
+++ static/js/publisher/routes/brand-store-beta-root.tsx
@@ -2,24 +2,28 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useAtom } from "jotai";
 import { atomWithQuery } from "jotai-tanstack-query";
 
-const brandStoresAtom = atomWithQuery(() => ({
-  queryKey: ["brandStores"],
-  queryFn: async () => {
-    const response = await fetch("/api/stores");
+const useBrandStoresBeta = () => {
+  return atomWithQuery(() => ({
+    queryKey: ["brandStores"],
+    queryFn: async () => {
+      const response = await fetch("/api/stores");
 
-    if (!response.ok) {
-      throw Error("Unable to fetch stores");
-    }
+      if (!response.ok) {
+        throw Error("Unable to fetch stores");
+      }
 
-    const responseData = await response.json();
+      const responseData = await response.json();
 
-    if (!responseData.success) {
-      throw Error(responseData.message);
-    }
+      if (!responseData.success) {
+        throw Error(responseData.message);
+      }
 
-    return responseData.data;
-  },
-}));
+      return responseData.data;
+    },
+  }));
+};
+
+const brandStoresAtom = useBrandStoresBeta();
 
 function BrandStoreBetaRoute() {
   const [{ data, isPending, isError }] = useAtom(brandStoresAtom);
