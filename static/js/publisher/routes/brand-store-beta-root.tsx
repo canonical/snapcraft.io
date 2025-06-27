13985ba9797fc5e051f7fc9fa2535553187f919b
Steve Rydz <steve.rydz@canonical.com>
2025-06-27 15:26:17 +0100

    Setup atoms and queries

--- static/js/publisher/routes/brand-store-beta-root.tsx
+++ static/js/publisher/routes/brand-store-beta-root.tsx
@@ -1,14 +1,28 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useAtom } from "jotai";
+import { atomWithQuery } from "jotai-tanstack-query";
 
-import { useBrandStores } from "../hooks";
+const brandStoresAtom = atomWithQuery(() => ({
+  queryKey: ["brandStores"],
+  queryFn: async () => {
+    const response = await fetch("/api/stores");
+
+    if (!response.ok) {
+      throw Error("Unable to fetch stores");
+    }
+
+    const responseData = await response.json();
+
+    if (!responseData.success) {
+      throw Error(responseData.message);
+    }
+
+    return responseData.data;
+  },
+}));
 
 function BrandStoreBetaRoute() {
-  const { data: brandStoresList, isLoading } = useBrandStores();
+  const [{ data, isPending, isError }] = useAtom(brandStoresAtom);
 
   return (
     <Router>
