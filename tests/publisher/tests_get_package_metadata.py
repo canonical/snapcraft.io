d63b648c522fc9b750697396c98632a31a29a7e1
Ilayda Cavusoglu Pars <ilayda.cavusoglupars@canonical.com>
2025-07-04 12:22:59 +0300

    fix:Added handlers for errors separately

--- tests/publisher/tests_get_package_metadata.py
+++ tests/publisher/tests_get_package_metadata.py
@@ -87,7 +87,7 @@ class TestGetPackageMetadata(BaseTestCases):
             [
                 {
                     "code": "permission-required",
-                    "message": "No publisher or collaborator permission for the teams-for-linux snap package"
+                    "message": "No publisher or collaborator permission for the teams-for-linux snap package",
                 }
-            ]
+            ],
         )
