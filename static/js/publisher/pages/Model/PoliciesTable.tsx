565f7e99ba1001a4870ce685282fc5b8602b2ff5
Steve Rydz <steve.rydz@canonical.com>
2025-07-14 11:16:19 +0100

    fix: Fix issue where paginated tables weren't sorting all items

--- static/js/publisher/pages/Model/PoliciesTable.tsx
+++ static/js/publisher/pages/Model/PoliciesTable.tsx
@@ -2,14 +2,21 @@ import { SetStateAction, useState, Dispatch } from "react";
 import { useAtomValue, useAtom } from "jotai";
 import { useParams } from "react-router-dom";
 import { format } from "date-fns";
-import { MainTable, Button, Modal, Icon } from "@canonical/react-components";
+import {
+  MainTable,
+  Button,
+  Modal,
+  Icon,
+  TablePagination,
+} from "@canonical/react-components";
 
-import AppPagination from "../../components/AppPagination";
-
+import { sortByDateDescending } from "../../utils";
 import { usePolicies } from "../../hooks";
 import { UsePoliciesResponse } from "../../types/shared";
 import { brandIdState } from "../../state/brandStoreState";
 import { filteredPoliciesListState } from "../../state/policiesState";
+
+import { useSortTableData } from "../../hooks";
 
 import type { Policy } from "../../types/shared";
 
@@ -18,14 +25,13 @@ type Props = {
   setShowDeletePolicyErrorNotification: Dispatch<SetStateAction<boolean>>;
 };
 
-function ModelsTable({
+function PoliciesTable({
   setShowDeletePolicyNotification,
   setShowDeletePolicyErrorNotification,
 }: Props): React.JSX.Element {
   const { id, model_id } = useParams();
   const brandId = useAtomValue(brandIdState);
   const [policiesList, setPoliciesList] = useAtom(filteredPoliciesListState);
-  const [itemsToShow, setItemsToShow] = useState<Array<Policy>>(policiesList);
   const [showModal, setShowModal] = useState<boolean>(false);
   const [selectedPolicy, setSelectedPolicy] = useState<number | undefined>();
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -87,132 +93,137 @@ function ModelsTable({
     }, 5000);
   };
 
+  const headers = [
+    {
+      content: "Revision",
+      sortKey: "revision",
+    },
+    {
+      content: "Signing key",
+      sortKey: "name",
+      className: "u-align--right",
+    },
+    {
+      content: "Creation date",
+      sortKey: "created-at",
+      className: "u-align--right",
+    },
+    {
+      content: "Last updated",
+      sortKey: "modified-at",
+      className: "u-align--right",
+    },
+    {
+      content: "",
+    },
+  ];
+
+  const rows = policiesList.sort(sortByDateDescending).map((policy: Policy) => {
+    return {
+      columns: [
+        {
+          content: policy.revision,
+        },
+        {
+          content: policy["signing-key-name"],
+          className: "u-align--right",
+        },
+        {
+          content: format(new Date(policy["created-at"]), "dd/MM/yyyy"),
+          className: "u-align--right",
+        },
+        {
+          content:
+            policy["modified-at"] && policy["modified-at"]
+              ? format(new Date(policy["modified-at"]), "dd/MM/yyyy")
+              : "-",
+          className: "u-align--right",
+        },
+        {
+          content: (
+            <Button
+              className="u-no-margin--bottom"
+              onClick={() => {
+                setSelectedPolicy(policy.revision);
+                setShowModal(true);
+              }}
+            >
+              Delete
+            </Button>
+          ),
+          className: "u-align--right",
+        },
+      ],
+      sortData: {
+        revision: policy.revision,
+        name: policy["signing-key-name"],
+        "created-at": policy["created-at"],
+        "modified-at": policy["modified-at"],
+      },
+    };
+  });
+
+  const { rows: sortedRows, updateSort } = useSortTableData({ rows });
+
   return (
     <>
-      <div className="u-flex-grow">
+      <TablePagination
+        data={sortedRows}
+        pageLimits={[25, 50, 100, 200]}
+        position="below"
+      >
         <MainTable
           data-testid="policies-table"
           sortable
+          onUpdateSort={updateSort}
           emptyStateMsg="No policies available"
-          headers={[
-            {
-              content: "Revision",
-              sortKey: "revision",
-            },
-            {
-              content: "Signing key",
-              sortKey: "name",
-              className: "u-align--right",
-            },
-            {
-              content: "Creation date",
-              sortKey: "created-at",
-              className: "u-align--right",
-            },
-            {
-              content: "Last updated",
-              sortKey: "modified-at",
-              className: "u-align--right",
-            },
-            {
-              content: "",
-            },
-          ]}
-          rows={itemsToShow.map((policy: Policy) => {
-            return {
-              columns: [
-                {
-                  content: policy.revision,
-                },
-                {
-                  content: policy["signing-key-name"],
-                  className: "u-align--right",
-                },
-                {
-                  content: format(new Date(policy["created-at"]), "dd/MM/yyyy"),
-                  className: "u-align--right",
-                },
-                {
-                  content:
-                    policy["modified-at"] && policy["modified-at"]
-                      ? format(new Date(policy["modified-at"]), "dd/MM/yyyy")
-                      : "-",
-                  className: "u-align--right",
-                },
-                {
-                  content: (
-                    <Button
-                      className="u-no-margin--bottom"
-                      onClick={() => {
-                        setSelectedPolicy(policy.revision);
-                        setShowModal(true);
-                      }}
-                    >
-                      Delete
-                    </Button>
-                  ),
-                  className: "u-align--right",
-                },
-              ],
-              sortData: {
-                revision: policy.revision,
-                name: policy["signing-key-name"],
-                "created-at": policy["created-at"],
-                "modified-at": policy["modified-at"],
-              },
-            };
-          })}
+          headers={headers}
         />
-        {showModal && (
-          <Modal
-            close={() => {
-              setShowModal(false);
-            }}
-            title="Delete policy"
-            buttonRow={
-              <>
-                <Button
-                  className="u-no-margin--bottom"
-                  onClick={() => {
-                    setSelectedPolicy(undefined);
-                    setShowModal(false);
-                  }}
-                >
-                  Cancel
-                </Button>
-                <Button
-                  className="u-no-margin--bottom u-no-margin--right"
-                  appearance="positive"
-                  onClick={() => {
-                    deletePolicy(selectedPolicy);
-                  }}
-                >
-                  Delete policy
-                </Button>
-              </>
-            }
-          >
-            {isLoading ? (
-              <p>
-                <Icon name="spinner" className="u-animation--spin" />
-                &nbsp;Deleting policy...
-              </p>
-            ) : (
-              <p>
-                Are you sure you want to delete this policy? This action cannot
-                be undone.
-              </p>
-            )}
-          </Modal>
-        )}
-      </div>
-      <AppPagination
-        keyword="policy"
-        items={policiesList}
-        setItemsToShow={setItemsToShow}
-      />
+      </TablePagination>
+      {showModal && (
+        <Modal
+          close={() => {
+            setShowModal(false);
+          }}
+          title="Delete policy"
+          buttonRow={
+            <>
+              <Button
+                className="u-no-margin--bottom"
+                onClick={() => {
+                  setSelectedPolicy(undefined);
+                  setShowModal(false);
+                }}
+              >
+                Cancel
+              </Button>
+              <Button
+                className="u-no-margin--bottom u-no-margin--right"
+                appearance="positive"
+                onClick={() => {
+                  deletePolicy(selectedPolicy);
+                }}
+              >
+                Delete policy
+              </Button>
+            </>
+          }
+        >
+          {isLoading ? (
+            <p>
+              <Icon name="spinner" className="u-animation--spin" />
+              &nbsp;Deleting policy...
+            </p>
+          ) : (
+            <p>
+              Are you sure you want to delete this policy? This action cannot be
+              undone.
+            </p>
+          )}
+        </Modal>
+      )}
     </>
   );
 }
 
-export default ModelsTable;
+export default PoliciesTable;
--- static/js/publisher/pages/Models/ModelsTable.tsx
+++ static/js/publisher/pages/Models/ModelsTable.tsx
@@ -1,114 +1,113 @@
-import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useAtomValue } from "jotai";
 import { format } from "date-fns";
-import { MainTable } from "@canonical/react-components";
+import { MainTable, TablePagination } from "@canonical/react-components";
 
-import AppPagination from "../../components/AppPagination";
-
-import { maskString } from "../../utils";
+import { maskString, sortByDateDescending } from "../../utils";
 
 import { filteredModelsListState } from "../../state/modelsState";
+
+import { useSortTableData } from "../../hooks";
 
 import type { Model as ModelType } from "../../types/shared";
 
 function ModelsTable(): React.JSX.Element {
   const { id } = useParams();
   const modelsList = useAtomValue<Array<ModelType>>(filteredModelsListState);
-  const [itemsToShow, setItemsToShow] = useState<Array<ModelType>>(modelsList);
+
+  const headers = [
+    {
+      content: `Name (${modelsList.length})`,
+      sortKey: "name",
+    },
+    {
+      content: "API key",
+      className: "u-align--right",
+    },
+    {
+      content: "Policy revision",
+      className: "u-align--right",
+      style: {
+        width: "150px",
+      },
+    },
+    {
+      content: "Last updated",
+      className: "u-align--right",
+      sortKey: "modified-at",
+      style: {
+        width: "120px",
+      },
+    },
+    {
+      content: "Created date",
+      className: "u-align--right",
+      sortKey: "created-at",
+      style: {
+        width: "120px",
+      },
+    },
+  ];
+
+  const rows = modelsList.sort(sortByDateDescending).map((model: ModelType) => {
+    return {
+      columns: [
+        {
+          content: (
+            <Link to={`/admin/${id}/models/${model.name}`}>{model.name}</Link>
+          ),
+          className: "u-truncate",
+        },
+        {
+          content: maskString(model["api-key"]) || "-",
+          className: "u-align--right",
+        },
+        {
+          content:
+            model["policy-revision"] !== undefined
+              ? model["policy-revision"]
+              : "-",
+          className: "u-align--right",
+        },
+        {
+          content:
+            model["modified-at"] && model["modified-at"] !== null
+              ? format(new Date(model["modified-at"]), "dd/MM/yyyy")
+              : "-",
+          className: "u-align--right",
+        },
+        {
+          content:
+            model["created-at"] !== null
+              ? format(new Date(model["created-at"]), "dd/MM/yyyy")
+              : "-",
+          className: "u-align--right",
+        },
+      ],
+      sortData: {
+        name: model.name,
+        "modified-at": model["modified-at"],
+        "created-at": model["created-at"],
+      },
+    };
+  });
+
+  const { rows: sortedRows, updateSort } = useSortTableData({ rows });
 
   return (
-    <>
-      <div className="u-flex-grow">
-        <MainTable
-          data-testid="models-table"
-          sortable
-          emptyStateMsg="No models match this filter"
-          headers={[
-            {
-              content: `Name (${modelsList.length})`,
-              sortKey: "name",
-            },
-            {
-              content: "API key",
-              className: "u-align--right",
-            },
-            {
-              content: "Policy revision",
-              className: "u-align--right",
-              style: {
-                width: "150px",
-              },
-            },
-            {
-              content: "Last updated",
-              className: "u-align--right",
-              sortKey: "modified-at",
-              style: {
-                width: "120px",
-              },
-            },
-            {
-              content: "Created date",
-              className: "u-align--right",
-              sortKey: "created-at",
-              style: {
-                width: "120px",
-              },
-            },
-          ]}
-          rows={itemsToShow.map((model: ModelType) => {
-            return {
-              columns: [
-                {
-                  content: (
-                    <Link to={`/admin/${id}/models/${model.name}`}>
-                      {model.name}
-                    </Link>
-                  ),
-                  className: "u-truncate",
-                },
-                {
-                  content: maskString(model["api-key"]) || "-",
-                  className: "u-align--right",
-                },
-                {
-                  content:
-                    model["policy-revision"] !== undefined
-                      ? model["policy-revision"]
-                      : "-",
-                  className: "u-align--right",
-                },
-                {
-                  content:
-                    model["modified-at"] && model["modified-at"] !== null
-                      ? format(new Date(model["modified-at"]), "dd/MM/yyyy")
-                      : "-",
-                  className: "u-align--right",
-                },
-                {
-                  content:
-                    model["created-at"] !== null
-                      ? format(new Date(model["created-at"]), "dd/MM/yyyy")
-                      : "-",
-                  className: "u-align--right",
-                },
-              ],
-              sortData: {
-                name: model.name,
-                "modified-at": model["modified-at"],
-                "created-at": model["created-at"],
-              },
-            };
-          })}
-        />
-      </div>
-      <AppPagination
-        keyword="model"
-        items={modelsList}
-        setItemsToShow={setItemsToShow}
+    <TablePagination
+      data={sortedRows}
+      pageLimits={[25, 50, 100, 200]}
+      position="below"
+    >
+      <MainTable
+        data-testid="models-table"
+        sortable
+        onUpdateSort={updateSort}
+        emptyStateMsg="No models match this filter"
+        headers={headers}
       />
-    </>
+    </TablePagination>
   );
 }
 
