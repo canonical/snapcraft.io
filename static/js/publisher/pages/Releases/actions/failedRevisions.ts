9a78e8e862e2914b83b6c10bd4a35924a4853345
Ilayda Cavusoglu Pars <ilayda.cavusoglupars@canonical.com>
2025-07-22 08:58:17 +0300

    fix: updated action parameter type

--- static/js/publisher/pages/Releases/actions/failedRevisions.ts
+++ static/js/publisher/pages/Releases/actions/failedRevisions.ts
@@ -1,6 +1,6 @@
 export const UPDATE_FAILED_REVISIONS = "UPDATE_FAILED_REVISIONS";
 
-export function updateFailedRevisions(failedRevisions: { channel: string, architecture: string }) {
+export function updateFailedRevisions(failedRevisions: Array<{ channel: string, architecture: string, revision: number }>) {
     return {
         type: UPDATE_FAILED_REVISIONS,
         payload: { failedRevisions },
