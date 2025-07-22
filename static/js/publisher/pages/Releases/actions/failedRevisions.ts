f677ce1278db5413c9bac3042a6357605f57503e
Ilayda Cavusoglu Pars <ilayda.cavusoglupars@canonical.com>
2025-07-22 13:08:40 +0300

    fix: add type for updateFailedRevision action

--- static/js/publisher/pages/Releases/actions/failedRevisions.ts
+++ static/js/publisher/pages/Releases/actions/failedRevisions.ts
@@ -1,6 +1,8 @@
 export const UPDATE_FAILED_REVISIONS = "UPDATE_FAILED_REVISIONS";
 
-export function updateFailedRevisions(failedRevisions: Array<{ channel: string, architecture: string, revision: number }>) {
+type FailedRevisionInfo = { channel: string, architecture: string, revision: number }
+
+export function updateFailedRevisions(failedRevisions: FailedRevisionInfo[]) {
     return {
         type: UPDATE_FAILED_REVISIONS,
         payload: { failedRevisions },
