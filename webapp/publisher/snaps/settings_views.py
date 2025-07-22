b19cd4a1f9e639edfc3f8ce7a36900a6420e1aef
Eduard M <4310497+edisile@users.noreply.github.com>
2025-07-22 12:11:21 +0200

    fix: use correct type conversion for None -> bool
    (fixes #5224)

--- webapp/publisher/snaps/settings_views.py
+++ webapp/publisher/snaps/settings_views.py
@@ -71,7 +71,7 @@ def get_settings_data(snap_name):
         "update_metadata_on_release": snap_details[
             "update_metadata_on_release"
         ],
-        "visibility_locked": snap_details["visibility_locked"] | False,
+        "visibility_locked": bool(snap_details["visibility_locked"]),
     }
 
     return flask.jsonify({"success": True, "data": context})
