06afde63ff811b6bf51c8422cc5f27e175de7676
Steve Rydz <steve.rydz@canonical.com>
2025-08-04 16:35:08 +0100

    chore: Move brand store settings endpoint to endpoints section of app

--- webapp/admin/views.py
+++ webapp/admin/views.py
@@ -46,24 +46,6 @@ def get_admin(path):
     return flask.render_template("admin/admin.html", **context)
 
 
-@admin.route("/api/store/<store_id>/settings", methods=["PUT"])
-@login_required
-@exchange_required
-def post_settings(store_id):
-    settings = {}
-    settings["private"] = json.loads(flask.request.form.get("private"))
-    settings["manual-review-policy"] = flask.request.form.get(
-        "manual-review-policy"
-    )
-
-    res = {}
-
-    dashboard.change_store_settings(flask.session, store_id, settings)
-    res["msg"] = "Changes saved"
-
-    return jsonify({"success": True})
-
-
 @admin.route("/api/store/<store_id>/snaps", methods=["POST"])
 @login_required
 @exchange_required
