5db94a35de9ceb86dd681b0ff478ac328e50de18
Eduard M <4310497+edisile@users.noreply.github.com>
2025-08-19 14:23:56 +0200

    feature: add Cypress to test bundling results (#5310)
    feature: js bundle e2e tests using Cypress

---
+++ cypress/e2e/cookie-policy.cy.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+describe("Test cookie-policy.js bundle on / page", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:8004/");
+  });
+
+  it("Renders correctly", () => {
+    cy.get("dialog.cookie-policy").should("not.be.empty");
+  });
+
+  it("Closes when accepting", () => {
+    cy.get("#cookie-policy-button-accept").click();
+    cy.get("dialog.cookie-policy").should("not.exist");
+  });
+
+  // TODO: more tests
+});
---
+++ cypress/e2e/publisher-details.cy.js
@@ -0,0 +1,40 @@
+/// <reference types="cypress" />
+describe("Test publisher-details.js bundle on a publisher page (specifically /publisher/jetbrains)", () => {
+  beforeEach(function () {
+    cy.visit("http://localhost:8004/publisher/jetbrains").then(() => {
+      // publisher-details.js is only loaded in templates/store/publisher-details.html,
+      // when publisher is "kde", "snapcrafters" or "jetbrains", and when there's a blog_slug
+      // attribute in the publisher details context
+      const script = cy.$$('script[src*="/publisher-details.js"]');
+      if (!script.length) {
+        this.skip();
+      }
+    });
+  });
+
+  it("sets 'window.snapcraft'", () => {
+    cy.window().then((window) => {
+      expect(typeof window?.snapcraft).to.equal("object");
+    });
+  });
+
+  it("sets 'window.snapcraft.public'", () => {
+    cy.window().then((window) => {
+      expect(typeof window?.snapcraft?.public).to.equal("object");
+    });
+  });
+
+  it("sets 'window.snapcraft.public.publisherDetails'", () => {
+    cy.window().then((window) => {
+      expect(typeof window?.snapcraft?.public?.blog).to.equal("object");
+    });
+  });
+
+  it("sets 'window.snapcraft.public.publisherDetails.snapDetailsPosts'", () => {
+    cy.window().then((window) => {
+      expect(
+        typeof window?.snapcraft?.public?.publisherDetails?.snapDetailsPosts,
+      ).to.equal("function");
+    });
+  });
+});
