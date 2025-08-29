5f8cfee8fa08b79cc8b5868c7da1d2eb5265a36a
Eduard M <4310497+edisile@users.noreply.github.com>
2025-08-29 14:09:47 +0200

    fix: load unprefixed env vars explicitly (#5335)
    * fix: load unprefixed env vars explicitly
    
    Fix an issue where the webapp.config module's variables do not reflect those available in the Flask app.config object created by flask_base
    
    * fix: import webapp.config at the top of app module
    
    * fix: add comment to explain why we load webapp.config first

--- webapp/app.py
+++ webapp/app.py
@@ -9,6 +9,10 @@ import webapp.api
 
 from talisker import logging
 
+# We import the config module before anything else to make sure env vars are
+# loaded properly and the FLASK_* prefix is stripped before they are parsed
+import webapp.config
+
 from canonicalwebteam.flask_base.app import FlaskBase
 from webapp.blog.views import init_blog
 from webapp.docs.views import init_docs
