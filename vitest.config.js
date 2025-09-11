44813615f3e8cd08e77ce3e4233bb789fdf29c97
Eduard M <4310497+edisile@users.noreply.github.com>
2025-09-11 12:25:36 +0200

    fix: improve vitest performance

--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  css: false,
   test: {
+    dir: "static/js",
     globals: true,
     environment: "happy-dom",
     silent: "passed-only",
+    pool: "threads",
     coverage: {
       enabled: false,
       reporter: ["cobertura", "html"],
