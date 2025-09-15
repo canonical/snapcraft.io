5f8d298b26ea0aada85680ae2f6ec3dbf882ac9c
Steve Rydz <steve.rydz@canonical.com>
2025-09-15 15:41:54 +0100

    chore: Remove first snap flow templates (#5368)

--- tests/tests_helpers.py
+++ tests/tests_helpers.py
@@ -3,8 +3,6 @@ import os
 import hashlib
 
 from webapp import helpers
-from webapp.app import create_app
-from flask_testing import TestCase
 
 
 class GetDnsVerificationTokenTest(unittest.TestCase):
@@ -16,17 +14,3 @@ class GetDnsVerificationTokenTest(unittest.TestCase):
             helpers.get_dns_verification_token("spotify", "spotify.com"),
             test_hash,
         )
-
-
-class DirectoryExistsTest(TestCase):
-    def create_app(self):
-        app = create_app(testing=True)
-        return app
-
-    def test_directory_exists(self):
-        filename = "first_snap/content/c"
-        self.assertTrue(helpers.directory_exists(filename))
-
-    def test_directory_not_exists(self):
-        filename = "first_snap/content/test"
-        self.assertFalse(helpers.directory_exists(filename))
