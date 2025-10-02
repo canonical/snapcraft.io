1fc12b9720df78ca2d3205b98f163c76d28bcd9a
Steve Rydz <steve.rydz@canonical.com>
2025-10-02 09:58:14 +0100

    Co-pilot code review fix

--- static/js/store/pages/Explore/CardsLoader.tsx
+++ static/js/store/pages/Explore/CardsLoader.tsx
@@ -2,16 +2,16 @@ import { Row, Col } from "@canonical/react-components";
 import { LoadingCard } from "@canonical/store-components";
 import { v4 as uuidv4 } from "uuid";
 
-function CardsLoader(): JSX.Element {
-  const elementIds: string[] = Array.of(
-    uuidv4(),
-    uuidv4(),
-    uuidv4(),
-    uuidv4(),
-    uuidv4(),
-    uuidv4(),
-  );
+const elementIds: string[] = Array.of(
+  uuidv4(),
+  uuidv4(),
+  uuidv4(),
+  uuidv4(),
+  uuidv4(),
+  uuidv4(),
+);
 
+function CardsLoader(): JSX.Element {
   return (
     <Row>
       {elementIds.map((id) => (
