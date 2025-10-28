be33f00a53651084c509fda9fb7ac6f731f46803
Steve Rydz <steve.rydz@canonical.com>
2025-10-28 08:43:59 +0000

    chore: Remove code for client-side explore page (#5441)

--- static/js/store/types/index.ts
+++ static/js/store/types/index.ts
@@ -3,13 +3,5 @@ import type { Category } from "./Category";
 import type { Package } from "./Package";
 import type { Packages } from "./Packages";
 import type { RecommendationData } from "./RecommendationData";
-import type { SlicesData } from "./SlicesData";
 
-export type {
-  CardData,
-  Category,
-  Package,
-  Packages,
-  RecommendationData,
-  SlicesData,
-};
+export type { CardData, Category, Package, Packages, RecommendationData };
