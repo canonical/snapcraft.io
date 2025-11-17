e1cd26f795d94e148534551c4f26b4ff61f31957
Alvaro Mateo <alvaromateo9@gmail.com>
2025-11-17 16:43:57 +0100

    WD-19723 - Add comments about duplicated code in charmhub.io

--- static/js/base/navigation/index.ts
+++ static/js/base/navigation/index.ts
@@ -1,3 +1,6 @@
+// This same code can be found duplicated in charmhub.io
+// If you modify these files for some reason it is likely that you will also need to modify charmhub.io's ones
+
 import "./login";
 
 import { createNav as createAllCanonicalNav } from "@canonical/global-nav";
