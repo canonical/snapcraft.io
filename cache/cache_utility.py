489c6f79f31b29ccfc341fe4a0f521a1127517f8
Eduard M <4310497+edisile@users.noreply.github.com>
2025-12-05 18:15:36 +0100

    fix: replace redis cache with mock for good reasons

--- cache/cache_utility.py
+++ cache/cache_utility.py
@@ -1,7 +1,20 @@
 from canonicalwebteam.stores_web_redis.utility import RedisCache
 from webapp.config import APP_NAME
 
-redis_cache = RedisCache(
+
+class MockCache(RedisCache):
+    # extend the redis cache by replacing all public methods with mocks
+    def get(self, key, expected_type=str):
+        return None
+
+    def set(self, key, value, ttl=300):
+        pass
+
+    def delete(self, key):
+        pass
+
+
+redis_cache = MockCache(
     namespace=APP_NAME,
     maxsize=1000,
     ttl=300,
