4f9dbd2839735c6c5ec31bcfcf89ca2fad2b0d80
Bartek Szopka <bartek.szopka@canonical.com>
2026-01-12 14:16:01 +0100

    Revert "Fix formatting publisher name (#5546)"
    This reverts commit 2df776 to avoid double escaping publisher names.

--- webapp/template_utils.py
+++ webapp/template_utils.py
@@ -3,7 +3,6 @@ import hashlib
 import os
 from dateutil import parser
 from emoji import replace_emoji
-from markupsafe import escape
 
 
 # generator functions for templates
@@ -118,22 +117,20 @@ def format_number(number: int):
 
 def format_display_name(display_name):
     """Template function that formats the displayed name
-    primarily to remove emoji and escape for safe HTML rendering
+    primarily to remove emoji
     """
-    return escape(replace_emoji(display_name, replace=""))
+    return replace_emoji(display_name, replace="")
 
 
 def display_name(display_name, username):
     """Template function that returns the displayed name if the username
-    is the same, or the displayed name and the username if different,
-    escaping both for safe HTML rendering
+    is the same, or the dispayed name and the username if differents
     """
-    safe_display_name = format_display_name(display_name)
-    safe_username = escape(username)
-    if safe_display_name.lower() == safe_username.lower():
-        return safe_display_name
+    display_name = format_display_name(display_name)
+    if display_name.lower() == username.lower():
+        return display_name
     else:
-        return f"{safe_display_name} ({safe_username})"
+        return f"{display_name} ({username})"
 
 
 def format_date(timestamp, format):
