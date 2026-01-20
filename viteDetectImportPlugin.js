fcf2445ce98d001fa2c7da26f3af54cd2ef10e5d
Eduard M <4310497+edisile@users.noreply.github.com>
2026-01-20 10:34:09 +0100

    fix: build on windows

--- viteDetectImportPlugin.js
+++ viteDetectImportPlugin.js
@@ -14,24 +14,31 @@ const viteDetectImportPlugin = () => ({
     // In dev mode we don't need to define entry points
     if (env.mode === "development") return config;
 
+    // In production mode the entry points can be read by parsing the strings
+    // passed as arguments to the `vite_import(...)` calls in templates/; we
+    // use ripgrep to search for all `vite_import(<filename>)` calls, the
+    // result is a big multi-line string with all the file names.
+    // We use ripgrep because it's very fast and cross-platform.
+
     let input = [];
 
     const pattern = /vite_import\(["'](.+)["']\)/
       .toString() // convert the regex into something that ripgrep can use
       .slice(1, -1) // remove regex / delimiters
-      .replaceAll(/([^\\])"/g, '$1\\"'); // escape all unescaped "
+      .replaceAll(/([^\\])"/g, '$1\\"'); // escape all unescaped double quotes
 
-    // In production mode the entry points can be read by parsing the strings
-    // passed as arguments to the `vite_import(...)` calls in templates/; we
-    // use ripgrep to search for all `vite_import(<filename>)` calls, the
-    // result is a big multi-line string with all the file names
+    const captureString =
+      process.platform === "win32"
+        ? `$1` // windows doesn't ask us to escape dollar sign with a backslash
+        : `\\$1`; // any other platform does though
+
     const viteImports =
       execSync(
-        `${rg} -oNI -t "html" -e "${pattern}" ./templates -r "\\$1"`,
+        `${rg} -oNI -t "html" -e "${pattern}" ./templates -r "${captureString}"`,
       ).toString() || "";
 
-    // viteImports is a multi-line string and might possibly contain empty
-    // lines, so split it and filter it
+    // the string has multiple lines, some of which might be blank, so split
+    // on newlines and filter out the empty strings
     const imports = viteImports.split("\n").filter(Boolean);
 
     // remove possible duplicate imports, sort just for clarity
