22d7a8c9429c3ec71d0e847e0fb154894cbfc77a
Steve Rydz <steve.rydz@canonical.com>
2026-02-12 14:47:59 +0000

    feat: Add configure remodels form

--- static/js/publisher/pages/Remodel/RemodelTable.tsx
+++ static/js/publisher/pages/Remodel/RemodelTable.tsx
@@ -1,8 +1,9 @@
 import { useAtomValue } from "jotai";
-import { MainTable } from "@canonical/react-components";
+import { MainTable, TablePagination } from "@canonical/react-components";
 import { format } from "date-fns";
 
 import { filteredRemodelsListState } from "../../state/remodelsState";
+import { useSortTableData } from "../../hooks";
 
 import type { Remodel } from "../../types/shared";
 
@@ -10,13 +11,22 @@ function RemodelTable(): React.JSX.Element {
   const remodels = useAtomValue(filteredRemodelsListState);
 
   const headers = [
-    { content: "Target model", sortKey: "to-model" },
-    { content: "Original model", sortKey: "from-model" },
-    { content: "Allowed devices", className: "u-align--right" },
+    { content: "Target model", sortKey: "to-model", style: { width: "250px" } },
+    {
+      content: "Original model",
+      sortKey: "from-model",
+      style: { width: "250px" },
+    },
+    {
+      content: "Allowed devices",
+      className: "u-align--right",
+      style: { width: "150px" },
+    },
     {
       content: "Created date",
       className: "u-align--right",
       sortKey: "created-at",
+      style: { width: "120px" },
     },
     { content: "Note" },
   ];
@@ -41,14 +51,23 @@ function RemodelTable(): React.JSX.Element {
     };
   });
 
+  const { rows: sortedRows, updateSort } = useSortTableData({ rows });
+
   return (
-    <MainTable
-      data-testid="remodel-table"
-      sortable
-      emptyStateMsg="No remodels match this filter"
-      headers={headers}
-      rows={rows}
-    />
+    <TablePagination
+      data={sortedRows}
+      pageLimits={[25, 50, 100, 200]}
+      position="below"
+    >
+      <MainTable
+        data-testid="remodel-table"
+        sortable
+        emptyStateMsg="No remodels match this filter"
+        headers={headers}
+        rows={rows}
+        onUpdateSort={updateSort}
+      />
+    </TablePagination>
   );
 }
 
