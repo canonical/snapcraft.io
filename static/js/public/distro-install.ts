83b3a5866352b464e81e70662a30ddc8afab244a
Ilayda Cavusoglu Pars <ilayda.cavusoglupars@canonical.com>
2026-03-13 19:22:26 +0300

    track installs on distro page

--- static/js/public/distro-install.ts
+++ static/js/public/distro-install.ts
@@ -2,7 +2,10 @@ import screenshots from "./snap-details/screenshots";
 import videos from "./snap-details/videos";
 import initExpandableArea from "./expandable-area";
 import triggerEventWhenVisible from "./ga-scroll-event";
+import initCopyCommand from "./snap-details/copyCommand";
 import declareGlobal from "../libs/declare";
+
+initCopyCommand();
 
 declareGlobal("snapcraft.public.distroInstall", {
   screenshots,
