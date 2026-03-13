766edf10a6f2bb54e76a38b9e33dee528b036d0e
Ilayda Cavusoglu Pars <ilayda.cavusoglupars@canonical.com>
2026-03-13 20:06:51 +0300

    track installs on distro page

--- static/js/public/distro-install.ts
+++ static/js/public/distro-install.ts
@@ -4,6 +4,11 @@ import initExpandableArea from "./expandable-area";
 import triggerEventWhenVisible from "./ga-scroll-event";
 import initCopyCommand from "./snap-details/copyCommand";
 import declareGlobal from "../libs/declare";
+import { trackPageView } from "@canonical/analytics-events";
+
+if (window.ANALYTICS_ENDPOINT) {
+  trackPageView("snap_distro_install_page");
+}
 
 initCopyCommand();
 
