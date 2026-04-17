e1dd5b2059b11a517c65ecea0612ac2bda774103
Eduard M <4310497+edisile@users.noreply.github.com>
2026-04-17 14:44:21 +0200

    feature: create charm for deployment

---
+++ charm/src/charm.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""Flask Charm entrypoint."""
+
+import logging
+import typing
+
+import ops
+
+import paas_charm.flask
+
+logger = logging.getLogger(__name__)
+
+
+class CharmCharm(paas_charm.flask.Charm):
+    """Flask Charm service."""
+
+    def __init__(self, *args: typing.Any) -> None:
+        """Initialize the instance.
+
+        Args:
+            args: passthrough to CharmBase.
+        """
+        super().__init__(*args)
+
+
+if __name__ == "__main__":
+    ops.main(CharmCharm)
