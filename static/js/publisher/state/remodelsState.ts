1736696e51b79970b7b4ccf7fffc421e4355bed6
Steve Rydz <steverydz@gmail.com>
2026-04-23 09:55:10 +0100

    chore: Remove sorting and filtering from remodels table

--- static/js/publisher/state/remodelsState.ts
+++ static/js/publisher/state/remodelsState.ts
@@ -4,37 +4,4 @@ import type { Remodel } from "../types/shared";
 
 const remodelsListState = atom([] as Remodel[]);
 
-const remodelsListFilterState = atom("" as string);
-
-function getFilteredRemodels(
-  remodels: Array<Remodel>,
-  filterQuery?: string | null,
-) {
-  if (!filterQuery) {
-    return remodels;
-  }
-
-  return remodels.filter((remodel: Remodel) => {
-    if (
-      (remodel["from-model"] && remodel["from-model"].includes(filterQuery)) ||
-      (remodel["to-model"] && remodel["to-model"].includes(filterQuery))
-    ) {
-      return true;
-    }
-
-    return false;
-  });
-}
-
-const filteredRemodelsListState = atom<Array<Remodel>>((get) => {
-  const filter = get(remodelsListFilterState);
-  const remodels = get(remodelsListState);
-
-  return getFilteredRemodels(remodels, filter);
-});
-
-export {
-  remodelsListState,
-  remodelsListFilterState,
-  filteredRemodelsListState,
-};
+export { remodelsListState };
