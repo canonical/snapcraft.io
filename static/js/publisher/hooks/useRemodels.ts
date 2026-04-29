5194b4a42036df414c75484cd82c7385a6722e44
Steve Rydz <steve.rydz@canonical.com>
2026-04-29 14:02:52 +0100

    feat: Use Store API pagination for remodels table (#5681)

--- static/js/publisher/hooks/useRemodels.ts
+++ static/js/publisher/hooks/useRemodels.ts
@@ -1,44 +1,46 @@
 import { useQuery, UseQueryResult } from "react-query";
-import type { Remodel, RemodelResponse, ApiResponse } from "../types/shared";
+import type { RemodelResponse, ApiResponse } from "../types/shared";
 
 const useRemodels = (
   brandId: string | undefined,
-  modelId: string | undefined,
+  urlSearchParams?: {
+    page?: string | null;
+    pageSize?: number;
+    fromModel?: string;
+  },
 ): UseQueryResult<ApiResponse<RemodelResponse>, Error> => {
+  const url = new URL(
+    `/api/store/${brandId}/models/remodel-allowlist`,
+    window.location.origin,
+  );
+
+  if (urlSearchParams) {
+    const { page, pageSize, fromModel } = urlSearchParams;
+
+    if (page) {
+      url.searchParams.set("page", page);
+    }
+
+    if (pageSize) {
+      url.searchParams.set("page-size", pageSize.toString());
+    }
+
+    if (fromModel) {
+      url.searchParams.set("from-model", fromModel);
+    }
+  }
+
   return useQuery<ApiResponse<RemodelResponse>, Error>({
-    queryKey: ["remodels", brandId, modelId],
+    queryKey: [
+      "remodels",
+      brandId,
+      urlSearchParams?.page,
+      urlSearchParams?.pageSize,
+      urlSearchParams?.fromModel,
+    ],
     queryFn: async () => {
-      const response = await fetch(
-        `/api/store/${brandId}/models/remodel-allowlist`,
-      );
-
+      const response = await fetch(url);
       const responseData = await response.json();
-
-      if (responseData.data?.allowlist) {
-        const remodelsForCurrentModel = responseData.data.allowlist.filter(
-          (remodel: Remodel) => {
-            return (
-              remodel["from-model"] === modelId ||
-              remodel["to-model"] === modelId
-            );
-          },
-        );
-
-        responseData.data.allowlist = remodelsForCurrentModel.sort(
-          (a: Remodel, b: Remodel) => {
-            if (a["created-at"] > b["created-at"]) {
-              return -1;
-            }
-
-            if (a["created-at"] < b["created-at"]) {
-              return 1;
-            }
-
-            return 0;
-          },
-        );
-      }
-
       return responseData;
     },
     enabled: !!brandId,
