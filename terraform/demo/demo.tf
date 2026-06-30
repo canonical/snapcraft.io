a06bc338f6668ef7e0b10277b9ec22b19b092a4f
Alvaro Mateo <alvaromateo9@gmail.com>
2026-06-30 09:26:36 +0200

    Rebase and update after migration to PS6

--- terraform/demo/demo.tf
+++ terraform/demo/demo.tf
@@ -7,30 +7,22 @@ resource "juju_application" "demo" {
   }
 
   config = {
+    environment = "production"
     bsi-url = "https://build.snapcraft.io"
     devicegw-url = "https://api.snapcraft.io/"
-    discourse-api-key = "secret:${data.juju_secret.snapcraft_io-discourse_api_key.secret_id}"
-    discourse-api-username = "system"
-    dns-verification-salt = "secret:${data.juju_secret.snapcraft_io-dns_verification_salt.secret_id}"
-    environment = "production"
-    flask-preferred-url-scheme = "HTTPS"
-    flask-secret-key = "secret:${data.juju_secret.snapcraft_io-flask_secret_key.secret_id}"
-    github-client-id = "029a65c1d9dc821b0227"
-    github-client-secret = "secret:${data.juju_secret.snapcraft_io-github_client_secret.secret_id}"
-    github-snapcraft-bot-user-token = "secret:${data.juju_secret.snapcraft_io-github_snapcraft_bot_user_token.secret_id}"
-    github-snapcraft-user-token = "secret:${data.juju_secret.snapcraft_io-github_snapcraft_user_token.secret_id}"
-    github-webhook-host-url = "https://snapcraft.io/"
-    github-webhook-secret = "secret:${data.juju_secret.snapcraft_io-github_webhook_secret.secret_id}"
     login-url = "https://login.ubuntu.com"
-    lp-api-token = "secret:${data.juju_secret.snapcraft_io-lp_api_token.secret_id}"
-    lp-api-token-secret = "secret:${data.juju_secret.snapcraft_io-lp_api_token_secret.secret_id}"
-    lp-api-username = "build.snapcraft.io"
-    marketo-client-id = "secret:${data.juju_secret.snapcraft_io-marketo_client_id.secret_id}"
-    marketo-client-secret = "secret:${data.juju_secret.snapcraft_io-marketo_client_secret.secret_id}"
     publishergw-url = "https://api.charmhub.io"
     report-sheet-url = "https://script.google.com/macros/s/AKfycbywNDNVeD4_xnE36HP7gJUbbLHNrrcxgy0yVuwr0poPfGoDnH0Vl1oOWjnRXNtLkrcmlQ/exec"
     snapstore-dashboard-api-url = "https://dashboard.snapcraft.io/"
-    youtube-api-key = "secret:${data.juju_secret.snapcraft_io-youtube_api_key.secret_id}"
+    analytics-endpoint = ""
+    // secrets
+    discourse = data.juju_secret.snapcraft-discourse.secret_uri
+    dns-verification = data.juju_secret.snapcraft-dns-verification.secret_uri
+    github = data.juju_secret.snapcraft-github.secret_uri
+    lp = data.juju_secret.snapcraft-lp.secret_uri
+    marketo = data.juju_secret.snapcraft-marketo.secret_uri
+    youtube = data.juju_secret.snapcraft-youtube.secret_uri
+    turnstile = data.juju_secret.snapcraft-turnstile.secret_uri
   }
 }
 
